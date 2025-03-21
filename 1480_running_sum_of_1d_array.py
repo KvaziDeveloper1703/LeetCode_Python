88b23d573f665a27a313eedf2eec8e15d19da822
Viktor <viktor.sbruev.1999@gmail.com>
2025-03-22 01:07:13 +0200

    Corrected task 1480

--- 1480_running_sum_of_1d_array.py
+++ 1480_running_sum_of_1d_array.py
@@ -1,26 +1,25 @@
 """
-Given an array nums. We define a running sum of an array as runningSum[i] = sum(nums[0]…nums[i]).
+Given an array of integers nums, return the running sum of the array.
 
-Return the running sum of nums.
+The running sum at index i is defined as the sum of all elements from index 0 to i, i.e.:
+runningSum[i] = nums[0] + nums[1] + ... + nums[i]
 
-Example 1:
-Input: nums = [1,2,3,4]
-Output: [1,3,6,10]
+Examples:
+Input: nums = [1, 2, 3, 4]
+Output: [1, 3, 6, 10]
 
-Example 2:
-Input: nums = [1,1,1,1,1]
-Output: [1,2,3,4,5]
+Input: nums = [1, 1, 1, 1, 1]
+Output: [1, 2, 3, 4, 5]
 
-Example 3:
-Input: nums = [3,1,2,10,1]
-Output: [3,4,6,16,17]
+Input: nums = [3, 1, 2, 10, 1]
+Output: [3, 4, 6, 16, 17]
 """
 
 class Solution(object):
     def runningSum(self, nums):
         result = []
         current_sum = 0
-        for num in nums:
-            current_sum += num
+        for number in nums:
+            current_sum += number
             result.append(current_sum)
         return result

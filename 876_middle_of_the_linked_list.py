086c53d297cd924497d7869dc89edcdf3f8cd3a0
Viktor <viktor.sbruev.1999@gmail.com>
2025-03-22 01:00:11 +0200

    Corrected task 876

--- 876_middle_of_the_linked_list.py
+++ 876_middle_of_the_linked_list.py
@@ -1,20 +1,20 @@
 """
-Given the head of a singly linked list, return the middle node of the linked list.
+You are given the head of a singly linked list. Return the middle node of the linked list.
+If the list has an even number of nodes, return the second of the two middle nodes.
+Return the middle node along with all the nodes that follow it.
 
-If there are two middle nodes, return the second middle node.
+Examples:
 
-Example 1:
-Input: head = [1,2,3,4,5]
-Output: [3,4,5]
+Input: head = [1, 2, 3, 4, 5]
+Output: [3, 4, 5]
 
-Example 2:
-Input: head = [1,2,3,4,5,6]
-Output: [4,5,6]
+Input: head = [1, 2, 3, 4, 5, 6]
+Output: [4, 5, 6]
 """
 
 class ListNode(object):
-    def __init__(self, val=0, next=None):
-        self.val = val
+    def __init__(self, value=0, next=None):
+        self.value = value
         self.next = next
 
 class Solution(object):

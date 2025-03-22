165fdbbdcf96e98ec44a2dc5d46c87b6546c371c
Viktor <viktor.sbruev.1999@gmail.com>
2025-03-22 17:38:07 +0200

    Corrected task 189

--- 189_rotate_array.py
+++ 189_rotate_array.py
@@ -9,3 +9,18 @@ Output: [5, 6, 7, 1, 2, 3, 4]
 Input: nums = [-1, -100, 3, 99], k = 2
 Output: [3, 99, -1, -100]
 '''
+
+class Solution:
+    def rotate(self, nums: List[int], k: int) -> None:
+        n = len(nums)
+        k = k % n
+
+        def reverse_part(start: int, end: int):
+            while start < end:
+                nums[start], nums[end] = nums[end], nums[start]
+                start += 1
+                end -= 1
+
+        reverse_part(0, n - 1)
+        reverse_part(0, k - 1)
+        reverse_part(k, n - 1)

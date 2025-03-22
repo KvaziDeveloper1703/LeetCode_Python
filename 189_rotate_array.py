ba391798c813578c73fc0a9b98a53c109ecdb4ba
Viktor <viktor.sbruev.1999@gmail.com>
2025-03-22 17:37:38 +0200

    Added task 189

---
+++ 189_rotate_array.py
@@ -0,0 +1,11 @@
+'''
+Given an integer array nums, rotate the array to the right by k steps, where k is a non-negative integer.
+The rotation should be done in-place, meaning you should modify the input array directly without using extra space for another array.
+
+Examples:
+Input: nums = [1, 2, 3, 4, 5, 6, 7], k = 3
+Output: [5, 6, 7, 1, 2, 3, 4]
+
+Input: nums = [-1, -100, 3, 99], k = 2
+Output: [3, 99, -1, -100]
+'''

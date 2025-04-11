770c3ee88a4b5a77cbea85b744200857b9ba71d4
Viktor <viktor.sbruev.1999@gmail.com>
2025-04-11 20:04:05 +0300

    Modified task 42

--- 45_jump_game_2.py
+++ 45_jump_game_2.py
@@ -1,28 +1,29 @@
 '''
-You are given a 0-indexed array of integers nums of length n. You start at the first element nums[0].
-Each element nums[i] represents the maximum number of steps you can jump forward from index i. In other words, from index i, you can jump to any index in the range [i + 1, i + nums[i]], as long as it is within bounds (i + j < n).
-Your task is to return the minimum number of jumps required to reach the last index (nums[n - 1]).
+You are given a 0-indexed array of integers numbers of length n. You start at the first element numbers[0].
+Each element numbers[i] represents the maximum number of steps you can jump forward from index i. In other words, from index i, you can jump to any index in the range [i + 1, i + numbers[i]], as long as it is within bounds (i + j < n).
+Your task is to return the minimum number of jumps required to reach the last index (numbers[n - 1]).
 You can assume that it is always possible to reach the last index.
 
 Examples:
-Input: nums = [2, 3, 1, 1, 4]
+Input: numbers = [2, 3, 1, 1, 4]
 Output: 2
 
-Input: nums = [2, 3, 0, 1, 4]
+Input: numbers = [2, 3, 0, 1, 4]
 Output: 2
 '''
 
-class Solution:
-    def jump(self, nums: List[int]) -> int:
-        jumps = 0
-        current_end = 0
-        farthest = 0
+from typing import List
 
-        for i in range(len(nums) - 1):
-            farthest = max(farthest, i + nums[i])
+def jump(numbers: List[int]) -> int:
+    jumps = 0
+    current_end = 0
+    farthest = 0
 
-            if i == current_end:
-                jumps += 1
-                current_end = farthest
+    for i in range(len(numbers) - 1):
+        farthest = max(farthest, i + numbers[i])
 
-        return jumps
+        if i == current_end:
+            jumps += 1
+            current_end = farthest
+
+    return jumps

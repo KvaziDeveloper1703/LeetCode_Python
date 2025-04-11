6e6358eb53132f4aac5bf6971e445e8d61438131
Viktor <viktor.sbruev.1999@gmail.com>
2025-04-11 20:44:35 +0300

    Modified task 58

--- 58_length_of_last_word.py
+++ 58_length_of_last_word.py
@@ -1,18 +1,18 @@
 '''
-Given a string s containing words and spaces, return the length of the last word in the string.
+Given a string S containing words and spaces, return the length of the last word in the string.
 A word is defined as the longest sequence of non-space characters.
 
-Examples:
-Input: s = "Hello World"
-Output: 5 (The last word is "World")
+Example:
+Input: S = "Hello World"
+Output: 5
 
-Input: s = " fly me to the moon "
-Output: 4 (The last word is "moon")
+Дана строка S, содержащая слова и пробелы. Верните длину последнего слова в строке.
+Словом считается самая длинная последовательность символов, не содержащих пробелов.
 
-Input: s = "luffy is still joyboy"
-Output: 6 (The last word is "joyboy")
+Пример:
+Вход: S = "Hello World"
+Выход: 5
 '''
 
-class Solution:
-    def lengthOfLastWord(self, s: str) -> int:
-        return len(s.strip().split()[-1])
+def length_of_last_word(S: str) -> int:
+    return len(S.strip().split()[-1])

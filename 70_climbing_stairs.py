cfdf296daf30a877031e82a7ab592e532cba6fb7
Viktor <viktor.sbruev.1999@gmail.com>
2025-04-11 22:36:42 +0300

    Modified task 70

--- 70_climbing_stairs.py
+++ 70_climbing_stairs.py
@@ -3,27 +3,33 @@ You are climbing a staircase. It takes n steps to reach the top.
 Each time, you can either climb 1 step or 2 steps. In how many distinct ways can you climb to the top?
 
 Examples:
-Input:
-n = 2
-Output:
-2
+Input: n = 2
+Output: 2
 
-Input:
-n = 3
-Output:
-3
+Input: n = 3
+Output: 3
+
+Вы поднимаетесь по лестнице, состоящей из n ступеней.
+За один шаг вы можете подняться либо на 1 ступень, либо на 2 ступени.
+Сколько существует различных способов добраться до вершины лестницы?
+
+Примеры:
+Вход: n = 2
+Выход: 2
+
+Вход: n = 3
+Выход: 3
 '''
 
-class Solution:
-    def climbStairs(self, n: int) -> int:
-        if n == 1:
-            return 1
+def climb_stairs(n: int) -> int:
+    if n == 1:
+        return 1
 
-        prev2, prev1 = 1, 1
+    previous_2, previous_1 = 1, 1
 
-        for i in range(2, n + 1):
-            current = prev1 + prev2
-            prev2 = prev1
-            prev1 = current
+    for i in range(2, n + 1):
+        current = previous_1 + previous_2
+        previous_2 = previous_1
+        previous_1 = current
 
-        return prev1
+    return previous_1

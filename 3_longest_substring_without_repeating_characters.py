67fd061bda897f046883bed888dc66d1002613d9
Viktor <viktor.sbruev.1999@gmail.com>
2025-04-11 13:02:50 +0300

    Modified task 3

--- 3_longest_substring_without_repeating_characters.py
+++ 3_longest_substring_without_repeating_characters.py
@@ -1,29 +1,41 @@
 '''
-You are given a string s. Your task is to find the length of the longest substring that contains no duplicate characters.
+You are given a string S. Your task is to find the length of the longest substring that contains no duplicate characters.
 A substring is a contiguous sequence of characters within the string.
 
 Examples:
-Input: s = "abcabcbb"
+Input: S = "abcabcbb"
 Output: 3
 
-Input: s = "bbbbb"
+Input: S = "bbbbb"
 Output: 1
 
-Input: s = "pwwkew"
+Input: S = "pwwkew"
 Output: 3
+
+Дана строка S. Ваша задача — найти длину самой длинной подстроки, не содержащей повторяющихся символов.
+Подстрока — это непрерывная последовательность символов внутри строки.
+
+Примеры:
+Ввод: S = "abcabcbb"
+Вывод: 3
+
+Ввод: S = "bbbbb"
+Вывод: 1
+
+Ввод: S = "pwwkew"
+Вывод: 3
 '''
 
-class Solution:
-    def lengthOfLongestSubstring(self, s: str) -> int:
-        char_set = set()
+def length_of_longest_substring(S: str) -> int:
+        character_set = set()
         left = 0
         max_length = 0
 
-        for right in range(len(s)):
-            while s[right] in char_set:
-                char_set.remove(s[left])
+        for right in range(len(S)):
+            while S[right] in character_set:
+                character_set.remove(S[left])
                 left += 1
-            char_set.add(s[right])
+            character_set.add(S[right])
             max_length = max(max_length, right - left + 1)
 
         return max_length

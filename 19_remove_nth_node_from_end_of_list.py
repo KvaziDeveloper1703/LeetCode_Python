b6cc81b0f071e47a07eea743add9de683e08ee62
Viktor <viktor.sbruev.1999@gmail.com>
2025-04-11 14:47:20 +0300

    Modified task 19

--- 19_remove_nth_node_from_end_of_list.py
+++ 19_remove_nth_node_from_end_of_list.py
@@ -2,31 +2,46 @@
 Given the head of a linked list, remove the nth node from the end of the list and return its head.
 
 Examples:
-Input:
-head = [1,2,3,4,5], n = 2
-Output:
-[1,2,3,5]
+Input: head = [1,2,3,4,5], n = 2
+Output: [1,2,3,5]
 
-Input:
-head = [1], n = 1
-Output:
-[]
+Input: head = [1], n = 1
+Output: []
+
+Дан указатель head на начало связного списка. Удалите n-й узел с конца списка и верните новый head.
+
+Примеры:
+Вход: head = [1, 2, 3, 4, 5], n = 2
+Выход: [1, 2, 3, 5]
+
+Вход: head = [1], n = 1
+Выход: []
 '''
 
-class Solution:
-    def removeNthFromEnd(self, head: ListNode, n: int) -> ListNode:
-        dummy = ListNode(0)
-        dummy.next = head
-        slow = dummy
-        fast = dummy
+from typing import Optional
 
-        for _ in range(n + 1):
+class ListNode:
+    def __init__(self, value: int = 0, next: Optional['ListNode'] = None):
+        self.value = value
+        self.next = next
+
+def remove_nth_from_end(head: ListNode, n: int) -> Optional[ListNode]:
+    dummy = ListNode(0)
+    dummy.next = head
+    slow = dummy
+    fast = dummy
+
+    for _ in range(n + 1):
+        if fast:
             fast = fast.next
+        else:
+            return head
 
-        while fast:
-            slow = slow.next
-            fast = fast.next
+    while fast:
+        slow = slow.next
+        fast = fast.next
 
+    if slow.next:
         slow.next = slow.next.next
 
-        return dummy.next
+    return dummy.next

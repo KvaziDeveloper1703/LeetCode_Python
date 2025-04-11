6c72c9c77e8e20106d27b312563772e968547b15
Viktor <viktor.sbruev.1999@gmail.com>
2025-04-11 20:35:26 +0300

    Modified task 56

--- 56_merge_intervals.py
+++ 56_merge_intervals.py
@@ -1,30 +1,39 @@
 '''
-Given an array of intervals where intervals[i] = [starti, endi], merge all overlapping intervals, and return an array of the non-overlapping intervals that cover all the intervals in the input.
+Given an array of intervals where intervals[i] = [start_index, end_index], merge all overlapping intervals, and return an array of the non-overlapping intervals that cover all the intervals in the input.
 
-Example 1:
+Examples:
 Input: intervals = [[1,3],[2,6],[8,10],[15,18]]
 Output: [[1,6],[8,10],[15,18]]
 
-Example 2:
 Input: intervals = [[1,4],[4,5]]
 Output: [[1,5]]
+
+Дан массив интервалов, где intervals[i] = [начало, конец]. Необходимо объединить все перекрывающиеся интервалы и вернуть массив неперекрывающихся интервалов, которые полностью покрывают все интервалы из исходного массива.
+
+Примеры:
+Ввод: intervals = [[1, 3], [2, 6], [8, 10], [15, 18]]
+Вывод: [[1, 6], [8, 10], [15, 18]]
+
+Ввод: intervals = [[1, 4], [4, 5]]
+Вывод: [[1, 5]]
 '''
 
-class Solution:
-    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
-        if not intervals:
-            return []
+from typing import List
 
-        intervals.sort(key=lambda x: x[0])
+def merge(intervals: List[List[int]]) -> List[List[int]]:
+    if not intervals:
+        return []
 
-        merged = [intervals[0]]
+    intervals.sort(key=lambda x: x[0])
 
-        for current in intervals[1:]:
-            last = merged[-1]
+    merged = [intervals[0]]
 
-            if current[0] <= last[1]:
-                last[1] = max(last[1], current[1])
-            else:
-                merged.append(current)
+    for current in intervals[1:]:
+        last = merged[-1]
 
-        return merged
+        if current[0] <= last[1]:
+            last[1] = max(last[1], current[1])
+        else:
+            merged.append(current)
+
+    return merged

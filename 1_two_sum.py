e3937d56f3912082e31a79977d88eeac5c76d809
Viktor <viktor.sbruev.1999@gmail.com>
2025-04-11 12:50:26 +0300

    Modified task 1

--- 1_two_sum.py
+++ 1_two_sum.py
@@ -3,29 +3,42 @@ Given an array of integers nums and an integer target, return the indices of the
 You may assume that each input has exactly one solution, and you may not use the same element twice.
 You can return the answer in any order.
 
-Example 1:
+Examples:
 Input: nums = [2,7,11,15], target = 9
 Output: [0,1]
 
-Example 2:
 Input: nums = [3,2,4], target = 6
 Output: [1,2]
 
-Example 3:
 Input: nums = [3,3], target = 6
 Output: [0,1]
+
+Дан массив целых чисел nums и целевое число target. Необходимо вернуть индексы двух чисел из массива, сумма которых равна target.
+Можно предположить, что для каждого входного набора существует ровно одно решение, и одно и то же число нельзя использовать дважды.
+Ответ можно вернуть в любом порядке.
+
+Примеры:
+Ввод: nums = [2, 7, 11, 15], target = 9
+Вывод: [0, 1]
+
+Ввод: nums = [3, 2, 4], target = 6
+Вывод: [1, 2]
+
+Ввод: nums = [3, 3], target = 6
+Вывод: [0, 1]
 '''
 
-class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
-        num_dict = {}
+from typing import List
 
-        for i, num in enumerate(nums):
-            complement = target - num
+def two_sum(given_numbers: List[int], given_target: int) -> List[int]:
+        numbers_dictionary = {}
 
-            if complement in num_dict:
-                return [num_dict[complement], i]
+        for i, number in enumerate(given_numbers):
+            complement = given_target - number
 
-            num_dict[num] = i
+            if complement in numbers_dictionary:
+                return [numbers_dictionary[complement], i]
+
+            numbers_dictionary[number] = i
 
         return []

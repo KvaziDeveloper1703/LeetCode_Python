7c7683e8befd969f59906b29e50c80638e211b97
Viktor <viktor.sbruev.1999@gmail.com>
2025-04-11 13:18:35 +0300

    Modified task 6

--- 6_zigzag_conversion.py
+++ 6_zigzag_conversion.py
@@ -1,26 +1,32 @@
 '''
-You are given a string s and an integer numRows. Write code to rearrange the characters of s in a zigzag pattern on numRows rows and then read the result line by line.
+You are given a string S and an integer number_of_rows. Write code to rearrange the characters of S in a zigzag pattern on number_of_rows rows and then read the result line by line.
 
-For example, if s = "PAYPALISHIRING" and numRows = 3, the zigzag pattern looks like this:
+For example, if S = "PAYPALISHIRING" and number_of_rows = 3, the zigzag pattern looks like this:
 P   A   H   N  
 A P L S I I G  
 Y   I   R
 Reading line by line, the output is: "PAHNAPLSIIGYIR"
+
+Дана строка S и целое число number_of_rows. Напишите код, который переставит символы строки S в зигзагообразный узор по заданному числу строк, а затем считает результат построчно.
+Например, если S = "PAYPALISHIRING" и number_of_rows = 3, зигзагообразный узор будет выглядеть так:
+P   A   H   N  
+A P L S I I G  
+Y   I   R
+Читая построчно, получим результат: "PAHNAPLSIIGYIR"
 '''
 
-class Solution:
-    def convert(self, s: str, numRows: int) -> str:
-        if numRows == 1 or numRows >= len(s):
-            return s
+def convert(S: str, number_of_rows: int) -> str:
+        if number_of_rows == 1 or number_of_rows >= len(S):
+            return S
 
-        rows = [''] * numRows
-        curr_row = 0
+        rows = [''] * number_of_rows
+        current_row = 0
         going_down = False
 
-        for char in s:
-            rows[curr_row] += char
-            if curr_row == 0 or curr_row == numRows - 1:
+        for character in S:
+            rows[current_row] += character
+            if current_row == 0 or current_row == number_of_rows - 1:
                 going_down = not going_down
-            curr_row += 1 if going_down else -1
+            current_row += 1 if going_down else -1
 
         return ''.join(rows)

78eb36d09953b994d0095875bfff5f0b5a9024c1
Viktor <viktor.sbruev.1999@gmail.com>
2025-04-11 20:32:05 +0300

    Modified task 55

--- 55_jump_game.py
+++ 55_jump_game.py
@@ -1,26 +1,27 @@
 '''
-You are given an integer array nums. You start at the first index of the array.
+You are given an integer array numbers. You start at the first index of the array.
 Each element in the array represents the maximum number of steps you can jump forward from that position.
 
 Return true if you can reach the last index, otherwise return false.
 
 Examples:
-Input: nums = [2, 3, 1, 1, 4]
+Input: numbers = [2, 3, 1, 1, 4]
 Output: true
 
-Input: nums = [3, 2, 1, 0, 4]
+Input: numbers = [3, 2, 1, 0, 4]
 Output: false
 '''
 
-class Solution:
-    def canJump(self, nums: List[int]) -> bool:
-        max_reach = 0
+from typing import List
 
-        for i, jump in enumerate(nums):
-            if i > max_reach:
-                return False
-            max_reach = max(max_reach, i + jump)
-            if max_reach >= len(nums) - 1:
-                return True
+def can_jump(numbers: List[int]) -> bool:
+    max_reach = 0
 
-        return True
+    for i, jump in enumerate(numbers):
+        if i > max_reach:
+            return False
+        max_reach = max(max_reach, i + jump)
+        if max_reach >= len(numbers) - 1:
+            return True
+
+    return True

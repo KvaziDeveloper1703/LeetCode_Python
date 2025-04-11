2ad773738beb1405e5268e1d473bf3bb6e37605e
Viktor <viktor.sbruev.1999@gmail.com>
2025-04-11 19:44:31 +0300

    Modified task 28

--- 28_find_the_index_of_the_first_occurrence_in_a_string.py
+++ 28_find_the_index_of_the_first_occurrence_in_a_string.py
@@ -8,17 +8,26 @@ Output: 0
 
 Input: haystack = "leetcode", needle = "leeto"
 Output: -1
+
+Даны две строки — needle и haystack. Верните индекс первого вхождения подстроки needle в строку haystack.
+Если needle не найден в haystack, верните -1.
+
+Примеры:
+Ввод: haystack = "sadbutsad", needle = "sad"
+Вывод: 0
+
+Ввод: haystack = "leetcode", needle = "leeto"
+Вывод: -1
 '''
 
-class Solution:
-    def strStr(self, haystack: str, needle: str) -> int:
-        n, m = len(haystack), len(needle)
+def strStr(haystack: str, needle: str) -> int:
+    n, m = len(haystack), len(needle)
 
-        if m == 0:
-            return 0
+    if m == 0:
+        return 0
 
-        for i in range(n - m + 1):
-            if haystack[i:i + m] == needle:
-                return i
+    for i in range(n - m + 1):
+        if haystack[i:i + m] == needle:
+            return i
 
-        return -1
+    return -1

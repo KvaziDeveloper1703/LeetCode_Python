9638b9a9f9b44908ef1c84ba163f379c7da4b8d6
Viktor <viktor.sbruev.1999@gmail.com>
2025-04-11 19:37:48 +0300

    Modified task 26

--- 26_remove_duplicates_from_sorted_array.py
+++ 26_remove_duplicates_from_sorted_array.py
@@ -1,10 +1,10 @@
 '''
-You are given a sorted integer array nums in non-decreasing order. Your task is to remove duplicates in-place so that each unique element appears only once, and the relative order of the elements is preserved.
+You are given a sorted integer array numbers in non-decreasing order. Your task is to remove duplicates in-place so that each unique element appears only once, and the relative order of the elements is preserved.
 After removing the duplicates, return the number of unique elements, denoted as k.
 
 Requirements:
-+ Modify the array nums in-place so that the first k elements contain the unique elements in their original order.
-+ The remaining elements beyond index k - 1 do not matter — you can leave them as-is.
++ Modify the array nums in-place so that the first k elements contain the unique elements in their original order;
++ The remaining elements beyond index k - 1 do not matter — you can leave them as-is;
 + Return the value of k.
 
 Examples:
@@ -13,18 +13,34 @@ Output: 2, nums = [1, 2, _]
 
 Input: nums = [0, 0, 1, 1, 1, 2, 2, 3, 3, 4]
 Output: 5, nums = [0, 1, 2, 3, 4, _, _, _, _, _]
+
+Дан отсортированный по неубыванию массив целых чисел nums. Ваша задача — удалить дубликаты на месте, чтобы каждый уникальный элемент встречался только один раз, при этом сохраняя относительный порядок элементов.
+После удаления дубликатов верните количество уникальных элементов — обозначим его как k.
+
+Требования:
++ Измените массив nums на месте так, чтобы первые k элементов содержали уникальные значения в исходном порядке;
++ Остальная часть массива (начиная с индекса k) может содержать любые значения — она не имеет значения;
++ Верните значение k.
+
+Примеры:
+Ввод: nums = [1, 1, 2]
+Вывод: 2, nums = [1, 2, _]
+
+Ввод: nums = [0, 0, 1, 1, 1, 2, 2, 3, 3, 4]
+Вывод: 5, nums = [0, 1, 2, 3, 4, _, _, _, _, _]
 '''
 
-class Solution:
-    def removeDuplicates(self, nums: List[int]) -> int:
-        if not nums:
-            return 0
+from typing import List
 
-        write_index = 1
+def remove_duplicates(numbers: List[int]) -> int:
+    if not numbers:
+        return 0
 
-        for read_index in range(1, len(nums)):
-            if nums[read_index] != nums[write_index - 1]:
-                nums[write_index] = nums[read_index]
-                write_index += 1
+    write_index = 1
 
-        return write_index
+    for read_index in range(1, len(numbers)):
+        if numbers[read_index] != numbers[write_index - 1]:
+            numbers[write_index] = numbers[read_index]
+            write_index += 1
+
+    return write_index

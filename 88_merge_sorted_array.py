43186d7a5b9c5715d81bae5b0dece91e69cbcffc
Viktor <viktor.sbruev.1999@gmail.com>
2025-04-12 17:42:01 +0300

    Modified 88

--- 88_merge_sorted_array.py
+++ 88_merge_sorted_array.py
@@ -1,47 +1,34 @@
 '''
-You are given two integer arrays, nums1 and nums2, both sorted in non-decreasing order, and two integers, m and n, representing the number of valid elements in nums1 and nums2, respectively.
-Your task is to merge nums2 into nums1 as one sorted array in-place.
-+ nums1 has a length of m + n, where:
-    + The first m elements contain the elements to be merged.
+You are given two integer arrays, numbers_1 and numbers_2, both sorted in non-decreasing order, and two integers, m and n, representing the number of valid elements in numbers_1 and numbers_2, respectively.
+Your task is to merge numbers_2 into numbers_1 as one sorted array in-place.
++ numbers_1 has a length of m + n, where:
+    + The first m elements contain the elements to be merged;
     + The last n elements are set to 0 and should be ignored.
-+ nums2 has exactly n elements.
++ number_s2 has exactly n elements.
 
 Requirements:
-+ Merge nums2 into nums1, so that nums1 becomes a sorted array of size m + n.
-+ Do not return the merged array — modify nums1 in-place.
++ Merge numbers_2 into numbers_1, so that numbers_1 becomes a sorted array of size m + n.
++ Do not return the merged array — modify numbers_1 in-place.
 
 Examples:
-Input:
-nums1 = [1,2,3,0,0,0], m = 3
-nums2 = [2,5,6], n = 3
-Output:
-nums1 = [1,2,2,3,5,6]
-
-Input:
-nums1 = [1], m = 1
-nums2 = [], n = 0
-Output:
-nums1 = [1]
-
-Example 3
-Input:
-nums1 = [0], m = 0
-nums2 = [1], n = 1
-Output:
-nums1 = [1]
+Input: numbers_1 = [1,2,3,0,0,0], m = 3, numbers_2 = [2,5,6], n = 3
+Output: numbers_1 = [1,2,2,3,5,6]
 '''
 
-class Solution:
-    def merge(self, nums1: List[int], n: int, nums2: List[int], m: int) -> None:
-        index_nums1 = n - 1
-        index_nums2 = m - 1
-        merge_position = n + m - 1
+from typing import List
 
-        while index_nums2 >= 0:
-            if index_nums1 >= 0 and nums1[index_nums1] > nums2[index_nums2]:
-                nums1[merge_position] = nums1[index_nums1]
-                index_nums1 -= 1
-            else:
-                nums1[merge_position] = nums2[index_nums2]
-                index_nums2 -= 1
-            merge_position -= 1
+def merge(numbers_1: List[int], n: int, numbers_2: List[int], m: int) -> None:
+    index_numbers_1 = n - 1
+    index_numbers_2 = m - 1
+    merge_position = n + m - 1
+
+    while index_numbers_2 >= 0:
+        if index_numbers_1 >= 0 and numbers_1[index_numbers_1] > numbers_2[index_numbers_2]:
+            numbers_1[merge_position] = numbers_1[index_numbers_1]
+            index_numbers_1 -= 1
+        else:
+            numbers_1[merge_position] = numbers_2[index_numbers_2]
+            index_numbers_2 -= 1
+        merge_position -= 1
+
+    return None

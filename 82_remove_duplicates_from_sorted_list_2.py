4c4dfbd721c86ea5ae3082b6b09eaa65e418c764
Viktor <viktor.sbruev.1999@gmail.com>
2025-04-12 17:23:45 +0300

    Modified 82

--- 82_remove_duplicates_from_sorted_list_2.py
+++ 82_remove_duplicates_from_sorted_list_2.py
@@ -1,32 +1,38 @@
 '''
-Given the head of a sorted linked list, delete all nodes that have duplicate numbers, leaving only distinct numbers from the original list. Return the linked list sorted as well.
+Given the head of a sorted linked list, delete all nodes that have duplicate numbers, leaving only distinct numbers from the original list. 
+Return the linked list sorted as well.
 
-Examples:
-Input:
-head = [1,2,3,3,4,4,5]
-Output:
-[1,2,5]
+Example:
+Input: head = [1,2,3,3,4,4,5]
+Output: [1,2,5]
 
-Input:
-head = [1,1,1,2,3]
-Output:
-[2,3]
+Дан указатель head на начало отсортированного связного списка. Необходимо удалить все узлы, содержащие повторяющиеся значения, оставив в списке только уникальные числа, которые встречаются только один раз в исходном списке.
+Верните отсортированный связный список после удаления дубликатов.
+
+Пример:
+Вход: head = [1, 2, 3, 3, 4, 4, 5]
+Выход: [1, 2, 5]
 '''
 
-class Solution:
-    def deleteDuplicates(self, head: ListNode) -> ListNode:
-        dummy = ListNode(0)
-        dummy.next = head
-        prev = dummy
+from typing import Optional
 
-        while head:
-            if head.next and head.val == head.next.val:
-                while head.next and head.val == head.next.val:
-                    head = head.next
-                prev.next = head.next
-            else:
-                prev = prev.next
+class ListNode:
+    def __init__(self, value: int = 0, next: Optional['ListNode'] = None):
+        self.value = value
+        self.next = next
 
-            head = head.next
+def delete_duplicates(head: Optional[ListNode]) -> Optional[ListNode]:
+    dummy = ListNode(0)
+    dummy.next = head
+    previous = dummy
 
-        return dummy.next
+    while head:
+        if head.next and head.value == head.next.value:
+            while head.next and head.value == head.next.value:
+                head = head.next
+            previous.next = head.next
+        else:
+            previous = previous.next
+        head = head.next
+
+    return dummy.next

e8a45aaae1b9fa70650a714a8d6eda6c0b48c812
Viktor <viktor.sbruev.1999@gmail.com>
2025-04-12 18:14:02 +0300

    Modified 135

--- 135_candy.py
+++ 135_candy.py
@@ -1,7 +1,7 @@
 '''
-There are n children standing in a line, and each child has a rating represented by the integer array ratings.
+There are N children standing in a line, and each child has a rating represented by the integer array ratings.
 You need to distribute candies to these children based on the following rules:
-+ Every child must receive at least one candy.
++ Every child must receive at least one candy;
 + A child with a higher rating than an immediate neighbor must receive more candies than that neighbor.
 
 Your task is to determine the minimum number of candies needed to distribute to all the children while satisfying these rules.
@@ -12,19 +12,34 @@ Output: 5
 
 Input: ratings = [1, 2, 2]
 Output: 4
+
+Есть N детей, стоящих в ряд, и у каждого ребёнка есть рейтинг, представленный массивом целых чисел ratings.
+Вам нужно раздать конфеты детям в соответствии со следующими правилами:
++ Каждый ребёнок должен получить как минимум одну конфету;
++ Ребёнок с более высоким рейтингом, чем его сосед, должен получить больше конфет, чем этот сосед.
+
+Ваша задача — определить минимальное количество конфет, которое нужно раздать всем детям, чтобы соблюсти указанные правила.
+
+Примеры:
+Вход: ratings = [1, 0, 2]
+Выход: 5
+
+Вход: ratings = [1, 2, 2]
+Выход: 4
 '''
 
-class Solution:
-    def candy(self, ratings: List[int]) -> int:
-        n = len(ratings)
-        candies = [1] * n
+from typing import List
 
-        for i in range(1, n):
-            if ratings[i] > ratings[i - 1]:
-                candies[i] = candies[i - 1] + 1
+def distribute_candies(ratings: List[int]) -> int:
+    number_of_children = len(ratings)
+    candies = [1] * number_of_children
 
-        for i in range(n - 2, -1, -1):
-            if ratings[i] > ratings[i + 1]:
-                candies[i] = max(candies[i], candies[i + 1] + 1)
+    for i in range(1, number_of_children):
+        if ratings[i] > ratings[i - 1]:
+            candies[i] = candies[i - 1] + 1
 
-        return sum(candies)
+    for i in range(number_of_children - 2, -1, -1):
+        if ratings[i] > ratings[i + 1]:
+            candies[i] = max(candies[i], candies[i + 1] + 1)
+
+    return sum(candies)

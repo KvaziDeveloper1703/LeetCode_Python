523c817000a0c6d0f551ee140b645c46d5546093
Viktor <viktor.sbruev.1999@gmail.com>
2025-04-12 18:28:23 +0300

    Modified 141

--- 141_linked_list_cycle.py
+++ 141_linked_list_cycle.py
@@ -1,30 +1,39 @@
 '''
 Given head, the head of a linked list, determine if the linked list has a cycle.
-A cycle exists in the linked list if there is a node that can be revisited by continuously following the next pointer. The variable pos denotes the index of the node that the tail's next pointer is connected to. Note that pos is not passed as a parameter.
+A cycle exists in the linked list if there is a node that can be revisited by continuously following the next pointer. 
+The variable position denotes the index of the node that the tail's next pointer is connected to. Note that pos is not passed as a parameter.
 Return true if there is a cycle in the linked list, otherwise return false.
 
-Examples:
-Input:
-head = [3,2,0,-4], pos = 1
-Output:
-true
+Example:
+Input: head = [3,2,0,-4], position = 1
+Output: true
 
-Input:
-head = [1,2], pos = 0
-Output:
-true
+Дан указатель head на начало связного списка. Необходимо определить, содержит ли список цикл.
+Цикл существует, если в списке есть такой узел, к которому можно вернуться, непрерывно переходя по указателю next.
+Переменная position указывает индекс узла, на который ссылается next последнего элемента списка. Обратите внимание, что pos не передаётся как параметр — он используется только для пояснения примера.
+Верните true, если в связном списке есть цикл, и false — если цикла нет.
+
+Пример:
+Вход: head = [3, 2, 0, -4], position = 1
+Выход: true
 '''
 
-class Solution:
-    def hasCycle(self, head: Optional[ListNode]) -> bool:
-        slow = head
-        fast = head
+from typing import Optional
 
-        while fast and fast.next:
-            slow = slow.next
-            fast = fast.next.next
+class ListNode:
+    def __init__(self, value: int = 0, next: Optional['ListNode'] = None):
+        self.value = value
+        self.next = next
 
-            if slow == fast:
-                return True
+def has_cycle(head: Optional[ListNode]) -> bool:
+    slow_pointer = head
+    fast_pointer = head
 
-        return False
+    while fast_pointer and fast_pointer.next:
+        slow_pointer = slow_pointer.next
+        fast_pointer = fast_pointer.next.next
+
+        if slow_pointer == fast_pointer:
+            return True
+
+    return False

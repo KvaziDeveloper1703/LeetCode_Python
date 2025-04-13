e4cc6e7cd106d3a39a001b86c80d30a9c043d333
Viktor <viktor.sbruev.1999@gmail.com>
2025-04-13 11:14:06 +0300

    Modified 290

--- 290_word_pattern.py
+++ 290_word_pattern.py
@@ -1,43 +1,57 @@
 '''
-You are given a pattern (a string of lowercase letters) and a string s consisting of words separated by spaces.
-Determine if s follows the same pattern.
-For s to follow the pattern, there must be a bijection between:
-+ Each character in pattern, and
-+ Each non-empty word in s.
+You are given a pattern and a string S consisting of words separated by spaces.
+Determine if S follows the same pattern.
+For S to follow the pattern, there must be a bijection between: each character in pattern, and each non-empty word in S.
 
 This means:
-+ Each letter in pattern maps to exactly one unique word in s.
-+ Each word in s maps to exactly one unique letter in pattern.
++ Each letter in pattern maps to exactly one unique word in S;
++ Each word in S maps to exactly one unique letter in pattern;
 + No two letters can map to the same word, and no two words can map to the same letter.
 
 Examples:
-Input: pattern = "abba", s = "dog cat cat dog"
+Input: pattern = "abba", S = "dog cat cat dog"
 Output: true
 
-Input: pattern = "abba", s = "dog cat cat fish"
+Input: pattern = "abba", S = "dog cat cat fish"
 Output: false
+
+Даны строка pattern и строка S, состоящая из слов, разделённых пробелами.
+Необходимо определить, соответствует ли строка S шаблону pattern.
+Чтобы S соответствовала шаблону, должна быть выполнена биекция между: каждым символом в pattern, и каждым непустым словом в S.
+
+Это означает:
++ Каждая буква из pattern должна отображаться в однозначное уникальное слово из S;
++ Каждое слово из S должно отображаться в однозначную уникальную букву из pattern;
++ Две разные буквы не могут соответствовать одному и тому же слову, и два разных слова не могут соответствовать одной и той же букве.
+
+Примеры:
+Ввод: pattern = "abba", S = "dog cat cat dog"
+Вывод: true
+
+Ввод: pattern = "abba", S = "dog cat cat fish"
+Вывод: false
 '''
 
-class Solution:
-    def wordPattern(self, pattern: str, s: str) -> bool:
-        words = s.split()
-        if len(pattern) != len(words):
-            return False
+def word_pattern(pattern: str, S: str) -> bool:
+    word_list = S.split()
 
-        char_to_word = {}
-        word_to_char = {}
+    if len(pattern) != len(word_list):
+        return False
 
-        for p, w in zip(pattern, words):
-            if p in char_to_word:
-                if char_to_word[p] != w:
-                    return False
-            else:
-                char_to_word[p] = w
+    pattern_char_to_word = {}
+    word_to_pattern_char = {}
 
-            if w in word_to_char:
-                if word_to_char[w] != p:
-                    return False
-            else:
-                word_to_char[w] = p
+    for pattern_char, word in zip(pattern, word_list):
+        if pattern_char in pattern_char_to_word:
+            if pattern_char_to_word[pattern_char] != word:
+                return False
+        else:
+            pattern_char_to_word[pattern_char] = word
 
-        return True
+        if word in word_to_pattern_char:
+            if word_to_pattern_char[word] != pattern_char:
+                return False
+        else:
+            word_to_pattern_char[word] = pattern_char
+
+    return True

cf7f26730039ff9e176828fd0a51bf60c23fd2d5
Viktor <viktor.sbruev.1999@gmail.com>
2025-04-13 14:09:34 +0300

    Modified 392

--- 392_is_subsequence.py
+++ 392_is_subsequence.py
@@ -1,17 +1,32 @@
 '''
-A subsequence of a string is a sequence that can be formed by deleting some (possibly none) of the characters from the original string, without changing the order of the remaining characters.
-Given two strings s and t, return true if s is a subsequence of t, and false otherwise.
+A subsequence of a string is a sequence that can be formed by deleting some of the characters from the original string, without changing the order of the remaining characters.
+Given two strings S and T, return true if S is a subsequence of T, and false otherwise.
 
 Examples:
-Input: s = "abc", t = "ahbgdc" → Output: true
-Input: s = "axc", t = "ahbgdc" → Output: false
+Input: S = "abc", T = "ahbgdc"
+Output: true
+
+Input: S = "axc", T = "ahbgdc"
+Output: false
+
+Подпоследовательность строки — это последовательность, которую можно получить, удалив некоторые символы из исходной строки, не меняя порядок оставшихся символов.
+Даны две строки S и T. Верните true, если S является подпоследовательностью строки T, и false — в противном случае.
+
+Примеры:
+Ввод: S = "abc", T = "ahbgdc"
+Вывод: true
+
+Ввод: S = "axc", T = "ahbgdc"
+Вывод: false
 '''
 
-class Solution:
-    def isSubsequence(self, s: str, t: str) -> bool:
-        i, j = 0, 0
-        while i < len(s) and j < len(t):
-            if s[i] == t[j]:
-                i += 1
-            j += 1
-        return i == len(s)
+def is_subsequence(S: str, T: str) -> bool:
+    subsequence_index = 0
+    target_index = 0
+
+    while subsequence_index < len(S) and target_index < len(T):
+        if S[subsequence_index] == T[target_index]:
+            subsequence_index += 1
+        target_index += 1
+
+    return subsequence_index == len(S)

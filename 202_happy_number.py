89efcaee367b183de6e9b832f5b0420e49e52575
Viktor <viktor.sbruev.1999@gmail.com>
2025-04-13 10:27:18 +0300

    Modified 202

--- 202_happy_number.py
+++ 202_happy_number.py
@@ -1,34 +1,48 @@
 '''
 Write an algorithm to determine if a number n is a happy number.
 A happy number is defined by the following process:
-+ Starting with any positive integer, replace the number by the sum of the squares of its digits.
-+ Repeat the process until the number equals 1 (where it will stay), or it loops endlessly in a cycle which does not include 1.
++ Starting with any positive integer, replace the number by the sum of the squares of its digits;
++ Repeat the process until the number equals 1, or it loops endlessly in a cycle which does not include 1;
 + Numbers that end in 1 are happy, while numbers that fall into a cycle (not including 1) are not.
 
 Return true if n is a happy number, and false if not.
 
-Example 1:
+Examples:
 Input: n = 19
 Output: true
 
-Example 2:
 Input: n = 2
 Output: false
+
+Напишите алгоритм, который определяет, является ли число n счастливым числом.
+Счастливое число определяется следующим образом:
++ Начав с любого положительного целого числа, заменяйте его суммой квадратов его цифр;
++ Повторяйте процесс до тех пор, пока число не станет равным 1, или пока процесс не начнёт бесконечно повторяться в цикле, который не содержит 1;
++ Числа, которые в итоге становятся 1, считаются счастливыми;
++ Числа, попадающие в цикл без единицы — не являются счастливыми.
+
+Верните true, если n — счастливое число, и false — если нет.
+
+Примеры:
+Ввод: n = 19
+Вывод: true
+
+Ввод: n = 2
+Вывод: false
 '''
 
-class Solution:
-    def isHappy(self, n: int) -> bool:
-        def get_sum_of_squares(num: int) -> int:
-            return sum(int(digit) ** 2 for digit in str(num))
+def is_happy(number: int) -> bool:
+    def get_sum_of_squares(num: int) -> int:
+        return sum(int(digit) ** 2 for digit in str(num))
 
-        seen_numbers = set()
+    seen_numbers = set()
 
-        while n != 1:
-            n = get_sum_of_squares(n)
+    while number != 1:
+        number = get_sum_of_squares(number)
 
-            if n in seen_numbers:
-                return False
+        if number in seen_numbers:
+            return False
 
-            seen_numbers.add(n)
+        seen_numbers.add(number)
 
-        return True
+    return True

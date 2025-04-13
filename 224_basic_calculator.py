823a50eef9923c88ee605c18daa1f1e123bbb1d5
Viktor <viktor.sbruev.1999@gmail.com>
2025-04-13 10:42:42 +0300

    Modified 224

--- 224_basic_calculator.py
+++ 224_basic_calculator.py
@@ -1,53 +1,61 @@
 '''
-Given a string s representing a valid mathematical expression, implement a basic calculator to evaluate it and return the result. You are not allowed to use built-in functions like eval().
+Given a string S representing a valid mathematical expression, implement a basic calculator to evaluate it and return the result. 
+You are not allowed to use built-in functions like eval().
 
 Examples:
 Input: "1 + 1"
 Output: 2
 
-Input: " 2-1 + 2 "
+Input: "2 - 1 + 2"
 Output: 3
 
-Input: "(1+(4+5+2)-3)+(6+8)"
-Output: 23
+Дана строка S, представляющая корректное математическое выражение.
+Необходимо реализовать простой калькулятор, который вычисляет значение этого выражения и возвращает результат.
+Нельзя использовать встроенные функции, такие как eval().
+
+Примеры:
+Ввод: "1 + 1"
+Вывод: 2
+
+Ввод: "2 - 1 + 2"
+Вывод: 3
 '''
 
-class Solution:
-    def calculate(self, s: str) -> int:
-        stack = []
-        current_number = 0
-        result = 0
-        sign = 1
-        
-        for i in range(len(s)):
-            char = s[i]
-            
-            if char.isdigit():
-                current_number = current_number * 10 + int(char)
-            
-            elif char == "+":
-                result += sign * current_number
-                current_number = 0
-                sign = 1
-                
-            elif char == "-":
-                result += sign * current_number
-                current_number = 0
-                sign = -1
-                
-            elif char == "(":
-                stack.append(result)
-                stack.append(sign)
-                result = 0
-                sign = 1
-                
-            elif char == ")":
-                result += sign * current_number
-                current_number = 0
-                
-                result *= stack.pop()
-                result += stack.pop()
-        
-        result += sign * current_number
-        
-        return result
+def calculate(expression: str) -> int:
+    stack = []
+    current_number = 0
+    result = 0
+    sign = 1
+
+    for character in expression:
+        if character.isdigit():
+            current_number = current_number * 10 + int(character)
+
+        elif character == "+":
+            result += sign * current_number
+            current_number = 0
+            sign = 1
+
+        elif character == "-":
+            result += sign * current_number
+            current_number = 0
+            sign = -1
+
+        elif character == "(":
+            stack.append(result)
+            stack.append(sign)
+            result = 0
+            sign = 1
+
+        elif character == ")":
+            result += sign * current_number
+            current_number = 0
+
+            result *= stack.pop()
+            result += stack.pop()
+
+        elif character == " ":
+            continue
+
+    result += sign * current_number
+    return result

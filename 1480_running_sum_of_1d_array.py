ccb2594f2d8deb3b0ce07a8c1078ffb486cc78ef
Viktor <viktor.sbruev.1999@gmail.com>
2025-04-13 14:30:06 +0300

    Modified 1480

--- 1480_running_sum_of_1d_array.py
+++ 1480_running_sum_of_1d_array.py
@@ -1,25 +1,35 @@
 """
-Given an array of integers nums, return the running sum of the array.
+Given an array of integers numbers, return the running sum of the array.
 
 The running sum at index i is defined as the sum of all elements from index 0 to i, i.e.:
-runningSum[i] = nums[0] + nums[1] + ... + nums[i]
+running_sum[i] = numbers[0] + numbers[1] + ... + numbers[i]
 
 Examples:
-Input: nums = [1, 2, 3, 4]
+Input: numbers = [1, 2, 3, 4]
 Output: [1, 3, 6, 10]
 
-Input: nums = [1, 1, 1, 1, 1]
+Input: numbers = [1, 1, 1, 1, 1]
 Output: [1, 2, 3, 4, 5]
 
-Input: nums = [3, 1, 2, 10, 1]
-Output: [3, 4, 6, 16, 17]
+Дан массив целых чисел numbers. Необходимо вернуть префиксную сумму (running sum) этого массива.
+
+Префиксная сумма на индексе i — это сумма всех элементов от индекса 0 до i, то есть:
+running_sum[i] = numbers[0] + numbers[1] + ... + numbers[i]
+
+Примеры:
+Ввод: numbers = [1, 2, 3, 4]
+Вывод: [1, 3, 6, 10]
+
+Ввод: numbers = [1, 1, 1, 1, 1]
+Вывод: [1, 2, 3, 4, 5]
 """
 
-class Solution(object):
-    def runningSum(self, nums):
-        result = []
-        current_sum = 0
-        for number in nums:
-            current_sum += number
-            result.append(current_sum)
-        return result
+from typing import List
+
+def running_sum(numbers: List[int]) -> List[int]:
+    result = []
+    current_sum = 0
+    for number in numbers:
+        current_sum += number
+        result.append(current_sum)
+    return result

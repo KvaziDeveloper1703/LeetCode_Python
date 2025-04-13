6361b61fffcccab3fa7b6356bae80dde87aecabb
Viktor <viktor.sbruev.1999@gmail.com>
2025-04-13 14:06:46 +0300

    Modified 383

--- 383_ransom_note.py
+++ 383_ransom_note.py
@@ -1,25 +1,33 @@
 """
-You are given two strings: ransomNote and magazine. Return true if the ransomNote can be constructed using the letters from magazine, and false otherwise.
-Each letter in magazine can only be used once in ransomNote.
+You are given two strings: ransom_note and magazine. Return true if the ransom_note can be constructed using the letters from magazine, and false otherwise.
+Each letter in magazine can only be used once in ransom_note.
 
 Examples:
-Input: ransomNote = "a", magazine = "b"
+Input: ransom_note = "a", magazine = "b"
 Output: false
 
-Input: ransomNote = "aa", magazine = "ab"
+Input: ransom_note = "aa", magazine = "ab"
 Output: false
 
-Input: ransomNote = "aa", magazine = "aab"
-Output: true
+Даны две строки: ransom_note и magazine. Необходимо вернуть true, если строку ransom_note можно составить из букв строки magazine, и false — в противном случае.
+Каждую букву из строки magazine можно использовать только один раз при составлении ransom_note.
+
+Примеры:
+Ввод: ransom_note = "a", magazine = "b"
+Вывод: false
+
+Ввод: ransom_note = "aa", magazine = "ab"
+Вывод: false
 """
+
 from collections import Counter
 
-class Solution(object):
-    def canConstruct(self, ransomNote, magazine):
-        ransom_count = Counter(ransomNote)
-        magazine_count = Counter(magazine)
+def can_construct(ransom_note: str, magazine: str) -> bool:
+    ransom_note_character_counts = Counter(ransom_note)
+    magazine_character_counts = Counter(magazine)
 
-        for character, count in ransom_count.items():
-            if magazine_count[character] < count:
-                return False
-        return True
+    for character, required_count in ransom_note_character_counts.items():
+        if magazine_character_counts[character] < required_count:
+            return False
+
+    return True

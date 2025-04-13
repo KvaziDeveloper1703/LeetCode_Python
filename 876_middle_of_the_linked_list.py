97531d148cab77ad2aa8be5a4c94e33bd422cdab
Viktor <viktor.sbruev.1999@gmail.com>
2025-04-13 14:18:29 +0300

    Modified 876

--- 876_middle_of_the_linked_list.py
+++ 876_middle_of_the_linked_list.py
@@ -9,6 +9,17 @@ Output: [3, 4, 5]
 
 Input: head = [1, 2, 3, 4, 5, 6]
 Output: [4, 5, 6]
+
+Дан указатель head на начало односвязного списка. Необходимо вернуть средний узел списка.
+Если в списке нечётное количество узлов — верните средний элемент. Если количество узлов чётное — верните второй из двух средних.
+Нужно вернуть средний узел вместе со всеми следующими за ним узлами.
+
+Примеры:
+Ввод: head = [1, 2, 3, 4, 5]
+Вывод: [3, 4, 5]
+
+Ввод: head = [1, 2, 3, 4, 5, 6]
+Вывод: [4, 5, 6]
 """
 
 class ListNode(object):
@@ -16,10 +27,9 @@ class ListNode(object):
         self.value = value
         self.next = next
 
-class Solution(object):
-    def middleNode(self, head):
-        slow = fast = head
-        while fast and fast.next:
-            slow = slow.next
-            fast = fast.next.next
-        return slow
+def middle_node(head):
+    slow_pointer = fast_pointer = head
+    while fast_pointer and fast_pointer.next:
+        slow_pointer = slow_pointer.next
+        fast_pointer = fast_pointer.next.next
+    return slow_pointer

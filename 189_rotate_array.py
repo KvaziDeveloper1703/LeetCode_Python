1687e59cf9297ff02aaf389077e1d4fdd2eed978
Viktor <viktor.sbruev.1999@gmail.com>
2025-04-13 10:23:41 +0300

    Modified 189

--- 189_rotate_array.py
+++ 189_rotate_array.py
@@ -1,26 +1,39 @@
 '''
-Given an integer array nums, rotate the array to the right by k steps, where k is a non-negative integer.
+Given an integer array numbers, rotate the array to the right by k steps, where k is a non-negative integer.
 The rotation should be done in-place, meaning you should modify the input array directly without using extra space for another array.
 
 Examples:
-Input: nums = [1, 2, 3, 4, 5, 6, 7], k = 3
+Input: numbers = [1, 2, 3, 4, 5, 6, 7], k = 3
 Output: [5, 6, 7, 1, 2, 3, 4]
 
-Input: nums = [-1, -100, 3, 99], k = 2
+Input: numbers = [-1, -100, 3, 99], k = 2
 Output: [3, 99, -1, -100]
+
+Дан массив целых чисел numbers и неотрицательное число k. Необходимо сдвинуть элементы массива вправо на k позиций.
+Сдвиг должен быть выполнен на месте, то есть нельзя использовать дополнительный массив — нужно изменить исходный массив напрямую.
+
+Примеры:
+Вход: numbers = [1, 2, 3, 4, 5, 6, 7], k = 3
+Выход: [5, 6, 7, 1, 2, 3, 4]
+
+Вход: numbers = [-1, -100, 3, 99], k = 2
+Выход: [3, 99, -1, -100]
 '''
 
-class Solution:
-    def rotate(self, nums: List[int], k: int) -> None:
-        n = len(nums)
-        k = k % n
+from typing import List
 
-        def reverse_part(start: int, end: int):
-            while start < end:
-                nums[start], nums[end] = nums[end], nums[start]
-                start += 1
-                end -= 1
+def rotate(numbers: List[int], k: int) -> None:
+    n = len(numbers)
+    k = k % n
 
-        reverse_part(0, n - 1)
-        reverse_part(0, k - 1)
-        reverse_part(k, n - 1)
+    def reverse_part(start_index: int, end_index: int):
+        while start_index < end_index:
+            numbers[start_index], numbers[end_index] = numbers[end_index], numbers[start_index]
+            start_index += 1
+            end_index -= 1
+
+    reverse_part(0, n - 1)
+    reverse_part(0, k - 1)
+    reverse_part(k, n - 1)
+
+    return None

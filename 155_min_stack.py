cdf254d7f6c31dc6e7365eed8c8e7faf34c1c6c1
Viktor <viktor.sbruev.1999@gmail.com>
2025-04-13 10:13:53 +0300

    Modified 155

--- 155_min_stack.py
+++ 155_min_stack.py
@@ -1,21 +1,22 @@
 '''
 Design a stack that supports push, pop, top, and retrieving the minimum element in constant time.
 Implement the MinStack class:
-+ MinStack() initializes the stack object.
-+ void push(int val) pushes the element val onto the stack.
-+ void pop() removes the element on the top of the stack.
-+ int top() gets the top element of the stack.
-+ int getMin() retrieves the minimum element in the stack.
++ MinStack() initializes the stack object;
++ void push(int value) pushes the element value onto the stack;
++ void pop() removes the element on the top of the stack;
++ int top() gets the top element of the stack;
++ int get_min() retrieves the minimum element in the stack.
 
 You must implement a solution with O(1) time complexity for each function.
 
-Example:
-Input:
-["MinStack","push","push","push","getMin","pop","top","getMin"]
-[[],[-2],[0],[-3],[],[],[],[]]
+Реализуйте стек MinStack, поддерживающий следующие операции за O(1):
++ MinStack() — инициализирует объект стека;
++ void push(int value) — помещает элемент value в стек;
++ void pop() — удаляет элемент с вершины стека;
++ int top() — возвращает верхний элемент стека;
++ int get_min() — возвращает минимальный элемент в стеке.
 
-Output:
-[null,null,null,null,-3,null,0,-2]
+Все функции должны работать за O(1) времени.
 '''
 
 class MinStack:
@@ -24,14 +25,14 @@ class MinStack:
         self.stack = []
         self.min_stack = []
 
-    def push(self, val: int) -> None:
-        self.stack.append(val)
-        if not self.min_stack or val <= self.min_stack[-1]:
-            self.min_stack.append(val)
+    def push(self, value: int) -> None:
+        self.stack.append(value)
+        if not self.min_stack or value <= self.min_stack[-1]:
+            self.min_stack.append(value)
 
     def pop(self) -> None:
-        val = self.stack.pop()
-        if val == self.min_stack[-1]:
+        value = self.stack.pop()
+        if value == self.min_stack[-1]:
             self.min_stack.pop()
 
     def top(self) -> int:

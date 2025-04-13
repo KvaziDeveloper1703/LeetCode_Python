8599a85f04b9a5c38b5afde384273adc1338238e
Viktor <viktor.sbruev.1999@gmail.com>
2025-04-13 10:49:49 +0300

    Modified 238

--- 238_product_of_array_except_self.py
+++ 238_product_of_array_except_self.py
@@ -1,30 +1,43 @@
 '''
-Given an integer array nums, return a new array answer such that answer[i] is equal to the product of all elements in nums except nums[i].
-+ The result for each position must be calculated without using the division operation.
-+ The algorithm must run in O(n) time complexity.
+Given an integer array numbers, return a new array answer such that answer[i] is equal to the product of all elements in numbers except numbers[i].
++ The result for each position must be calculated without using the division operation;
++ The algorithm must run in O(n) time complexity;
 + It is guaranteed that the product of all prefix or suffix elements will fit within a 32-bit integer.
 
 Examples:
-Input: nums = [1, 2, 3, 4]
+Input: numbers = [1, 2, 3, 4]
 Output: [24, 12, 8, 6]
 
-Input: nums = [-1, 1, 0, -3, 3]
+Input: numbers = [-1, 1, 0, -3, 3]
 Output: [0, 0, 9, 0, 0]
+
+Дан массив целых чисел numbers. Необходимо вернуть новый массив answer, такой что answer[i] равен произведению всех элементов массива numbers, кроме numbers[i].
++ Нельзя использовать операцию деления при вычислениях;
++ Алгоритм должен работать за O(n) по времени;
++ Гарантируется, что произведения всех префиксов и суффиксов поместятся в 32-битное целое число.
+
+Примеры:
+Ввод: numbers = [1, 2, 3, 4]
+Вывод: [24, 12, 8, 6]
+
+Ввод: numbers = [-1, 1, 0, -3, 3]
+Вывод: [0, 0, 9, 0, 0]
 '''
 
-class Solution:
-    def productExceptSelf(self, nums: List[int]) -> List[int]:
-        N = len(nums)
-        answer = [1] * N
+from typing import List
 
-        left_product = 1
-        for i in range(N):
-            answer[i] = left_product
-            left_product *= nums[i]
+def product_except_self(numbers: List[int]) -> List[int]:
+    length = len(numbers)
+    result = [1] * length
 
-        right_product = 1
-        for i in range(N - 1, -1, -1):
-            answer[i] *= right_product
-            right_product *= nums[i]
+    left_product = 1
+    for i in range(length):
+        result[i] = left_product
+        left_product *= numbers[i]
 
-        return answer
+    right_product = 1
+    for i in range(length - 1, -1, -1):
+        result[i] *= right_product
+        right_product *= numbers[i]
+
+    return result

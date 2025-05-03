e20f765d6c5b271dcda234e58351988145c97925
Viktor <viktor.sbruev.1999@gmail.com>
2025-05-03 16:59:20 +0300

    Added task 53

---
+++ 53_maximum_subarray.py
@@ -0,0 +1,24 @@
+'''
+Given an integer array numbers, find a contiguous subarray (containing at least one number) which has the largest sum, and return that sum.
+
+Example:
+Input: numbers = [-2, 1, -3, 4, -1, 2, 1, -5, 4]
+Output: 6
+
+Дан целочисленный массив numbers. Найдите непрерывный подмассив (содержащий хотя бы один элемент) с наибольшей суммой, и верните эту сумму.
+
+Пример:
+Ввод: numbers = [-2, 1, -3, 4, -1, 2, 1, -5, 4]
+Вывод: 6
+'''
+
+from typing import List
+
+def max_sub_array(numbers: List[int]) -> int:
+    max_sum = current_sum = numbers[0]
+
+    for number in numbers[1:]:
+        current_sum = max(number, current_sum + number)
+        max_sum = max(max_sum, current_sum)
+
+    return max_sum
---
+++ 59_spiral_matrix_2.py
@@ -0,0 +1,20 @@
+'''
+Given a positive integer n, generate an n x n matrix filled with elements from 1 to n² in spiral order, starting from the top-left corner and moving clockwise.
+
+Example:
+Input: n = 3
+Output: [   [1, 2, 3],
+        [8, 9, 4],
+        [7, 6, 5]
+    ]
+
+Дано положительное целое число n. Сгенерируйте квадратную матрицу размером n x n, заполненную числами от 1 до n² в спиральном порядке, начиная с левого верхнего угла и двигаясь по часовой стрелке.
+
+Пример:
+Ввод: n = 3
+Вывод: [    [1, 2, 3],
+            [8, 9, 4],
+            [7, 6, 5]
+    ]
+'''
+

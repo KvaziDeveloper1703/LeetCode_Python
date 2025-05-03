7c9c9c1d825bcec65ca28d77ccd63847c6887aca
Viktor <viktor.sbruev.1999@gmail.com>
2025-05-03 17:08:26 +0300

    Added task 59

--- 59_spiral_matrix_2.py
+++ 59_spiral_matrix_2.py
@@ -18,3 +18,37 @@ Output: [   [1, 2, 3],
     ]
 '''
 
+from typing import List
+
+def generate_matrix(matrix_size: int) -> List[List[int]]:
+    spiral_matrix = [[0] * matrix_size for _ in range(matrix_size)]
+    current_value = 1
+    left_boundary = 0
+    right_boundary = matrix_size - 1
+    top_boundary = 0
+    bottom_boundary = matrix_size - 1
+
+    while left_boundary <= right_boundary and top_boundary <= bottom_boundary:
+        for column in range(left_boundary, right_boundary + 1):
+            spiral_matrix[top_boundary][column] = current_value
+            current_value += 1
+        top_boundary += 1
+
+        for row in range(top_boundary, bottom_boundary + 1):
+            spiral_matrix[row][right_boundary] = current_value
+            current_value += 1
+        right_boundary -= 1
+
+        if top_boundary <= bottom_boundary:
+            for column in range(right_boundary, left_boundary - 1, -1):
+                spiral_matrix[bottom_boundary][column] = current_value
+                current_value += 1
+            bottom_boundary -= 1
+
+        if left_boundary <= right_boundary:
+            for row in range(bottom_boundary, top_boundary - 1, -1):
+                spiral_matrix[row][left_boundary] = current_value
+                current_value += 1
+            left_boundary += 1
+
+    return spiral_matrix

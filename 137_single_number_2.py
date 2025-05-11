613c985efaedb559a4830902837b23e13f61d889
Viktor <viktor.sbruev.1999@gmail.com>
2025-05-11 17:27:47 +0300

    modified task 137

---
+++ 137_single_number_2.py
@@ -0,0 +1,30 @@
+'''
+Given an integer array numbers where every element appears exactly three times except for one element which appears exactly once, find and return that single one.
+You must implement a solution with linear runtime complexity (O(n)) and use only constant extra space (O(1)).
+
+Examples:
+Input: numbers = [2, 2, 3, 2]
+Output: 3
+
+Input: numbers = [0, 1, 0, 1, 0, 1, 99]
+Output: 99
+
+Дан массив целых чисел numbers, в котором каждый элемент встречается ровно три раза, кроме одного, который встречается один раз. Найдите и верните этот единственный элемент.
+Решение должно иметь линейную временную сложность (O(n)) и использовать только постоянное количество дополнительной памяти (O(1)).
+
+Примеры:
+Вход: numbers = [2, 2, 3, 2]
+Выход: 3
+
+Вход: numbers = [0, 1, 0, 1, 0, 1, 99]
+Выход: 99
+'''
+
+from typing import List
+
+def single_number(numbers: List[int]) -> int:
+    ones, twos = 0, 0
+    for number in numbers:
+        ones = (ones ^ number) & ~twos
+        twos = (twos ^ number) & ~ones
+    return ones

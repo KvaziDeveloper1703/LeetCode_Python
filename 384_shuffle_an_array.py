7cb2b1de40bbdd8befea5ad1b542f8b96031213c
Viktor <viktor.sbruev.1999@gmail.com>
2025-05-24 12:03:50 +0300

    Added task 384

---
+++ 384_shuffle_an_array.py
@@ -0,0 +1,40 @@
+'''
+You are given an integer array numbers. Design a class Solution to:
+    + Solution(int[] numbers): Initialize the object with the integer array numbers;
+    + int[] reset(): Reset the array to its original configuration and return it.
+    + int[] shuffle(): Return a random shuffling of the array. All permutations must be equally likely.
+
+Example:
+Input: ["Solution", "shuffle", "reset", "shuffle"], [[[1, 2, 3]], [], [], []]
+Output: [null, [3, 1, 2], [1, 2, 3], [1, 3, 2]]
+
+Вам дан массив целых чисел numbers. Необходимо реализовать класс Solution, который поддерживает следующие методы:
+    + Solution(int[] numbers): Конструктор, инициализирующий объект с массивом numbers;
+    + int[] reset(): Сбрасывает массив в исходную конфигурацию и возвращает его;
+    + int[] shuffle(): Возвращает случайную перестановку элементов массива. Все возможные перестановки должны быть равновероятными.
+
+Пример:
+Ввод: ["Solution", "shuffle", "reset", "shuffle"], [[[1, 2, 3]], [], [], []]
+Вывод: [null, [3, 1, 2], [1, 2, 3], [1, 3, 2]]
+'''
+
+import random
+from typing import List
+
+class Solution:
+
+    def __init__(self, numbers: List[int]):
+        self.original = numbers[:]
+        self.array = numbers[:]
+
+    def reset(self) -> List[int]:
+        self.array = self.original[:]
+        return self.array
+
+    def shuffle(self) -> List[int]:
+        shuffled = self.array[:]
+        n = len(shuffled)
+        for i in range(n):
+            j = random.randint(i, n - 1)
+            shuffled[i], shuffled[j] = shuffled[j], shuffled[i]
+        return shuffled

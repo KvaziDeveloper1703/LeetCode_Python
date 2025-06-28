67c00879cc0c9dafc7d2cf62b8e697ed424a84df
Viktor <viktor.sbruev.1999@gmail.com>
2025-06-28 14:11:50 +0300

    Added task 83

---
+++ 83_remove_duplicates_from_sorted_list.py
@@ -0,0 +1,41 @@
+'''
+Given the head of a sorted linked list, delete all duplicates such that each element appears only once.
+Return the linked list, which should remain sorted.
+
+Examples:
+Input:  head = [1,1,2]
+Output: [1,2]
+
+Input:  head = [1,1,2,3,3]
+Output: [1,2,3]
+
+Дан head — голова отсортированного связного списка.
+Удалите все дубликаты так, чтобы каждый элемент встречался только один раз.
+Верните связный список, который при этом должен остаться отсортированным.
+
+Примеры:
+Ввод:  head = [1,1,2]
+Вывод: [1,2]
+
+Ввод:  head = [1,1,2,3,3]
+Вывод: [1,2,3]
+'''
+
+from typing import Optional
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    def deleteDuplicates(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        current = head
+
+        while current and current.next:
+            if current.val == current.next.val:
+                current.next = current.next.next
+            else:
+                current = current.next
+
+        return head

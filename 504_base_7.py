12d24c85c82dac323d76bd49c6362fd12dffbeb4
Viktor <viktor.sbruev.1999@gmail.com>
2025-07-05 14:05:21 +0300

    Added task 504

---
+++ 504_base_7.py
@@ -0,0 +1,33 @@
+'''
+Given an integer number, return a string representing its base 7 representation.
+
+Examples:
+Input: number = 100
+Output: "202"
+
+Input: number = -7
+Output: "-10"
+
+Дано целое число number. Верните строку, представляющую его запись в семеричной системе счисления (с основанием 7).
+
+Примеры:
+Ввод: number = 100
+Вывод: "202"
+
+Ввод: number = -7
+Вывод: "-10"
+'''
+
+def convert_to_base_7(self, number: int) -> str:
+    if number == 0:
+        return "0"
+        
+    negative = number < 0
+    number = abs(number)
+    result = ""
+        
+    while number > 0:
+        result = str(number % 7) + result
+        number //= 7
+        
+    return "-" + result if negative else result

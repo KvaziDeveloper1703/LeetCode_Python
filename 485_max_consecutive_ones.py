791b97d5723ffbf43e7d969d5718de3b1198b7e2
Viktor <viktor.sbruev.1999@gmail.com>
2025-07-05 13:42:40 +0300

    Added task 485

---
+++ 485_max_consecutive_ones.py
@@ -0,0 +1,32 @@
+'''
+Given a binary array numbers, return the maximum number of consecutive 1s in the array.
+
+Examples:
+Input: numbers = [1,1,0,1,1,1]
+Output: 3
+
+Input: numbers = [1,0,1,1,0,1]
+Output: 2
+
+Дан бинарный массив numbers, содержащий только 0 и 1. Верните максимальное количество подряд идущих 1 в массиве.
+
+Примеры:
+Ввод: numbers = [1,1,0,1,1,1]
+Вывод: 3
+
+Ввод: numbers = [1,0,1,1,0,1]
+Вывод: 2
+'''
+
+from typing import List
+
+def find_max_consecutive_ones(self, numbers: List[int]) -> int:
+    max_count = 0
+    count = 0
+    for number in numbers:
+        if number == 1:
+            count += 1
+            max_count = max(max_count, count)
+        else:
+            count = 0
+    return max_count

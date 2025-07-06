690d9c672d8949002749bb3117fd6f1ee28a282c
Viktor <viktor.sbruev.1999@gmail.com>
2025-07-06 22:34:04 +0300

    Formatted task 118

--- Easy/118_pascals_triangle.py
+++ Easy/118_pascals_triangle.py
@@ -1,31 +1,31 @@
 '''
-Given an integer num_rows, return the first num_rows of Pascal's triangle.
+Given an integer number_of_rows, return the first number_of_rows of Pascal's triangle.
 In Pascal's triangle, each number is the sum of the two numbers directly above it.
 
 Examples:
-Input: num_rows = 5
+Input: number_of_rows = 5
 Output: [[1],[1,1],[1,2,1],[1,3,3,1],[1,4,6,4,1]]
 
-Input: num_rows = 1
+Input: number_of_rows = 1
 Output: [[1]]
 
-Дано целое число num_rows. Верните первые num_rows строк треугольника Паскаля.
+Дано целое число number_of_rows. Верните первые num_rows строк треугольника Паскаля.
 В треугольнике Паскаля каждое число — это сумма двух чисел, находящихся непосредственно над ним.
 
 Пример:
-Ввод: num_rows = 5
+Ввод: number_of_rows = 5
 Вывод: [[1],[1,1],[1,2,1],[1,3,3,1],[1,4,6,4,1]]
 
-Ввод: num_rows = 1
+Ввод: number_of_rows = 1
 Вывод: [[1]]
 '''
 
 from typing import List
 
-def generate(self, num_rows: int) -> List[List[int]]:
+def generate(number_of_rows: int) -> List[List[int]]:
     triangle = []
 
-    for i in range(num_rows):
+    for i in range(number_of_rows):
         row = [1] * (i + 1)
         for j in range(1, i):
             row[j] = triangle[i - 1][j - 1] + triangle[i - 1][j]

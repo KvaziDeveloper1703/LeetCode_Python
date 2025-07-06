e4df5a0103f70bee685e6010bc1eedc48632fab3
Viktor <viktor.sbruev.1999@gmail.com>
2025-07-06 20:47:22 +0300

    Formatted task 67

--- Easy/67_add_binary.py
+++ Easy/67_add_binary.py
@@ -1,16 +1,16 @@
 '''
-You are given two binary strings a and b.
+You are given two binary strings first_binary and second_binary.
 Return their sum as a binary string.
 
 Examples:
-Input: a = "11", b = "1"
+Input: first_binary = "11", second_binary = "1"
 Output: "100"
 
-Даны две двоичные строки a и b.
+Даны две двоичные строки first_binary и second_binary.
 Верните их сумму в виде двоичной строки.
 
 Примеры:
-Ввод: a = "11", b = "1"
+Ввод: first_binary = "11", second_binary = "1"
 Вывод: "100"
 '''
 
@@ -22,9 +22,9 @@ def add_binary(first_binary: str, second_binary: str) -> str:
     index_second = len(second_binary) - 1
 
     while index_first >= 0 or index_second >= 0 or carry:
-        bit1 = int(first_binary[index_first]) if index_first >= 0 else 0
-        bit2 = int(second_binary[index_second]) if index_second >= 0 else 0
-        total = bit1 + bit2 + carry
+        bit_1 = int(first_binary[index_first]) if index_first >= 0 else 0
+        bit_2 = int(second_binary[index_second]) if index_second >= 0 else 0
+        total = bit_1 + bit_2 + carry
         carry = total // 2
         result.append(str(total % 2))
         index_first -= 1

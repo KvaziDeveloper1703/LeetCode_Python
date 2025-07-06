ab88a155066bd5521cfab9cc3eb678016299476e
Viktor <viktor.sbruev.1999@gmail.com>
2025-07-06 20:25:51 +0300

    Formatted task 20

--- Easy/20_valid_parentheses.py
+++ Easy/20_valid_parentheses.py
@@ -1,38 +1,39 @@
 '''
 Given a string S containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
 An input string is valid if:
-+ Open brackets must be closed by the same type of brackets;
-+ Open brackets must be closed in the correct order;
-+ Every close bracket has a corresponding open bracket of the same type.
+    + Open brackets must be closed by the same type of brackets;
+    + Open brackets must be closed in the correct order;
+    + Every close bracket has a corresponding open bracket of the same type.
 
 Examples:
-Input: s = "()"
+Input: S = "()"
 Output: true
 
-Input: s = "()[]{}"
+Input: S = "()[]{}"
 Output: true
 
 Дана строка S, содержащая только символы '(', ')', '{', '}', '[' и ']'. Определите, является ли эта строка правильной.
 Строка считается правильной, если выполняются следующие условия:
-+ Открывающие скобки должны закрываться скобками того же типа;
-+ Открывающие скобки должны закрываться в правильном порядке;
-+ Каждой закрывающей скобке должна соответствовать открывающая скобка того же типа.
+    + Открывающие скобки должны закрываться скобками того же типа;
+    + Открывающие скобки должны закрываться в правильном порядке;
+    + Каждой закрывающей скобке должна соответствовать открывающая скобка того же типа.
 
 Примеры:
-Вход: s = "()"
+Вход: S = "()"
 Выход: true
 
-Вход: s = "()[]{}"
+Вход: S = "()[]{}"
 Выход: true
 '''
 
 def is_valid(S: str) -> bool:
     stack = []
-    bracket_map = {')': '(', '}': '{', ']': '['}
+    bracket_map = { ')':'(', 
+                    '}':'{', 
+                    ']':'['}
     for character in S:
         if character in bracket_map:
             top_element = stack.pop() if stack else '#'
-
             if bracket_map[character] != top_element:
                 return False
         else:

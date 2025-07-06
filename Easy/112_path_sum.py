e6240a29335b655281038a8ac87ab91288b88cd1
Viktor <viktor.sbruev.1999@gmail.com>
2025-07-06 22:31:41 +0300

    Formatted task 112

--- Easy/112_path_sum.py
+++ Easy/112_path_sum.py
@@ -3,24 +3,15 @@ Given the root of a binary tree and an integer target_sum, return true if the tr
 A leaf is a node with no children.
 
 Example:
-Input: 
-root = [5,4,8,11,null,13,4,7,2,null,null,null,1]
-target_sum = 22
+Input: root = [5,4,8,11,null,13,4,7,2,null,null,null,1], target_sum = 22
+Output: True
 
-Output:
-true
-
-Дано бинарное дерево и целое число target_sum.
-Нужно вернуть true, если в дереве существует путь от корня до листа, сумма значений вдоль которого равна target_sum.
+Дано бинарное дерево и целое число target_sum. Нужно вернуть true, если в дереве существует путь от корня до листа, сумма значений вдоль которого равна target_sum.
 Лист — это узел, у которого нет дочерних элементов.
 
 Пример:
-Ввод: 
-root = [5,4,8,11,null,13,4,7,2,null,null,null,1]
-target_sum = 22
-
-Вывод:
-true
+Ввод: root = [5,4,8,11,null,13,4,7,2,null,null,null,1], target_sum = 22
+Вывод: True
 '''
 
 from typing import Optional
@@ -31,16 +22,12 @@ class TreeNode:
         self.left = left
         self.right = right
 
-class Solution:
-    def has_path_sum(self, root: Optional[TreeNode], target_sum: int) -> bool:
-        if not root:
-            return False
+def has_path_sum(root: Optional[TreeNode], target_sum: int) -> bool:
+    if not root:
+        return False
 
-        if not root.left and not root.right:
-            return root.value == target_sum
+    if not root.left and not root.right:
+        return root.value == target_sum
 
-        remaining_sum = target_sum - root.value
-        return (
-            self.has_path_sum(root.left, remaining_sum) or
-            self.has_path_sum(root.right, remaining_sum)
-        )
+    remaining_sum = target_sum - root.value
+    return (self.has_path_sum(root.left, remaining_sum) or self.has_path_sum(root.right, remaining_sum))

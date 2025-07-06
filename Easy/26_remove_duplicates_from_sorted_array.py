d4c9dafe15fe648d08d53ceaa79f475d3004d8cb
Viktor <viktor.sbruev.1999@gmail.com>
2025-07-06 20:32:22 +0300

    Formatted task 26

--- Easy/26_remove_duplicates_from_sorted_array.py
+++ Easy/26_remove_duplicates_from_sorted_array.py
@@ -3,31 +3,31 @@ You are given a sorted integer array numbers in non-decreasing order. Your task 
 After removing the duplicates, return the number of unique elements, denoted as k.
 
 Requirements:
-+ Modify the array nums in-place so that the first k elements contain the unique elements in their original order;
-+ The remaining elements beyond index k - 1 do not matter — you can leave them as-is;
-+ Return the value of k.
+    + Modify the array numbers in-place so that the first k elements contain the unique elements in their original order;
+    + The remaining elements beyond index k - 1 do not matter — you can leave them as-is;
+    + Return the value of k.
 
 Examples:
-Input: nums = [1, 1, 2]
-Output: 2, nums = [1, 2, _]
+Input: numbers = [1, 1, 2]
+Output: 2, numbers = [1, 2, _]
 
-Input: nums = [0, 0, 1, 1, 1, 2, 2, 3, 3, 4]
-Output: 5, nums = [0, 1, 2, 3, 4, _, _, _, _, _]
+Input: numbers = [0, 0, 1, 1, 1, 2, 2, 3, 3, 4]
+Output: 5, numbers = [0, 1, 2, 3, 4, _, _, _, _, _]
 
-Дан отсортированный по неубыванию массив целых чисел nums. Ваша задача — удалить дубликаты на месте, чтобы каждый уникальный элемент встречался только один раз, при этом сохраняя относительный порядок элементов.
+Дан отсортированный по неубыванию массив целых чисел numbers. Ваша задача — удалить дубликаты на месте, чтобы каждый уникальный элемент встречался только один раз, при этом сохраняя относительный порядок элементов.
 После удаления дубликатов верните количество уникальных элементов — обозначим его как k.
 
 Требования:
-+ Измените массив nums на месте так, чтобы первые k элементов содержали уникальные значения в исходном порядке;
-+ Остальная часть массива (начиная с индекса k) может содержать любые значения — она не имеет значения;
-+ Верните значение k.
+    + Измените массив numbers на месте так, чтобы первые k элементов содержали уникальные значения в исходном порядке;
+    + Остальная часть массива может содержать любые значения — она не имеет значения;
+    + Верните значение k.
 
 Примеры:
-Ввод: nums = [1, 1, 2]
-Вывод: 2, nums = [1, 2, _]
+Ввод: numbers = [1, 1, 2]
+Вывод: 2, numbers = [1, 2, _]
 
-Ввод: nums = [0, 0, 1, 1, 1, 2, 2, 3, 3, 4]
-Вывод: 5, nums = [0, 1, 2, 3, 4, _, _, _, _, _]
+Ввод: numbers = [0, 0, 1, 1, 1, 2, 2, 3, 3, 4]
+Вывод: 5, numbers = [0, 1, 2, 3, 4, _, _, _, _, _]
 '''
 
 from typing import List

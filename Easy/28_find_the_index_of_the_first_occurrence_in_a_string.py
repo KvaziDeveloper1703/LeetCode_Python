c99d6105389045f4ad7f945503a1c7f0d7c6a891
Viktor <viktor.sbruev.1999@gmail.com>
2025-07-06 20:40:15 +0300

    Formatted task 28

--- Easy/28_find_the_index_of_the_first_occurrence_in_a_string.py
+++ Easy/28_find_the_index_of_the_first_occurrence_in_a_string.py
@@ -20,14 +20,11 @@ Output: -1
 Вывод: -1
 '''
 
-def strStr(haystack: str, needle: str) -> int:
+def index_of_substring(haystack: str, needle: str) -> int:
     n, m = len(haystack), len(needle)
-
     if m == 0:
         return 0
-
     for i in range(n - m + 1):
         if haystack[i:i + m] == needle:
             return i
-
     return -1

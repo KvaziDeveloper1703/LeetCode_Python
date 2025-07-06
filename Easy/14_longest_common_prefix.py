36e4a4edf073c9ca62e9a14f94f8f972d949b108
Viktor <viktor.sbruev.1999@gmail.com>
2025-07-06 20:20:21 +0300

    Formatted task 14

--- Easy/14_longest_common_prefix.py
+++ Easy/14_longest_common_prefix.py
@@ -3,35 +3,35 @@ Write a function that finds the longest common prefix among an array of strings.
 If there is no common prefix, return an empty string "".
 
 Examples:
-Input: strs = ["flower", "flow", "flight"]
+Input: strings = ["flower", "flow", "flight"]
 Output: "fl"
 
-Input: strs = ["dog", "racecar", "car"]
+Input: strings = ["dog", "racecar", "car"]
 Output: ""
 
 Напишите функцию, которая находит наибольший общий префикс среди массива строк.
 Если общего префикса нет, верните пустую строку "".
 
 Примеры:
-Вход: strs = ["flower", "flow", "flight"]
+Ввод: strings = ["flower", "flow", "flight"]
 Выход: "fl"
 
-Вход: strs = ["dog", "racecar", "car"]
+Ввод: strings = ["dog", "racecar", "car"]
 Выход: ""
 '''
 
 from typing import List
 
-def longest_common_prefix(given_strings: List[str]) -> str:
-        if not given_strings:
-            return ""
+def longest_common_prefix(strings: List[str]) -> str:
+    if not strings:
+        return ""
         
-        prefix = given_strings[0]
+    prefix = strings[0]
 
-        for word in given_strings[1:]:
-            while not word.startswith(prefix):
-                prefix = prefix[:-1]
-                if not prefix:
-                    return ""
+    for word in strings[1:]:
+        while not word.startswith(prefix):
+            prefix = prefix[:-1]
+            if not prefix:
+                return ""
 
-        return prefix
+    return prefix

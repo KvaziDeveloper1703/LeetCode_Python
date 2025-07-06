cd829edb2de80b8b608bccaa8c9368a187337b9a
Viktor <viktor.sbruev.1999@gmail.com>
2025-07-06 22:56:58 +0300

    Formatted task 168

--- Easy/168_excel_sheet_column_title.py
+++ Easy/168_excel_sheet_column_title.py
@@ -2,11 +2,11 @@
 Given an integer column_number, return its corresponding column title as it appears in an Excel sheet.
 
 Excel columns are labeled as follows:
-A  -> 1  
-B  -> 2  
-C  -> 3  
+A -> 1  
+B -> 2  
+C -> 3  
 ...  
-Z  -> 26  
+Z -> 26  
 AA -> 27  
 AB -> 28  
 
@@ -19,14 +19,14 @@ Output: "A"
 Input: column_number = 28
 Output: "AB"
 
-Дано целое число colum_number. Верните название столбца, как оно отображается в Excel.
+Дано целое число column_number. Верните название столбца, как оно отображается в Excel.
 
 В Excel используется следующая система именования столбцов:
-A  -> 1  
-B  -> 2  
-C  -> 3  
+A -> 1  
+B -> 2  
+C -> 3  
 ...  
-Z  -> 26  
+Z -> 26  
 AA -> 27  
 AB -> 28  
 
@@ -42,9 +42,11 @@ AB -> 28
 
 def convert_to_title(column_number: int) -> str:
     result = []
+
     while column_number > 0:
         column_number -= 1
         remainder = column_number % 26
         result.append(chr(ord('A') + remainder))
         column_number //= 26
+
     return ''.join(reversed(result))

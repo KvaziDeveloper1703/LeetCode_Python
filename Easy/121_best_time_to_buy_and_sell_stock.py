5d20f4e2599a4bf3a68f2734337f098f38961b08
Viktor <viktor.sbruev.1999@gmail.com>
2025-07-06 22:43:08 +0300

    Formatted task 121

--- Easy/121_best_time_to_buy_and_sell_stock.py
+++ Easy/121_best_time_to_buy_and_sell_stock.py
@@ -31,6 +31,7 @@ from typing import List
 def max_profit(prices: List[int]) -> int:
     min_price = float('inf')
     max_profit = 0
+
     for price in prices:
         if price < min_price:
             min_price = price

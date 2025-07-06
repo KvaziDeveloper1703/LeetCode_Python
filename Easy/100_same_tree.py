ea24c5082b8675c93b8e126cec6dab5b32422168
Viktor <viktor.sbruev.1999@gmail.com>
2025-07-06 21:58:26 +0300

    Formatted task 100

--- Easy/100_same_tree.py
+++ Easy/100_same_tree.py
@@ -28,13 +28,12 @@ class TreeNode:
         self.left = left
         self.right = right
 
-class Solution:
-    def is_same_tree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
-        if not p and not q:
-            return True
-        if not p or not q:
-            return False
-        if p.value != q.value:
-            return False
+def is_same_tree(p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+    if not p and not q:
+        return True
+    if not p or not q:
+        return False
+    if p.value != q.value:
+        return False
 
-        return self.is_same_tree(p.left, q.left) and self.is_same_tree(p.right, q.right)
+    return self.is_same_tree(p.left, q.left) and self.is_same_tree(p.right, q.right)

43f7a352ce8154b836e07179d4b63fcea4f9d9a1
Viktor <viktor.sbruev.1999@gmail.com>
2025-07-06 22:55:20 +0300

    Formatted task 141

--- Easy/141_linked_list_cycle.py
+++ Easy/141_linked_list_cycle.py
@@ -2,11 +2,12 @@
 Given head, the head of a linked list, determine if the linked list has a cycle.
 A cycle exists in the linked list if there is a node that can be revisited by continuously following the next pointer. 
 The variable position denotes the index of the node that the tail's next pointer is connected to. Note that pos is not passed as a parameter.
+
 Return true if there is a cycle in the linked list, otherwise return false.
 
 Example:
 Input: head = [3,2,0,-4], position = 1
-Output: true
+Output: True
 
 Дан указатель head на начало связного списка. Необходимо определить, содержит ли список цикл.
 Цикл существует, если в списке есть такой узел, к которому можно вернуться, непрерывно переходя по указателю next.
@@ -14,8 +15,8 @@ Output: true
 Верните true, если в связном списке есть цикл, и false — если цикла нет.
 
 Пример:
-Вход: head = [3, 2, 0, -4], position = 1
-Выход: true
+Ввод: head = [3, 2, 0, -4], position = 1
+Вывод: True
 '''
 
 from typing import Optional
@@ -32,7 +33,6 @@ def has_cycle(head: Optional[ListNode]) -> bool:
     while fast_pointer and fast_pointer.next:
         slow_pointer = slow_pointer.next
         fast_pointer = fast_pointer.next.next
-
         if slow_pointer == fast_pointer:
             return True
 

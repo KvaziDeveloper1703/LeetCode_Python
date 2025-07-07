d60ff77ac574ff51a60a86311881508819be1820
Viktor <viktor.sbruev.1999@gmail.com>
2025-07-07 22:57:11 +0300

    Formatted task 476

--- Easy/476_number_complement.py
+++ Easy/476_number_complement.py
@@ -1,32 +1,32 @@
 '''
-The complement of an integer is formed by flipping all the 0's to 1's and all the 1's to 0's in its binary representation (ignoring leading zero bits).
-Given an integer num, return its complement.
+The complement of an integer is formed by flipping all the 0's to 1's and all the 1's to 0's in its binary representation.
+Given an integer number, return its complement.
 
 Examples:
-Input: num = 5
+Input: number = 5
 Output: 2
 
-Input: num = 1
+Input: number = 1
 Output: 0
 
-Дополнение числа — это число, полученное путём замены всех 0 на 1, а всех 1 на 0 в его двоичной записи (без учёта ведущих нулей).
-Дан целое число num. Верните его дополнение.
+Дополнение числа — это число, полученное путём замены всех 0 на 1, а всех 1 на 0 в его двоичной записи.
+Дан целое число number. Верните его дополнение.
 
 Примеры:
-Ввод: num = 5
+Ввод: number = 5
 Вывод: 2
 
-Ввод: num = 1
+Ввод: number = 1
 Вывод: 0
 '''
 
-class Solution:
-    def findComplement(self, num: int) -> int:
-        result = 0
-        bit = 1
-        while num > 0:
-            if num % 2 == 0:
-                result += bit
-            num //= 2
-            bit <<= 1
-        return result
+def find_complement(number: int) -> int:
+    result = 0
+    bit = 1
+
+    while number > 0:
+        if number % 2 == 0:
+            result += bit
+        number //= 2
+        bit <<= 1
+    return result

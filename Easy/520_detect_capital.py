a42692e1767e75a1aab19f61db2dc512d0206689
Viktor <viktor.sbruev.1999@gmail.com>
2025-07-07 23:36:35 +0300

    Formatted task 520

--- Easy/520_detect_capital.py
+++ Easy/520_detect_capital.py
@@ -1,34 +1,34 @@
 '''
 We say the usage of capitals in a word is correct if one of the following conditions holds:
-    + All the letters in the word are capitals, like "USA".
-    + All the letters in the word are not capitals, like "leetcode".
-    + Only the first letter in the word is capital (and the rest are lowercase), like "Google".
+    + All the letters in the word are capitals, like "USA";
+    + All the letters in the word are not capitals, like "leetcode";
+    + Only the first letter in the word is capital, like "Google".
 
-Given a string word, return true if its usage of capitals is correct according to the above rules. Otherwise, return false.
+Given a string word, return True if its usage of capitals is correct according to the above rules. Otherwise, return False.
 
 Examples:
 Input: word = "USA"
-Output: true
+Output: True
 
 Input: word = "FlaG"
-Output: false
+Output: False
 
 Говорят, что использование заглавных букв в слове корректно, если выполняется одно из следующих условий:
-    + Все буквы в слове заглавные, например: "USA".
-    + Все буквы в слове строчные, например: "leetcode".
-    + Только первая буква в слове заглавная (остальные строчные), например: "Google".
+    + Все буквы в слове заглавные, например: "USA";
+    + Все буквы в слове строчные, например: "leetcode";
+    + Только первая буква в слове заглавная, например: "Google".
 
-Дан строковый параметр word. Верните true, если использование заглавных букв в нём корректно в соответствии с описанными правилами. Иначе верните false.
+Дан строковый параметр word. Верните True, если использование заглавных букв в нём корректно в соответствии с описанными правилами. Иначе верните False.
 
 Примеры:
 Ввод: word = "USA"
-Вывод: true
+Вывод: True
 
 Ввод: word = "FlaG"
-Вывод: false
+Вывод: False
 '''
 
-def detect_capital_use(self, word: str) -> bool:
+def detect_capital_use(word: str) -> bool:
     if word.isupper():
         return True
     elif word.islower():

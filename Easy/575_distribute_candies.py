4a413c803e0d21cdfaae03b300af3e24729465d3
Viktor <viktor.sbruev.1999@gmail.com>
2025-07-07 23:58:18 +0300

    Formatted task 575

--- Easy/575_distribute_candies.py
+++ Easy/575_distribute_candies.py
@@ -1,6 +1,7 @@
 '''
 Alice has n candies, where the i-th candy is of type candyType[i].
-Alice noticed that she was starting to gain weight, so she visited a doctor. The doctor advised her to eat only n / 2 of the candies she has (it is guaranteed that n is always even).
+Alice noticed that she was starting to gain weight, so she visited a doctor. 
+The doctor advised her to eat only n / 2 of the candies she has (it is guaranteed that n is always even).
 Alice loves her candies very much and wants to eat the maximum number of different types of candies possible while still following the doctor's advice.
 
 Given the integer array candyType of length n, return the maximum number of different types of candies she can eat if she only eats n / 2 candies.
@@ -13,7 +14,8 @@ Input: candyType = [1,1,2,3]
 Output: 2
 
 У Алисы есть n конфет, при этом i-я конфета относится к типу candyType[i].
-Алиса заметила, что начала набирать вес, и поэтому пошла к врачу. Врач посоветовал Алисе съесть только n / 2 конфет (гарантируется, что n всегда чётное).
+Алиса заметила, что начала набирать вес, и поэтому пошла к врачу. 
+Врач посоветовал Алисе съесть только n / 2 конфет (гарантируется, что n всегда чётное).
 Алиса очень любит свои конфеты и хочет съесть максимально возможное количество различных типов конфет, при этом следуя совету врача.
 
 По данному целочисленному массиву candyType длины n, необходимо вернуть максимальное количество различных типов конфет, которые Алиса может съесть, если съест только n / 2 конфет.

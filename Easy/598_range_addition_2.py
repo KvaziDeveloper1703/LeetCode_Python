6d1379cfe222d1574c26f4cd8c527bec11ad0fbd
Viktor <viktor.sbruev.1999@gmail.com>
2025-07-08 00:03:23 +0300

    Formatted task 598

--- Easy/598_range_addition_2.py
+++ Easy/598_range_addition_2.py
@@ -30,15 +30,15 @@ Output: 4
 
 from typing import List
 
-def max_count(self, m: int, n: int, ops: List[List[int]]) -> int:
+def max_count(m: int, n: int, ops: List[List[int]]) -> int:
     if not ops:
         return m * n
-        
+
     min_a = m
     min_b = n
-        
+
     for a, b in ops:
         min_a = min(min_a, a)
         min_b = min(min_b, b)
-        
+
     return min_a * min_b

35fb22be9e0210523d313c033228217e00e42780
Viktor <viktor.sbruev.1999@gmail.com>
2025-07-07 22:25:34 +0300

    Formatted task 278

--- Easy/278_first_bad_version.py
+++ Easy/278_first_bad_version.py
@@ -1,7 +1,8 @@
 '''
-You are a product manager leading a team to develop a new product. Unfortunately, the latest version failed the quality check. Since each version is built upon the previous one, all versions after a bad version are also bad.
+You are a product manager leading a team to develop a new product. Unfortunately, the latest version failed the quality check.
+Since each version is built upon the previous one, all versions after a bad version are also bad.
 You are given n versions, numbered from 1 to n, and you need to find the first bad version that caused all subsequent versions to fail.
-An API bool isBadVersion(version) is provided, which returns true if the version is bad and false otherwise.
+An API bool is_bad_version(version) is provided, which returns True if the version is bad and False otherwise.
 Implement a function to find the first bad version. Try to minimize the number of API calls.
 
 Examples:
@@ -11,9 +12,10 @@ Output: 4
 Input: n = 1, bad = 1
 Output: 1
 
-Вы — менеджер продукта и руководите командой, разрабатывающей новый продукт. К сожалению, последняя версия продукта не прошла проверку качества. Поскольку каждая версия разрабатывается на основе предыдущей, все версии после плохой тоже являются плохими.
+Вы — менеджер продукта и руководите командой, разрабатывающей новый продукт. К сожалению, последняя версия продукта не прошла проверку качества.
+Поскольку каждая версия разрабатывается на основе предыдущей, все версии после плохой тоже являются плохими.
 У вас есть n версий, пронумерованных от 1 до n. Ваша задача — найти первую плохую версию, из-за которой испортились все последующие.
-У вас уже есть API-функция bool isBadVersion(version), которая возвращает true, если версия плохая, и false, если хорошая.
+У вас уже есть API-функция bool is_bad_version(version), которая возвращает True, если версия плохая, и False, если хорошая.
 Реализуйте функцию, чтобы найти первую плохую версию. Постарайтесь минимизировать количество вызовов API.
 
 Примеры:
@@ -24,14 +26,14 @@ Output: 1
 Вывод: 1
 '''
 
-def firstBadVersion(self, n: int) -> int:
+def first_bad_version(n: int) -> int:
     left, right = 1, n
         
     while left < right:
-        mid = (left + right) // 2
-        if is_bad_version(mid):
-            right = mid
+        middle = (left + right) // 2
+        if is_bad_version(middle):
+            right = middle
         else:
-            left = mid + 1
+            left = middle + 1
         
     return left

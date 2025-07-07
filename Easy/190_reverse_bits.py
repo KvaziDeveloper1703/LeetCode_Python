956fe1356fc4a1da1e30d0d5976edc51056b7085
Viktor <viktor.sbruev.1999@gmail.com>
2025-07-07 20:31:35 +0300

    Formatted task 190

--- Easy/190_reverse_bits.py
+++ Easy/190_reverse_bits.py
@@ -1,12 +1,10 @@
 '''
 Given a 32-bit unsigned integer n, return its bits reversed.
-In some languages, there is no unsigned integer type. 
-However, the underlying binary representation is the same for signed and unsigned integers. 
-So treat the integer as a sequence of 32 bits and reverse the order of the bits.
+In some languages, there is no unsigned integer type. However, the underlying binary representation is the same for signed and unsigned integers. So treat the integer as a sequence of 32 bits and reverse the order of the bits.
 
 Example:
 Input: n = 00000010100101000001111010011100
-Output: 964176192 (binary: 00111001011110000010100101000000)
+Output: 964176192
 
 Дано 32-битное беззнаковое целое число n. Требуется перевернуть порядок битов в этом числе и вернуть результат.
 В некоторых языках программирования нет отдельного типа для беззнаковых целых чисел, но это не влияет на решение — внутреннее бинарное представление остаётся одинаковым для signed и unsigned типов.

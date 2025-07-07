f4c00a20ed68695fa7e11a55d5aa4b930503ffb0
Viktor <viktor.sbruev.1999@gmail.com>
2025-07-08 00:06:57 +0300

    Formatted task 605

--- Easy/605_can_place_flowers.py
+++ Easy/605_can_place_flowers.py
@@ -1,9 +1,9 @@
 '''
 You have a long flowerbed represented by an integer array flowerbed, where:
-    + 0 means the plot is empty.
+    + 0 means the plot is empty;
     + 1 means the plot already has a flower planted.
-
 Flowers cannot be planted in adjacent plots.
+
 Given an integer n, determine if you can plant n new flowers in the flowerbed without violating the no-adjacent-flowers rule.
 
 Return True if it is possible, otherwise return False.
@@ -18,8 +18,8 @@ Output: False
 У вас есть длинная клумба, представленная массивом целых чисел flowerbed, где:
     + 0 означает, что участок пустой.
     + 1 означает, что на участке уже посажен цветок.
-
 Цветы нельзя сажать на смежных участках.
+
 Дано целое число n. Определите, можно ли посадить n новых цветов, не нарушая правило о том, что цветы не могут находиться на соседних участках.
 
 Если можно, верните True, иначе — False.
@@ -37,16 +37,16 @@ from typing import List
 def can_place_flowers(flowerbed: List[int], n: int) -> bool:
     count = 0
     length = len(flowerbed)
-        
+
     for i in range(length):
         if flowerbed[i] == 0:
             empty_left = (i == 0) or (flowerbed[i - 1] == 0)
             empty_right = (i == length - 1) or (flowerbed[i + 1] == 0)
-                
+
             if empty_left and empty_right:
                 flowerbed[i] = 1
                 count += 1
                 if count >= n:
                     return True
-                        
+
     return count >= n

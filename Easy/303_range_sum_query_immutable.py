5d0672ac9076c0b79bad29a6f713320597e774c8
Viktor <viktor.sbruev.1999@gmail.com>
2025-07-07 22:32:42 +0300

    Formatted task 303

--- Easy/303_range_sum_query_immutable.py
+++ Easy/303_range_sum_query_immutable.py
@@ -1,24 +1,24 @@
 '''
-You are given an integer array nums. You need to handle multiple queries of the following type:
-Calculate the sum of the elements of nums between indices left and right inclusive, where left <= right.
+You are given an integer array numbers. You need to handle multiple queries of the following type:
+Calculate the sum of the elements of numbers between indices left and right inclusive, where left <= right.
 
 Implement the class NumArray with the following methods:
-+ NumArray(int[] nums): Initializes the object with the given integer array nums.
-+ int sumRange(int left, int right): Returns the sum of the elements between indices left and right inclusive (i.e., nums[left] + nums[left + 1] + ... + nums[right]).
+    + NumArray(int[] numbers): Initializes the object with the given integer array numbers.
+    + int sum_range(int left, int right): Returns the sum of the elements between indices left and right inclusive (i.e., numbers[left] + numbers[left + 1] + ... + numbers[right]).
 
 Example:
-Input: ["NumArray", "sumRange", "sumRange", "sumRange"], [[[-2, 0, 3, -5, 2, -1]], [0, 2], [2, 5], [0, 5]]
+Input: ["NumArray", "sum_range", "sum_range", "sum_range"], [[[-2, 0, 3, -5, 2, -1]], [0, 2], [2, 5], [0, 5]]
 Output: [null, 1, -1, -3]
 
-Вам дан массив целых чисел nums. Необходимо обрабатывать несколько запросов следующего типа:
-Вычислить сумму элементов массива nums на отрезке между индексами left и right включительно, где left <= right.
+Вам дан массив целых чисел numbers. Необходимо обрабатывать несколько запросов следующего типа:
+Вычислить сумму элементов массива numbers на отрезке между индексами left и right включительно, где left <= right.
 
 Реализуйте класс NumArray с двумя методами:
-+ NumArray(int[] nums): конструктор, инициализирующий объект с переданным массивом nums.
-+ int sumRange(int left, int right): возвращает сумму элементов массива с индекса left по индекс right включительно (то есть nums[left] + nums[left + 1] + ... + nums[right]).
+    + NumArray(int[] numbers): конструктор, инициализирующий объект с переданным массивом numbers.
+    + int sum_range(int left, int right): возвращает сумму элементов массива с индекса left по индекс right включительно (то есть numbers[left] + numbers[left + 1] + ... + numbers[right]).
 
 Пример:
-Ввод: ["NumArray", "sumRange", "sumRange", "sumRange"], [[[-2, 0, 3, -5, 2, -1]], [0, 2], [2, 5], [0, 5]]
+Ввод: ["NumArray", "sum_range", "sum_range", "sum_range"], [[[-2, 0, 3, -5, 2, -1]], [0, 2], [2, 5], [0, 5]]
 Вывод: [null, 1, -1, -3]
 '''
 
@@ -30,5 +30,5 @@ class NumArray:
         for number in numbers:
             self.prefix.append(self.prefix[-1] + number)
 
-    def sumRange(self, left: int, right: int) -> int:
+    def sum_range(self, left: int, right: int) -> int:
         return self.prefix[right + 1] - self.prefix[left]

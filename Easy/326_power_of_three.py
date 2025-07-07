2a659c7c0cab153a363a8e81eb27af427c08acaa
Viktor <viktor.sbruev.1999@gmail.com>
2025-07-07 22:34:20 +0300

    Formatted task 326

--- Easy/326_power_of_three.py
+++ Easy/326_power_of_three.py
@@ -1,30 +1,30 @@
 '''
-Given an integer n, return true if it is a power of three. Otherwise, return false.
-An integer n is a power of three if there exists an integer x such that:
-n == 3ˣ
+Given an integer n, return True if it is a power of three. Otherwise, return False.
+An integer n is a power of three if there exists an integer x such that: n == 3ˣ
 
 Examples:
 Input: n = 27
-Output: true
+Output: True
 
 Input: n = 0
-Output: false
+Output: False
 
-Дано целое число n. Верните true, если n является степенью тройки, и false — в противном случае.
-Целое число n считается степенью тройки, если существует такое целое число x, что:
-n == 3ˣ
+Дано целое число n. Верните True, если n является степенью тройки, и False — в противном случае.
+Целое число n считается степенью тройки, если существует такое целое число x, что: n == 3ˣ
 
 Примеры:
 Вход: n = 27
-Выход: true
+Выход: True
 
 Вход: n = 0
-Выход: false
+Выход: False
 '''
 
-def is_power_of_three(self, n: int) -> bool:
+def is_power_of_three(n: int) -> bool:
     if n <= 0:
         return False
+
     while n % 3 == 0:
         n //= 3
+
     return n == 1

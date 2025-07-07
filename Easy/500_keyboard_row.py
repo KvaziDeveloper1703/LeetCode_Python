0554942ed91d42213b96e69488ebbca24c1a473f
Viktor <viktor.sbruev.1999@gmail.com>
2025-07-07 23:06:52 +0300

    Formatted task 500

--- Easy/500_keyboard_row.py
+++ Easy/500_keyboard_row.py
@@ -1,12 +1,11 @@
 '''
-Given an array of strings words, return the words that can be typed using letters of the alphabet from only one row of the American keyboard (as shown below).
-
+Given an array of strings words, return the words that can be typed using letters of the alphabet from only one row of the American keyboard.
 Note that the strings are case-insensitive, meaning both lowercase and uppercase versions of the same letter are treated as being on the same keyboard row.
 
 On the American keyboard:
-    + The first row consists of the characters: "qwertyuiop"
-    + The second row consists of the characters: "asdfghjkl"
-    + The third row consists of the characters: "zxcvbnm"
+    + The first row consists of the characters: "qwertyuiop";
+    + The second row consists of the characters: "asdfghjkl";
+    + The third row consists of the characters: "zxcvbnm".
 
 Examples:
 Input: words = ["Hello","Alaska","Dad","Peace"]
@@ -15,14 +14,13 @@ Output: ["Alaska","Dad"]
 Input: words = ["omk"]
 Output: []
 
-Дан массив строк words. Верните те слова из массива, которые можно напечатать, используя буквы только из одной строки американской клавиатуры (см. схему ниже).
-
+Дан массив строк words. Верните те слова из массива, которые можно напечатать, используя буквы только из одной строки американской клавиатуры.
 Учтите, что регистр букв не имеет значения: строчные и заглавные буквы считаются находящимися в одной строке клавиатуры.
 
 На американской клавиатуре:
-    + Первая строка содержит символы: "qwertyuiop"
-    + Вторая строка содержит символы: "asdfghjkl"
-    + Третья строка содержит символы: "zxcvbnm"
+    + Первая строка содержит символы: "qwertyuiop";
+    + Вторая строка содержит символы: "asdfghjkl";
+    + Третья строка содержит символы: "zxcvbnm".
 
 Примеры:
 Ввод: words = ["Hello","Alaska","Dad","Peace"]
@@ -34,7 +32,7 @@ Output: []
 
 from typing import List
 
-def find_words(self, words: List[str]) -> List[str]:
+def find_words(words: List[str]) -> List[str]:
     rows = [
         set("qwertyuiop"),
         set("asdfghjkl"),

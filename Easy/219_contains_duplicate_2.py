b49b02f75ec63c4372cc315451ba4d50666e8973
Viktor <viktor.sbruev.1999@gmail.com>
2025-07-07 20:39:46 +0300

    Formatted task 219

--- Easy/219_contains_duplicate_2.py
+++ Easy/219_contains_duplicate_2.py
@@ -1,32 +1,32 @@
 '''
-Given an integer array numbers and an integer k. Return true if there are two distinct indices i and j in the array such that numbers[i] == numbers[j] and abs(i - j) <= k.
+Given an integer array numbers and an integer k. Return True if there are two distinct indices i and j in the array such that numbers[i] == numbers[j] and abs(i - j) <= k.
 
 Examples:
 Input: numbers = [1,2,3,1], k = 3
-Output: true
+Output: True
 
 Input: numbers = [1,0,1,1], k = 1
-Output: true
+Output: True
 
 Дан массив целых чисел numbers и целое число k. Необходимо определить, существуют ли два различных индекса i и j, такие что: numbers[i] == numbers[j], и |i - j| <= k. 
-Верните true, если такие индексы существуют, иначе — false.
+Верните True, если такие индексы существуют, иначе — false.
 
 Примеры:
 Ввод: numbers = [1, 2, 3, 1], k = 3
-Вывод: true
+Вывод: True
 
 Ввод: numbers = [1, 0, 1, 1], k = 1
-Вывод: true
+Вывод: True
 '''
 
 from typing import List
 
 def contains_nearby_duplicate(numbers: List[int], k: int) -> bool:
-    num_index_map = {}
+    number_index_map = {}
 
     for index, number in enumerate(numbers):
-        if number in num_index_map and index - num_index_map[number] <= k:
+        if number in number_index_map and index - number_index_map[number] <= k:
             return True
-        num_index_map[number] = index
+        number_index_map[number] = index
 
     return False

2858218bb58d80b318815442993d18b09960e952
Viktor <viktor.sbruev.1999@gmail.com>
2025-07-07 22:59:26 +0300

    Formatted task 482

--- Easy/482_license_key_formatting.py
+++ Easy/482_license_key_formatting.py
@@ -2,9 +2,9 @@
 You are given a license key represented as a string S that consists only of alphanumeric characters and dashes (-). The string is separated into n + 1 groups by n dashes.
 You are also given an integer k.
 Your task is to reformat the string s so that:
-    + Each group (except possibly the first one) contains exactly k characters.
-    + The first group must contain at least one character and could be shorter than k.
-    + Groups are separated by a single dash (-).
+    + Each group (except possibly the first one) contains exactly k characters;
+    + The first group must contain at least one character and could be shorter than k;
+    + Groups are separated by a single dash (-);
     + All lowercase letters should be converted to uppercase.
 
 Return the reformatted license key.
@@ -19,9 +19,9 @@ Output: "2-5G-3J"
 Дан строковый ключ лицензии S, который состоит только из буквенно-цифровых символов и дефисов (-). Строка разделена на n + 1 групп с помощью n дефисов.
 Также дано целое число k.
 Необходимо отформатировать строку S так, чтобы:
-    + Каждая группа (кроме, возможно, первой) содержала ровно k символов.
-    + Первая группа содержала хотя бы один символ и могла быть короче k.
-    + Группы разделялись одним дефисом (-).
+    + Каждая группа (кроме, возможно, первой) содержала ровно k символов;
+    + Первая группа содержала хотя бы один символ и могла быть короче k;
+    + Группы разделялись одним дефисом (-);
     + Все строчные буквы были преобразованы в заглавные.
 
 Верните отформатированный лицензионный ключ.
@@ -34,13 +34,13 @@ Output: "2-5G-3J"
 Вывод: "2-5G-3J"
 '''
 
-def license_key_formatting(self, S: str, k: int) -> str:
+def license_key_formatting(S: str, k: int) -> str:
     S = S.replace('-', '').upper()
     size = len(S)
     first_group = size % k or k
     groups = [S[:first_group]]
-        
+
     for i in range(first_group, size, k):
         groups.append(S[i:i+k])
-        
+
     return '-'.join(groups)

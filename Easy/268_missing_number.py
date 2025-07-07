5aea6ef82017dca7db87c9be7c3e5710874a79a1
Viktor <viktor.sbruev.1999@gmail.com>
2025-07-07 22:18:17 +0300

    Formatted task 268

--- Easy/268_missing_number.py
+++ Easy/268_missing_number.py
@@ -1,5 +1,6 @@
 '''
-Given an array numbers containing n distinct numbers in the range [0, n], return the only number in the range that is missing from the array.
+Given an array numbers containing n distinct numbers in the range [0, n].
+Return the only number in the range that is missing from the array.
 
 Examples:
 Input: numbers = [3, 0, 1]
@@ -8,19 +9,20 @@ Output: 2
 Input: numbers = [0, 1]
 Output: 2
 
-Дан массив numbers, содержащий n уникальных чисел в диапазоне от 0 до n включительно. Верните единственное число, которое отсутствует в этом диапазоне, но должно быть в массиве.
+Дан массив numbers, содержащий n уникальных чисел в диапазоне от 0 до n включительно. 
+Верните единственное число, которое отсутствует в этом диапазоне, но должно быть в массиве.
 
 Примеры:
-Вход: numbers = [3, 0, 1]
-Выход: 2
+Ввод: numbers = [3, 0, 1]
+Вывод: 2
 
-Вход: numbers = [0, 1]
-Выход: 2
+Ввод: numbers = [0, 1]
+Вывод: 2
 '''
 
 from typing import List
 
-def missingNumber(self, numbers: List[int]) -> int:
+def missing_number(numbers: List[int]) -> int:
     n = len(numbers)
     expected_sum = n * (n + 1) // 2
     actual_sum = sum(numbers)

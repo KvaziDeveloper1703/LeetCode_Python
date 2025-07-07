f7025e8d4186397116ae46b2598606cf664960d3
Viktor <viktor.sbruev.1999@gmail.com>
2025-07-07 22:11:00 +0300

    Formatted task 242

--- Easy/242_valid_anagram.py
+++ Easy/242_valid_anagram.py
@@ -1,23 +1,23 @@
 '''
-Given two strings, S and T, return true if T is an anagram of S, and false otherwise.
+Given two strings, S and T, return True if T is an anagram of S, and False otherwise.
 An anagram is a word formed from the same letters as another word, with the same number of each letter, but in a different order.
 
 Examples:
 Input: S = "anagram", T = "nagaram"
-Output: true
+Output: True
 
 Input: S = "rat", T = "car"
-Output: false
+Output: False
 
-Даны две строки S и T. Необходимо вернуть true, если T является анаграммой строки S, и false — в противном случае.
+Даны две строки S и T. Необходимо вернуть True, если T является анаграммой строки S, и False — в противном случае.
 Анаграмма — это слово, составленное из тех же букв, что и другое слово, с тем же количеством каждой буквы, но в другом порядке.
 
 Примеры:
 Ввод: S = "anagram", T = "nagaram"
-Вывод: true
+Вывод: True
 
 Ввод: S = "rat", T = "car"
-Вывод: false
+Вывод: False
 '''
 
 def is_anagram(S: str, T: str) -> bool:

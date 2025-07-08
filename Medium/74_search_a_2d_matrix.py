ed5c2bd2c5ca8ddf28a55cceb4706e29af938c43
Viktor <viktor.sbruev.1999@gmail.com>
2025-07-08 21:31:49 +0300

    Formatted task 74

--- Medium/74_search_a_2d_matrix.py
+++ Medium/74_search_a_2d_matrix.py
@@ -1,9 +1,10 @@
 '''
 You are given an m x n integer matrix with the following two properties:
-+ Each row is sorted in non-decreasing order.
-+ The first integer of each row is greater than the last integer of the previous row.
+    + Each row is sorted in non-decreasing order;
+    + The first integer of each row is greater than the last integer of the previous row.
 
-Given an integer target, return true if target is in the matrix, and false otherwise.
+Given an integer target, return True if target is in the matrix, and False otherwise.
+
 You must implement a solution with O(log(m * n)) time complexity.
 
 Example:
@@ -11,13 +12,13 @@ Input: matrix = [   [1,3,5,7],
                     [10,11,16,20],
                     [23,30,34,60]
             ], target = 3  
-Output: true
+Output: True
 
 Вам дана целочисленная матрица размера m x n, обладающая следующими свойствами:
-+ Каждая строка отсортирована по неубыванию.
-+ Первый элемент каждой строки больше последнего элемента предыдущей строки.
+    + Каждая строка отсортирована по неубыванию;
+    + Первый элемент каждой строки больше последнего элемента предыдущей строки.
 
-Дан целочисленный target. Необходимо определить, содержится ли target в матрице. Верните true, если элемент найден, и false в противном случае.
+Дан целочисленный target. Необходимо определить, содержится ли target в матрице. Верните True, если элемент найден, и False в противном случае.
 Требуется реализовать алгоритм с временной сложностью O(log(m * n)).
 
 Пример:
@@ -25,7 +26,7 @@ Output: true
                 [10,11,16,20],
                 [23,30,34,60]
             ], target = 3  
-Вывод: true
+Вывод: True
 '''
 
 from typing import List
@@ -38,14 +39,14 @@ def search_matrix(matrix: List[List[int]], target: int) -> bool:
     left, right = 0, m * n - 1
 
     while left <= right:
-        mid = (left + right) // 2
-        mid_value = matrix[mid // n][mid % n]
-        
-        if mid_value == target:
+        middle = (left + right) // 2
+        middle_value = matrix[middle // n][middle % n]
+
+        if middle_value == target:
             return True
-        elif mid_value < target:
-            left = mid + 1
+        elif middle_value < target:
+            left = middle + 1
         else:
-            right = mid - 1
+            right = middle - 1
 
     return False

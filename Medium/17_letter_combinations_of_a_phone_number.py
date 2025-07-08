d935ffa630d09ea80fcd3ae4f1acadd38b39df9e
Viktor <viktor.sbruev.1999@gmail.com>
2025-07-08 17:04:47 +0300

    Formatted task 17

--- Medium/17_letter_combinations_of_a_phone_number.py
+++ Medium/17_letter_combinations_of_a_phone_number.py
@@ -1,6 +1,7 @@
 '''
-Given a string containing digits from 2-9 inclusive, return all possible letter combinations that the number could represent. 
-Return the answer in any order.
+Given a string containing digits from 2-9 inclusive.
+
+Return all possible letter combinations that the number could represent.
 
 A mapping of digits to letters:
 2 -> "abc"
@@ -19,8 +20,9 @@ Output: ["ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf"]
 Input: digits = ""
 Output: []
 
-Дана строка, содержащая цифры от 2 до 9 включительно. Необходимо вернуть все возможные комбинации букв, которые могут быть представлены этими цифрами. 
-Ответ можно вернуть в любом порядке.
+Дана строка, содержащая цифры от 2 до 9 включительно. 
+
+Необходимо вернуть все возможные комбинации букв, которые могут быть представлены этими цифрами. 
 
 Сопоставление цифр с буквами следующее:
 2 -> "abc"
@@ -43,23 +45,28 @@ Output: []
 def letter_сombinations(digits: str):
     if not digits:
         return []
-    
-    digit_to_letters = {
-        '2': 'abc', '3': 'def', '4': 'ghi', '5': 'jkl',
-        '6': 'mno', '7': 'pqrs', '8': 'tuv', '9': 'wxyz'
-    }
-    
+
+    digit_to_letters = {'2': 'abc', 
+                        '3': 'def', 
+                        '4': 'ghi', 
+                        '5': 'jkl',
+                        '6': 'mno', 
+                        '7': 'pqrs', 
+                        '8': 'tuv', 
+                        '9': 'wxyz'
+                }
+
     result = []
-    
+
     def backtrack(index, path):
         if index == len(digits):
             result.append("".join(path))
             return
-        
+
         for letter in digit_to_letters[digits[index]]:
             path.append(letter)
             backtrack(index + 1, path)
             path.pop()
-    
+
     backtrack(0, [])
     return result

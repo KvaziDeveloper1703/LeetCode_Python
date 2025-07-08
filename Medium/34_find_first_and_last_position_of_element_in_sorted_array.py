d44c60b766d68aa374616f7dd5b9447e1e8a1787
Viktor <viktor.sbruev.1999@gmail.com>
2025-07-08 17:36:25 +0300

    Formatted task 34

--- Medium/34_find_first_and_last_position_of_element_in_sorted_array.py
+++ Medium/34_find_first_and_last_position_of_element_in_sorted_array.py
@@ -1,5 +1,6 @@
 '''
-You are given an array of integers numbers sorted in non-decreasing order. Your task is to find the starting and ending position of a given target value in the array.
+You are given an array of integers numbers sorted in non-decreasing order.
+Your task is to find the starting and ending position of a given target value in the array.
 If the target is not found, return [-1, -1].
 
 You must implement an algorithm with O(log n) runtime complexity.
@@ -11,7 +12,8 @@ Output: [3,4]
 Input: numbers = [5,7,7,8,8,10], target = 6
 Output: [-1,-1]
 
-Дан массив целых чисел numbers, отсортированный в неубывающем порядке. Необходимо найти начальную и конечную позицию заданного значения target в массиве.
+Дан массив целых чисел numbers, отсортированный в неубывающем порядке.
+Необходимо найти начальную и конечную позицию заданного значения target в массиве.
 Если target не найден, верните [-1, -1].
 
 Реализуйте алгоритм с логарифмической сложностью — O(log n).
@@ -27,32 +29,39 @@ Output: [-1,-1]
 from typing import List
 
 def search_range(numbers: List[int], target: int) -> List[int]:
+
     def find_first(numbers, target):
         left, right = 0, len(numbers) - 1
         first = -1
+
         while left <= right:
-            mid = (left + right) // 2
-            if numbers[mid] == target:
-                first = mid
-                right = mid - 1
-            elif numbers[mid] < target:
-                left = mid + 1
+            middle = (left + right) // 2
+
+            if numbers[middle] == target:
+                first = middle
+                right = middle - 1
+            elif numbers[middle] < target:
+                left = middle + 1
             else:
-                right = mid - 1
+                right = middle - 1
+
         return first
-    
+
     def find_last(numbers, target):
         left, right = 0, len(numbers) - 1
         last = -1
+
         while left <= right:
-            mid = (left + right) // 2
-            if numbers[mid] == target:
-                last = mid
-                left = mid + 1
-            elif numbers[mid] < target:
-                left = mid + 1
+            middle = (left + right) // 2
+
+            if numbers[middle] == target:
+                last = middle
+                left = middle + 1
+            elif numbers[middle] < target:
+                left = middle + 1
             else:
-                right = mid - 1
+                right = middle - 1
+
         return last
     
     return [find_first(numbers, target), find_last(numbers, target)]

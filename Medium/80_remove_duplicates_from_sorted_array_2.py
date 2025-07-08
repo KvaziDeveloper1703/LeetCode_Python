45f21a1a09fd7ff01c54f7b9f3bf6474df82f7ac
Viktor <viktor.sbruev.1999@gmail.com>
2025-07-08 21:57:06 +0300

    Formatted task 80

--- Medium/80_remove_duplicates_from_sorted_array_2.py
+++ Medium/80_remove_duplicates_from_sorted_array_2.py
@@ -1,11 +1,12 @@
 '''
 You are given a sorted integer array numbers (in non-decreasing order). Modify the array in-place so that each unique element appears at most twice, while preserving the relative order of the elements.
-Since it's not possible to change the size of the array in some programming languages, you should place the result in the first part of the array nums. More formally:
-+ Let k be the number of elements after removing extra duplicates;
-+ The first k elements of numbers should contain the final result;
-+ The remaining elements beyond the first k positions do not matter.
+Since it's not possible to change the size of the array in some programming languages, you should place the result in the first part of the array numbers. More formally:
+    + Let k be the number of elements after removing extra duplicates;
+    + The first k elements of numbers should contain the final result;
+    + The remaining elements beyond the first k positions do not matter.
 
 You must not use any extra space — the algorithm should run in-place with O(1) extra memory.
+
 Return the value of k.
 
 Examples:
@@ -15,22 +16,22 @@ Output: 5, numbers = [1, 1, 2, 2, 3, _]
 Input: numbers = [0, 0, 1, 1, 1, 1, 2, 3, 3]
 Output: 7, numbers = [0, 0, 1, 1, 2, 3, 3, _, _]
 
-Дан отсортированный по неубыванию массив целых чисел numbers.
-Необходимо изменить массив на месте так, чтобы каждое уникальное число встречалось не более двух раз, при этом сохранив относительный порядок элементов.
+Дан отсортированный по неубыванию массив целых чисел numbers. Необходимо изменить массив на месте так, чтобы каждое уникальное число встречалось не более двух раз, при этом сохранив относительный порядок элементов.
 Так как в некоторых языках программирования нельзя менять размер массива, результат нужно разместить в первых k элементах массива numbers. Формально:
-+ Пусть k — количество элементов после удаления лишних повторов;
-+ Первые k элементов массива должны содержать итоговый результат;
-+ Остальные элементы после позиции k могут быть любыми — они не имеют значения.
+    + Пусть k — количество элементов после удаления лишних повторов;
+    + Первые k элементов массива должны содержать итоговый результат;
+    + Остальные элементы после позиции k могут быть любыми — они не имеют значения.
 
 Нельзя использовать дополнительную память — алгоритм должен работать на месте с O(1) дополнительной памяти.
+
 Верните значение k.
 
 Примеры:
-Вход: numbers = [1, 1, 1, 2, 2, 3]
-Выход: 5, numbers = [1, 1, 2, 2, 3, _]
+Ввод: numbers = [1, 1, 1, 2, 2, 3]
+Вывод: 5, numbers = [1, 1, 2, 2, 3, _]
 
-Вход: numbers = [0, 0, 1, 1, 1, 1, 2, 3, 3]
-Выход: 7, numbers = [0, 0, 1, 1, 2, 3, 3, _, _]
+Ввод: numbers = [0, 0, 1, 1, 1, 1, 2, 3, 3]
+Вывод: 7, numbers = [0, 0, 1, 1, 2, 3, 3, _, _]
 '''
 
 from typing import List

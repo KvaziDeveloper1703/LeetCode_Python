e0ac414af0dba772dc13ccc2849b9e67562682fd
Viktor <viktor.sbruev.1999@gmail.com>
2025-07-08 17:14:12 +0300

    Formatted task 22

--- Medium/22_generate_parentheses.py
+++ Medium/22_generate_parentheses.py
@@ -3,13 +3,23 @@ Given n pairs of parentheses, write a function to generate all combinations of w
 
 Example:
 Input: n = 3
-Output: ["((()))", "(()())", "(())()", "()(())", "()()()"]
+Output: [   "((()))", 
+            "(()())", 
+            "(())()", 
+            "()(())", 
+            "()()()"
+    ]
 
 Дано n пар скобок. Напишите функцию, которая генерирует все возможные комбинации правильно сбалансированных скобок.
 
 Пример:
 Ввод: n = 3
-Вывод: ["((()))", "(()())", "(())()", "()(())", "()()()"]
+Вывод: ["((()))", 
+        "(()())", 
+        "(())()", 
+        "()(())", 
+        "()()()"
+    ]
 '''
 
 def generate_parenthesis(n: int) -> list[str]:
@@ -19,11 +29,11 @@ def generate_parenthesis(n: int) -> list[str]:
         if len(current_string) == 2 * n:
             result.append(current_string)
             return
-        
+
         if open_count < n:
             backtrack(current_string + '(', open_count + 1, close_count)
         if close_count < open_count:
             backtrack(current_string + ')', open_count, close_count + 1)
-    
+
     backtrack("", 0, 0)
     return result

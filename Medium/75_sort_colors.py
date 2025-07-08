2109584e7bfb814ca2235ceadee52783ab4be055
Viktor <viktor.sbruev.1999@gmail.com>
2025-07-08 21:46:26 +0300

    Formatted task 75

--- Medium/75_sort_colors.py
+++ Medium/75_sort_colors.py
@@ -1,25 +1,24 @@
 '''
 You are given an array numbers with n objects colored red, white, or blue, represented by integers:
-+ 0 for red,
-+ 1 for white,
-+ 2 for blue.
+    + 0 for red,
+    + 1 for white,
+    + 2 for blue.
 
-Sort the array in-place so that objects of the same color are adjacent, with the colors in the order: red, white, then blue.
-You must not use the library's sort function.
+Sort the array in-place so that objects of the same color are adjacent, with the colors in the order: red, white, then blue. You must not use the library's sort function.
 
 Дан массив numbers, содержащий n элементов, каждый из которых обозначает цвет: красный, белый или синий. Цвета представлены числами:
-+ 0 — красный,
-+ 1 — белый,
-+ 2 — синий.
+    + 0 — красный,
+    + 1 — белый,
+    + 2 — синий.
 
-Отсортируйте массив на месте так, чтобы одинаковые цвета шли рядом, а порядок цветов был: красный, затем белый, затем синий.
-Нельзя использовать встроенные функции сортировки.
+Отсортируйте массив на месте так, чтобы одинаковые цвета шли рядом, а порядок цветов был: красный, затем белый, затем синий. Нельзя использовать встроенные функции сортировки.
 '''
 
 from typing import List
 
 def sort_colors(numbers: List[int]) -> None:
     low, mid, high = 0, 0, len(numbers) - 1
+
     while mid <= high:
         if numbers[mid] == 0:
             numbers[low], numbers[mid] = numbers[mid], numbers[low]

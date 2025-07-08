9ac59c61a61dc9cbb4cabafd41a19e90d0c5a078
Viktor <viktor.sbruev.1999@gmail.com>
2025-07-08 15:08:32 +0300

    Formatted task 23

--- Hard/23_merge_k_sorted_lists.py
+++ Hard/23_merge_k_sorted_lists.py
@@ -1,14 +1,20 @@
 '''
-You are given an array of k linked-lists, each linked-list is sorted in ascending order. Merge all the linked-lists into one sorted linked-list and return it.
+You are given an array of k linked-lists, each linked-list is sorted in ascending order.
+Merge all the linked-lists into one sorted linked-list and return it.
 
 Example:
-Input: lists = [[1,4,5], [1,3,4], [2,6]]
+Input: lists = [[1,4,5], 
+                [1,3,4], 
+                [2,6]]
 Output: [1,1,2,3,4,4,5,6]
 
-Дан массив из k отсортированных связанных списков. Объедините все эти списки в один отсортированный связанный список и верните его.
+Дан массив из k отсортированных связанных списков.
+Объедините все эти списки в один отсортированный связанный список и верните его.
 
 Пример:
-Ввод: lists = [[1,4,5], [1,3,4], [2,6]]
+Ввод: lists = [ [1,4,5], 
+                [1,3,4], 
+                [2,6]]
 Вывод: [1,1,2,3,4,4,5,6]
 '''
 
@@ -22,53 +28,22 @@ class ListNode:
     def __repr__(self):
         return f"{self.value} -> {repr(self.next)}"
 
-class Solution:
-    def mergeKLists(self, lists: list[ListNode]) -> ListNode:
+def merge_k_lists(lists: list[ListNode]) -> ListNode:
         min_heap = []
-        
+
         for i, l in enumerate(lists):
             if l:
                 heapq.heappush(min_heap, (l.value, i, l))
-        
+
         dummy = ListNode(None)
         current = dummy
-        
+
         while min_heap:
             value, i, node = heapq.heappop(min_heap)
             current.next = node
             current = current.next
-            
+
             if node.next:
                 heapq.heappush(min_heap, (node.next.value, i, node.next))
-        
-        return dummy.next
-import heapq
 
-class ListNode:
-    def __init__(self, value=0, next=None):
-        self.value = value
-        self.next = next
-
-    def __repr__(self):
-        return f"{self.value} -> {repr(self.next)}"
-
-class Solution:
-    def mergeKLists(self, lists: list[ListNode]) -> ListNode:
-        min_heap = []
-        
-        for i, l in enumerate(lists):
-            if l:
-                heapq.heappush(min_heap, (l.value, i, l))
-        
-        dummy = ListNode(None)
-        current = dummy
-        
-        while min_heap:
-            value, i, node = heapq.heappop(min_heap)
-            current.next = node
-            current = current.next
-            
-            if node.next:
-                heapq.heappush(min_heap, (node.next.value, i, node.next))
-        
         return dummy.next

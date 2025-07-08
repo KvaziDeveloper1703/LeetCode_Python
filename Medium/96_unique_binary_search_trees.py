dc97752c94fa581ddeb8b0e74ff3d7e39c0e1717
Viktor <viktor.sbruev.1999@gmail.com>
2025-07-08 22:19:20 +0300

    Formatted task 96

--- Medium/96_unique_binary_search_trees.py
+++ Medium/96_unique_binary_search_trees.py
@@ -1,5 +1,5 @@
 '''
-You are given an integer n. Your task is to return the number of structurally unique Binary Search Trees (BSTs) that can be formed with n distinct values from 1 to n.
+You are given an integer n. Your task is to return the number of structurally unique Binary Search Trees that can be formed with n distinct values from 1 to n.
 
 Examples:
 Input: n = 3
@@ -8,7 +8,7 @@ Output: 5
 Input: n = 1
 Output: 1
 
-Дано целое число n. Необходимо определить количество структурно уникальных бинарных деревьев поиска (BST), которые можно построить, используя n различных значений от 1 до n.
+Дано целое число n. Необходимо определить количество структурно уникальных бинарных деревьев поиска, которые можно построить, используя n различных значений от 1 до n.
 
 Примеры:
 Ввод: n = 3
@@ -18,17 +18,17 @@ Output: 1
 Вывод: 1
 '''
 
-def num_trees(n: int) -> int:
-    num_unique_bsts = [0] * (n + 1)
-    num_unique_bsts[0] = 1
-    num_unique_bsts[1] = 1
+def number_of_trees(n: int) -> int:
+    number_of_unique_bsts = [0] * (n + 1)
+    number_of_unique_bsts[0] = 1
+    number_of_unique_bsts[1] = 1
 
     for total_nodes in range(2, n + 1):
         for root in range(1, total_nodes + 1):
             left_subtree_size = root - 1
             right_subtree_size = total_nodes - root
-            num_unique_bsts[total_nodes] += (
-                num_unique_bsts[left_subtree_size] * num_unique_bsts[right_subtree_size]
+            number_of_unique_bsts[total_nodes] += (
+                number_of_unique_bsts[left_subtree_size] * number_of_unique_bsts[right_subtree_size]
             )
 
-    return num_unique_bsts[n]
+    return number_of_unique_bsts[n]

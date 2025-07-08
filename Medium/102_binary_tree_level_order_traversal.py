f119c3e88878cd60740c2b16c8177eb63e77485b
Viktor <viktor.sbruev.1999@gmail.com>
2025-07-08 22:28:02 +0300

    Formatted task 102

--- Medium/102_binary_tree_level_order_traversal.py
+++ Medium/102_binary_tree_level_order_traversal.py
@@ -1,11 +1,15 @@
 '''
-Given the root of a binary tree, return the level order traversal of its nodes' values.
+Given the root of a binary tree.
+
+Return the level order traversal of its nodes' values.
 
 Example:
 Input: root = [3,9,20,null,null,15,7]
 Output: [[3],[9,20],[15,7]]
 
-Дано бинарное дерево. Требуется вернуть уровневой обход его узлов — то есть, список списков значений узлов, слева направо, уровень за уровнем.
+Дано бинарное дерево. 
+
+Требуется вернуть уровневой обход его узлов — то есть, список списков значений узлов, слева направо, уровень за уровнем.
 
 Пример:
 Ввод: root = [3,9,20,null,null,15,7]
@@ -21,27 +25,26 @@ class TreeNode:
         self.left = left
         self.right = right
 
-class Solution:
-    def level_order(self, root: Optional[TreeNode]) -> List[List[int]]:
-        if not root:
-            return []
+def level_order(self, root: Optional[TreeNode]) -> List[List[int]]:
+    if not root:
+        return []
 
-        result = []
-        queue = deque([root])
+    result = []
+    queue = deque([root])
 
-        while queue:
-            level_size = len(queue)
-            current_level = []
+    while queue:
+        level_size = len(queue)
+        current_level = []
 
-            for _ in range(level_size):
-                current_node = queue.popleft()
-                current_level.append(current_node.value)
+        for _ in range(level_size):
+            current_node = queue.popleft()
+            current_level.append(current_node.value)
 
-                if current_node.left:
-                    queue.append(current_node.left)
-                if current_node.right:
-                    queue.append(current_node.right)
+            if current_node.left:
+                queue.append(current_node.left)
+            if current_node.right:
+                queue.append(current_node.right)
 
-            result.append(current_level)
+        result.append(current_level)
 
-        return result
+    return result

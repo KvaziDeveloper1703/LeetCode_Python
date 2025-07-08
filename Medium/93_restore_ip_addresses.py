6060d50c0ddf09bec8d1705cbe4b8ff58563ee72
Viktor <viktor.sbruev.1999@gmail.com>
2025-07-08 22:08:54 +0300

    Formatted task 93

--- Medium/93_restore_ip_addresses.py
+++ Medium/93_restore_ip_addresses.py
@@ -1,35 +1,33 @@
 '''
-You are given a string S consisting only of digits. 
-Your task is to return all possible valid IP addresses that can be created by inserting exactly three dots into the string. 
-You must not reorder or remove any digits.
+You are given a string S consisting only of digits. Your task is to return all possible valid IP addresses that can be created by inserting exactly three dots into the string. You must not reorder or remove any digits.
 
 A valid IP address:
-+ Has exactly four integer parts, separated by dots;
-+ Each part must be a number from 0 to 255;
-+ Leading zeros are not allowed, except for the number 0 itself.
+    + Has exactly four integer parts, separated by dots;
+    + Each part must be a number from 0 to 255;
+    + Leading zeros are not allowed, except for the number 0 itself.
 
 Return all valid IP addresses that can be formed from the string S, in any order.
 
 Example:
 Input: "25525511135"
 Output: [   "255.255.11.135", 
-            "255.255.111.35"]
+            "255.255.111.35"
+    ]
 
-Дана строка S, содержащая только цифры. 
-Нужно вернуть все возможные корректные IP-адреса, которые можно получить, вставляя ровно три точки в строку. 
-Нельзя менять порядок символов или удалять их.
+Дана строка S, содержащая только цифры. Нужно вернуть все возможные корректные IP-адреса, которые можно получить, вставляя ровно три точки в строку. Нельзя менять порядок символов или удалять их.
 
 Корректный IP-адрес:
-+ Состоит из четырёх чисел, разделённых точками;
-+ Каждое число находится в диапазоне от 0 до 255;
-+ Число не может начинаться с нуля, если оно не равно самому нулю.
+    + Состоит из четырёх чисел, разделённых точками;
+    + Каждое число находится в диапазоне от 0 до 255;
+    + Число не может начинаться с нуля, если оно не равно самому нулю.
 
 Нужно вернуть все допустимые IP-адреса, которые можно составить из строки S, в любом порядке.
 
 Пример:
 Ввод: "25525511135"
 Вывод: ["255.255.11.135", 
-        "255.255.111.35"]
+        "255.255.111.35"
+    ]
 '''
 
 from typing import List
@@ -50,6 +48,7 @@ def restore_ip_addresses(S: str) -> List[str]:
 
             if (segment.startswith("0") and len(segment) > 1) or int(segment) > 255:
                 continue
+
             backtrack(start + length, path + [segment])
 
     backtrack(0, [])

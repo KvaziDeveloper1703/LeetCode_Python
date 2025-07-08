f9477a35fd399c011e3230762599527fbc72f727
Viktor <viktor.sbruev.1999@gmail.com>
2025-07-08 17:51:26 +0300

    Formatted task 40

--- Medium/40_combination_sum_2.py
+++ Medium/40_combination_sum_2.py
@@ -1,25 +1,20 @@
 '''
 Given a list of integers candidates and a target integer target, find all unique combinations in candidates where the numbers sum to target.
-Each number in candidates may only be used once in each combination.
-Note: The solution set must not contain duplicate combinations.
+Each number in candidates may only be used once in each combination. The solution set must not contain duplicate combinations.
 
 Example:
 Input: candidates = [10,1,2,7,6,1,5], target = 8
-
 Output: [[1,1,6],
          [1,2,5],
          [1,7],
          [2,6]
     ]
 
-Дан список чисел candidates и целевое число target. 
-Найдите все уникальные комбинации чисел из candidates, сумма которых равна target.
-Каждое число из candidates можно использовать только один раз в каждой комбинации.
-Важно: Множество решений не должно содержать повторяющихся комбинаций.
+Дан список чисел candidates и целевое число target. Найдите все уникальные комбинации чисел из candidates, сумма которых равна target.
+Каждое число из candidates можно использовать только один раз в каждой комбинации. Множество решений не должно содержать повторяющихся комбинаций.
 
 Пример:
 Ввод: candidates = [10,1,2,7,6,1,5], target = 8
-
 Вывод: [[1,1,6],
         [1,2,5],
         [1,7],
@@ -43,6 +38,7 @@ def combination_sum_2(candidates: List[int], target: int) -> List[List[int]]:
         for i in range(start, len(candidates)):
             if i > start and candidates[i] == candidates[i - 1]:
                 continue
+
             path.append(candidates[i])
             backtrack(i + 1, path, total + candidates[i])
             path.pop()

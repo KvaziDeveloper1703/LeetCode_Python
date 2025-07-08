3931b3c6045c57c3db2685012213a415cc81d58d
Viktor <viktor.sbruev.1999@gmail.com>
2025-07-08 16:00:05 +0300

    Formatted task 135

--- Hard/135_candy.py
+++ Hard/135_candy.py
@@ -1,8 +1,8 @@
 '''
 There are N children standing in a line, and each child has a rating represented by the integer array ratings.
 You need to distribute candies to these children based on the following rules:
-+ Every child must receive at least one candy;
-+ A child with a higher rating than an immediate neighbor must receive more candies than that neighbor.
+    + Every child must receive at least one candy;
+    + A child with a higher rating than an immediate neighbor must receive more candies than that neighbor.
 
 Your task is to determine the minimum number of candies needed to distribute to all the children while satisfying these rules.
 
@@ -15,17 +15,17 @@ Output: 4
 
 Есть N детей, стоящих в ряд, и у каждого ребёнка есть рейтинг, представленный массивом целых чисел ratings.
 Вам нужно раздать конфеты детям в соответствии со следующими правилами:
-+ Каждый ребёнок должен получить как минимум одну конфету;
-+ Ребёнок с более высоким рейтингом, чем его сосед, должен получить больше конфет, чем этот сосед.
+    + Каждый ребёнок должен получить как минимум одну конфету;
+    + Ребёнок с более высоким рейтингом, чем его сосед, должен получить больше конфет, чем этот сосед.
 
 Ваша задача — определить минимальное количество конфет, которое нужно раздать всем детям, чтобы соблюсти указанные правила.
 
 Примеры:
-Вход: ratings = [1, 0, 2]
-Выход: 5
+Ввод: ratings = [1, 0, 2]
+Вывод: 5
 
-Вход: ratings = [1, 2, 2]
-Выход: 4
+Ввод: ratings = [1, 2, 2]
+Вывод: 4
 '''
 
 from typing import List

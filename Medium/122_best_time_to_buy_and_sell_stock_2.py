46c2412067520070f44364cbeb14410142405f48
Viktor <viktor.sbruev.1999@gmail.com>
2025-07-08 23:23:58 +0300

    Formatted task 122

--- Medium/122_best_time_to_buy_and_sell_stock_2.py
+++ Medium/122_best_time_to_buy_and_sell_stock_2.py
@@ -1,8 +1,6 @@
 '''
-You are given an integer array prices, where prices[i] represents the price of a given stock on the i‑th day.
-On each day, you may choose to buy and/or sell the stock.
-However, you can only hold at most one share at a time, meaning you must sell the stock before buying again.
-You are allowed to buy and sell on the same day.
+You are given an integer array prices, where prices[i] represents the price of a given stock on the i‑th day. On each day, you may choose to buy and/or sell the stock.
+However, you can only hold at most one share at a time, meaning you must sell the stock before buying again. You are allowed to buy and sell on the same day.
 
 Return the maximum profit you can achieve.
 
@@ -13,19 +11,17 @@ Output: 7
 Input: prices = [1, 2, 3, 4, 5]
 Output: 4
 
-Дан массив целых чисел prices, где prices[i] — это цена акции в i-й день.
-Каждый день вы можете покупать и/или продавать акции.
-Однако вы можете держать не более одной акции одновременно, то есть перед тем как купить новую акцию, вы должны продать текущую.
-Разрешено покупать и продавать акцию в один и тот же день.
+Дан массив целых чисел prices, где prices[i] — это цена акции в i-й день. Каждый день вы можете покупать и/или продавать акции.
+Однако вы можете держать не более одной акции одновременно, то есть перед тем как купить новую акцию, вы должны продать текущую. Разрешено покупать и продавать акцию в один и тот же день.
 
 Верните максимальную прибыль, которую можно получить.
 
 Примеры:
-Вход: prices = [7, 1, 5, 3, 6, 4]
-Выход: 7
+Ввод: prices = [7, 1, 5, 3, 6, 4]
+Вывод: 7
 
-Вход: prices = [1, 2, 3, 4, 5]
-Выход: 4
+Ввод: prices = [1, 2, 3, 4, 5]
+Вывод: 4
 '''
 
 from typing import List

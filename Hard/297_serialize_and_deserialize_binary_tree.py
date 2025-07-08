f1d9ee2e5d4f6071ea1311e702649282e562ec9d
Viktor <viktor.sbruev.1999@gmail.com>
2025-07-08 16:15:44 +0300

    Formatted task 297

--- Hard/297_serialize_and_deserialize_binary_tree.py
+++ Hard/297_serialize_and_deserialize_binary_tree.py
@@ -1,8 +1,6 @@
 '''
 Serialization is the process of converting a data structure or object into a sequence of bits so that it can be stored in a file or memory buffer, or transmitted across a network connection, to be reconstructed later in the same or another computer environment.
-
-Design an algorithm to serialize and deserialize a binary tree.
-There is no restriction on how your serialization/deserialization algorithm should work. You simply need to ensure that a binary tree can be serialized into a string and this string can then be deserialized back into the original tree structure.
+Design an algorithm to serialize and deserialize a binary tree. There is no restriction on how your serialization/deserialization algorithm should work. You simply need to ensure that a binary tree can be serialized into a string and this string can then be deserialized back into the original tree structure.
 
 Clarification:
 The input/output format is the same as how LeetCode serializes a binary tree.
@@ -13,9 +11,7 @@ Input:  root = [1,2,3,null,null,4,5]
 Output: [1,2,3,null,null,4,5]
 
 Сериализация — это процесс преобразования структуры данных или объекта в последовательность битов, чтобы их можно было сохранить в файле или буфере памяти, либо передать по сети для последующего восстановления в той же или другой среде.
-
-Необходимо разработать алгоритм для сериализации и десериализации бинарного дерева.
-Ограничений на то, как именно должна работать ваша схема сериализации/десериализации, нет.
+Необходимо разработать алгоритм для сериализации и десериализации бинарного дерева. Ограничений на то, как именно должна работать ваша схема сериализации/десериализации, нет.
 Вам нужно лишь гарантировать, что бинарное дерево может быть преобразовано в строку (сериализовано), а затем эта строка может быть обратно преобразована в исходную структуру дерева (десериализовано).
 
 Уточнение:
@@ -29,7 +25,7 @@ Output: [1,2,3,null,null,4,5]
 
 class TreeNode(object):
     def __init__(self, x):
-        self.val = x
+        self.value = x
         self.left = None
         self.right = None
 
@@ -37,25 +33,28 @@ class Codec:
     def serialize(self, root):
         def helper(node):
             if node is None:
-                vals.append('None')
+                values.append('None')
             else:
-                vals.append(str(node.val))
+                values.append(str(node.value))
                 helper(node.left)
                 helper(node.right)
-        
-        vals = []
+
+        values = []
         helper(root)
+
         return ','.join(vals)
 
     def deserialize(self, data):
         def helper():
-            val = next(vals)
-            if val == 'None':
+            value = next(values)
+            if value == 'None':
                 return None
-            node = TreeNode(int(val))
+
+            node = TreeNode(int(value))
             node.left = helper()
             node.right = helper()
+
             return node
-        
-        vals = iter(data.split(','))
+
+        values = iter(data.split(','))
         return helper()

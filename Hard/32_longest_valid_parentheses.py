675ca0ca19f895435e36d0e085c9cfbdab489492
Viktor <viktor.sbruev.1999@gmail.com>
2025-07-08 15:12:00 +0300

    Formatted task 32

--- Hard/32_longest_valid_parentheses.py
+++ Hard/32_longest_valid_parentheses.py
@@ -1,5 +1,5 @@
 '''
-You are given a string S consisting only of the characters '(' and ')'. Your task is to return the length of the longest valid (well-formed) parentheses substring.
+You are given a string S consisting only of the characters '(' and ')'. Your task is to return the length of the longest valid parentheses substring.
 A valid substring is one where every opening parenthesis has a corresponding closing one, and all parentheses are properly nested.
 
 Examples:
@@ -9,7 +9,7 @@ Output: 2
 Input: S = ")()())"
 Output: 4
 
-Дана строка S, состоящая только из символов '(' и ')'. Необходимо вернуть длину самой длинной корректной (правильно вложенной) подстроки с круглыми скобками.
+Дана строка S, состоящая только из символов '(' и ')'. Необходимо вернуть длину самой длинной корректной подстроки с круглыми скобками.
 Корректной считается подстрока, в которой каждая открывающая скобка имеет соответствующую закрывающую, и все скобки правильно вложены.
 
 Примеры:
@@ -23,6 +23,7 @@ Output: 4
 def longest_valid_parentheses(S: str) -> int:
     max_len = 0
     stack = [-1]
+
     for i, character in enumerate(S):
         if character == '(':
             stack.append(i)
@@ -32,4 +33,5 @@ def longest_valid_parentheses(S: str) -> int:
                 stack.append(i)
             else:
                 max_len = max(max_len, i - stack[-1])
+
     return max_len

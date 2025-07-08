92fe0a5030c45d929f404762259fca0f74b2b334
Viktor <viktor.sbruev.1999@gmail.com>
2025-07-08 21:50:02 +0300

    Formatted task 78

--- Medium/78_subsets.py
+++ Medium/78_subsets.py
@@ -1,7 +1,6 @@
 '''
 Given an integer array numbers of unique elements, return all possible subsets.
-The solution set must not contain duplicate subsets. 
-You can return the subsets in any order.
+The solution set must not contain duplicate subsets. You can return the subsets in any order.
 
 Examples:
 Input: numbers = [1,2,3]  
@@ -10,10 +9,8 @@ Output: [[], [1], [2], [1,2], [3], [1,3], [2,3], [1,2,3]]
 Input: numbers = [0]  
 Output: [[], [0]]
 
-Дан целочисленный массив numbers, содержащий уникальные элементы. 
-Необходимо вернуть все возможные подмножества этого массива.
-Результат не должен содержать повторяющихся подмножеств. 
-Порядок возвращаемых подмножеств может быть любым.
+Дан целочисленный массив numbers, содержащий уникальные элементы. Необходимо вернуть все возможные подмножества этого массива.
+Результат не должен содержать повторяющихся подмножеств. Порядок возвращаемых подмножеств может быть любым.
 
 Примеры:
 Ввод: numbers = [1,2,3]  

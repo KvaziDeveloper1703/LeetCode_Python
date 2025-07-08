f4cbee10cc7a3384b2f3ea080913ab1257c2e8e5
Viktor <viktor.sbruev.1999@gmail.com>
2025-07-08 23:26:56 +0300

    Formatted task 129

--- Medium/129_sum_root_to_leaf_numbers.py
+++ Medium/129_sum_root_to_leaf_numbers.py
@@ -1,10 +1,10 @@
 '''
 You are given the root of a binary tree containing digits from 0 to 9 only.
 Each root-to-leaf path in the tree represents a number.
+
 For example, the path 1 -> 2 -> 3 represents the number 123.
 
-Return the total sum of all root-to-leaf numbers.
-A leaf node is a node with no children.
+Return the total sum of all root-to-leaf numbers. A leaf node is a node with no children.
 
 Example:
 Input: root = [1,2,3]
@@ -12,10 +12,10 @@ Output: 25
 
 Дано бинарное дерево, в каждом узле которого содержится одна цифра от 0 до 9.
 Каждый путь от корня до листа дерева представляет собой число, составленное из цифр вдоль этого пути.
+
 Например, путь 1 → 2 → 3 представляет число 123.
 
-Нужно вернуть сумму всех таких чисел, образованных от корня до каждого листа.
-Лист — это узел, у которого нет детей.
+Нужно вернуть сумму всех таких чисел, образованных от корня до каждого листа. Лист — это узел, у которого нет детей.
 
 Пример:
 Ввод: root = [1,2,3]
@@ -30,17 +30,16 @@ class TreeNode:
         self.left = left
         self.right = right
 
-class Solution:
-    def sum_numbers(self, root: Optional[TreeNode]) -> int:
-        def dfs(current_node: TreeNode, current_number: int) -> int:
-            if not current_node:
-                return 0
+def sum_numbers(root: Optional[TreeNode]) -> int:
+    def dfs(current_node: TreeNode, current_number: int) -> int:
+        if not current_node:
+            return 0
 
-            current_number = current_number * 10 + current_node.value
+        current_number = current_number * 10 + current_node.value
 
-            if not current_node.left and not current_node.right:
-                return current_number
+        if not current_node.left and not current_node.right:
+            return current_number
 
-            return dfs(current_node.left, current_number) + dfs(current_node.right, current_number)
+        return dfs(current_node.left, current_number) + dfs(current_node.right, current_number)
 
-        return dfs(root, 0)
+    return dfs(root, 0)

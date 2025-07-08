396b5d0f918f3a2b6acff39abb27a667c282c157
Viktor <viktor.sbruev.1999@gmail.com>
2025-07-08 21:02:04 +0300

    Formatted task 57

--- Medium/57_insert_interval.py
+++ Medium/57_insert_interval.py
@@ -1,8 +1,11 @@
 '''
-You are given an array of non-overlapping intervals intervals where intervals[i] = [start_index, end_index] represent the start and the end of the ith interval. The intervals are sorted in ascending order by start_index. You are also given an interval new_interval = [start, end] that represents the start and end of another interval.
-Insert new_interval into intervals such that intervals is still sorted in ascending order by start_index and there are no overlapping intervals (merge overlapping intervals if necessary).
-Return the updated intervals after the insertion.
-Note that you don't need to modify intervals in-place. You can create a new array and return it.
+You are given an array of non-overlapping intervals intervals where intervals[i] = [start_index, end_index] represent the start and the end of the ith interval. 
+The intervals are sorted in ascending order by start_index. 
+You are also given an interval new_interval = [start, end] that represents the start and end of another interval.
+
+Insert new_interval into intervals such that intervals is still sorted in ascending order by start_index and there are no overlapping intervals.
+
+Return the updated intervals after the insertion. Note that you don't need to modify intervals in-place. You can create a new array and return it.
 
 Examples:
 Input: intervals = [[1,3],[6,9]], new_interval = [2,5]
@@ -11,10 +14,14 @@ Output: [[1,5],[6,9]]
 Input: intervals = [[1,2],[3,5],[6,7],[8,10],[12,16]], new_interval = [4,8]
 Output: [[1,2],[3,10],[12,16]]
 
-Дан массив непересекающихся интервалов intervals, где intervals[i] = [начало, конец] — это начало и конец i-го интервала. Интервалы отсортированы по возрастанию начала. Также дан новый интервал new_interval = [start, end], который нужно вставить в массив.
+Дан массив непересекающихся интервалов intervals, где intervals[i] = [начало, конец] — это начало и конец i-го интервала. 
+Интервалы отсортированы по возрастанию начала. 
+Также дан новый интервал new_interval = [start, end], который нужно вставить в массив.
+
 Вставьте new_interval в intervals так, чтобы:
-+ Массив остался отсортированным по началу интервала;
-+ В итоговом массиве не было пересекающихся интервалов — объедините перекрывающиеся, если потребуется.
+    + Массив остался отсортированным по началу интервала;
+    + В итоговом массиве не было пересекающихся интервалов — объедините перекрывающиеся, если потребуется.
+
 Верните обновлённый массив интервалов после вставки.
 
 Примеры:

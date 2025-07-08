44b161dd9828c0141ae4543785c3658caf81ec52
Viktor <viktor.sbruev.1999@gmail.com>
2025-07-08 22:46:22 +0300

    Formatted task 103

--- Medium/103_binary_tree_zigzag_level_order_traversal.py
+++ Medium/103_binary_tree_zigzag_level_order_traversal.py
@@ -1,8 +1,8 @@
 '''
 Given the root of a binary tree, return the zigzag level order traversal of its nodes' values:
-+ First level: left to right;
-+ Second level: right to left;
-+ Third level: left to right.
+    + First level: left to right;
+    + Second level: right to left;
+    + Third level: left to right.
 
 Alternate directions at each level
 
@@ -11,9 +11,9 @@ Input: root = [3,9,20,null,null,15,7]
 Output: [[3],[20,9],[15,7]]
 
 Дано бинарное дерево. Требуется вернуть зигзагообразный уровневой обход значений его узлов:
-+ Первый уровень — слева направо;
-+ Второй уровень — справа налево;
-+ Третий — снова слева направо.
+    + Первый уровень — слева направо;
+    + Второй уровень — справа налево;
+    + Третий — снова слева направо.
 
 И так далее, чередуя направление на каждом уровне
 
@@ -31,33 +31,32 @@ class TreeNode:
         self.left = left
         self.right = right
 
-class Solution:
-    def zigzag_level_order(self, root: Optional[TreeNode]) -> List[List[int]]:
-        if not root:
-            return []
+def zigzag_level_order(root: Optional[TreeNode]) -> List[List[int]]:
+    if not root:
+        return []
 
-        result = []
-        queue = deque([root])
-        left_to_right = True
+    result = []
+    queue = deque([root])
+    left_to_right = True
 
-        while queue:
-            level_size = len(queue)
-            current_level = deque()
+    while queue:
+        level_size = len(queue)
+        current_level = deque()
 
-            for _ in range(level_size):
-                node = queue.popleft()
+        for _ in range(level_size):
+            node = queue.popleft()
 
-                if left_to_right:
-                    current_level.append(node.value)
-                else:
-                    current_level.appendleft(node.value)
+            if left_to_right:
+                current_level.append(node.value)
+            else:
+                current_level.appendleft(node.value)
 
-                if node.left:
-                    queue.append(node.left)
-                if node.right:
-                    queue.append(node.right)
+            if node.left:
+                queue.append(node.left)
+            if node.right:
+                queue.append(node.right)
 
-            result.append(list(current_level))
-            left_to_right = not left_to_right
+        result.append(list(current_level))
+        left_to_right = not left_to_right
 
-        return result
+    return result

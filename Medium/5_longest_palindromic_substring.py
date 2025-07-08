99583f89a392d965bc95542df0d742ef7de79783
Viktor <viktor.sbruev.1999@gmail.com>
2025-07-08 16:36:33 +0300

    Formatted task 5

--- Medium/5_longest_palindromic_substring.py
+++ Medium/5_longest_palindromic_substring.py
@@ -13,17 +13,22 @@ Output: "bab"
 '''
 
 def longest_palindrome(S: str) -> str:
+
     def expand_around_center(left: int, right: int) -> str:
         while left >= 0 and right < len(S) and S[left] == S[right]:
             left -= 1
             right += 1
         return S[left + 1:right]
+
     longest = ""
+
     for i in range(len(S)):
         palindrome_1 = expand_around_center(i, i)
         palindrome_2 = expand_around_center(i, i + 1)
+
         if len(palindrome_1) > len(longest):
             longest = palindrome_1
         if len(palindrome_2) > len(longest):
             longest = palindrome_2
+
     return longest

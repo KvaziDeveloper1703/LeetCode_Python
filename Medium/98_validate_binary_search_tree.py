a063a4270657001664d4a4648dc718ecf4909d20
Viktor <viktor.sbruev.1999@gmail.com>
2025-07-08 22:24:25 +0300

    Formatted task 98

--- Medium/98_validate_binary_search_tree.py
+++ Medium/98_validate_binary_search_tree.py
@@ -1,33 +1,31 @@
 '''
-You are given the root of a binary tree.
-Determine whether it is a valid Binary Search Tree (BST).
+You are given the root of a binary tree. Determine whether it is a valid Binary Search Tree.
 
 A binary tree is a valid BST if:
-+ The left subtree of a node contains only nodes with values less than the node’s value;
-+ The right subtree of a node contains only nodes with values greater than the node’s value;
-+ Both subtrees must also be valid BSTs.
+    + The left subtree of a node contains only nodes with values less than the node’s value;
+    + The right subtree of a node contains only nodes with values greater than the node’s value;
+    + Both subtrees must also be valid BSTs.
 
 Examples:
 Input: root = [2,1,3]
-Output: true
+Output: True
 
 Input: root = [5,1,4,null,null,3,6]
-Output: false
+Output: False
 
-Дан корень бинарного дерева.
-Определи, является ли оно корректным бинарным деревом поиска (BST).
+Дан корень бинарного дерева. Определи, является ли оно корректным бинарным деревом поиска.
 
 Бинарное дерево считается корректным BST, если выполняются условия:
-+ В левом поддереве узла находятся только значения меньше значения самого узла;
-+ В правом поддереве узла — только значения больше;
-+ Оба поддерева также должны быть корректными BST.
+    + В левом поддереве узла находятся только значения меньше значения самого узла;
+    + В правом поддереве узла — только значения больше;
+    + Оба поддерева также должны быть корректными BST.
 
 Примеры:
 Ввод: root = [2,1,3]
-Вывод: true
+Вывод: True
 
 Ввод: root = [5,1,4,null,null,3,6]
-Вывод: false
+Вывод: False
 '''
 
 from typing import Optional
@@ -38,16 +36,16 @@ class TreeNode:
         self.left = left
         self.right = right
 
-class Solution:
-    def isValidBST(self, root: Optional[TreeNode]) -> bool:
-        def validate(node: Optional[TreeNode], low: float, high: float) -> bool:
-            if not node:
-                return True
-            if not (low < node.value < high):
-                return False
-            return (
-                validate(node.left, low, node.value) and
-                validate(node.right, node.value, high)
-            )
+def is_valid_BST(root: Optional[TreeNode]) -> bool:
+    def validate(node: Optional[TreeNode], low: float, high: float) -> bool:
+        if not node:
+            return True
 
-        return validate(root, float('-inf'), float('inf'))
+        if not (low < node.value < high):
+            return False
+        return (
+            validate(node.left, low, node.value) and
+            validate(node.right, node.value, high)
+        )
+
+    return validate(root, float('-inf'), float('inf'))

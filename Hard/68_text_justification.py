8c918b8ceaf2dbc095cea82f4ec5f275f86e90fd
Viktor <viktor.sbruev.1999@gmail.com>
2025-07-08 15:45:29 +0300

    Formatted task 68

--- Hard/68_text_justification.py
+++ Hard/68_text_justification.py
@@ -3,36 +3,33 @@ Given an array of words and an integer max_width, format the text so that each l
 Use a greedy approach: fill each line with as many words as possible.
 
 Rules:
-+ Add spaces between words to make the line exactly max_width characters long;
-+ Distribute spaces as evenly as possible between words;
-+ If extra spaces remain, put more on the left;
-+ The last line should be left-justified, with words separated by a single space and any extra spaces added at the end.
+    + Add spaces between words to make the line exactly max_width characters long;
+    + Distribute spaces as evenly as possible between words;
+    + If extra spaces remain, put more on the left;
+    + The last line should be left-justified, with words separated by a single space and any extra spaces added at the end.
 
 Example:
-Input: 
-  words = ["This", "is", "an", "example", "of", "text", "justification."], 
-  max_width = 16
-Output: ["This    is    an",
-         "example  of text",
-         "justification.  "
-      ]
+Input: words = ["This", "is", "an", "example", "of", "text", "justification."], max_width = 16
+Output: [   "This    is    an",
+            "example  of text",
+            "justification.  "
+    ]
 
 Дан массив слов words и целое число max_width. Необходимо отформатировать текст так, чтобы каждая строка содержала ровно max_width символов и была полностью выровнена по ширине.
 Используйте жадный подход: заполняйте каждую строку максимально возможным количеством слов.
 
-Правила форматирования:
-+ Между словами нужно вставить пробелы так, чтобы длина строки была ровно max_width символов;
-+ Пробелы распределяются как можно равномернее между словами;
-+ Если остаются лишние пробелы, они добавляются в начале между левыми словами;
-+ Последняя строка должна быть выровнена по левому краю — между словами ставится по одному пробелу, а оставшиеся пробелы добавляются в конец строки.
+Правила:
+    + Между словами нужно вставить пробелы так, чтобы длина строки была ровно max_width символов;
+    + Пробелы распределяются как можно равномернее между словами;
+    + Если остаются лишние пробелы, они добавляются в начале между левыми словами;
+    + Последняя строка должна быть выровнена по левому краю — между словами ставится по одному пробелу, а оставшиеся пробелы добавляются в конец строки.
 
 Пример:
-Ввод: 
-  words = ["This", "is", "an", "example", "of", "text", "justification."]  
-  max_width = 16
+Ввод: words = ["This", "is", "an", "example", "of", "text", "justification."], max_width = 16
 Вывод: ["This    is    an",  
         "example  of text",  
-        "justification.  "]
+        "justification.  "
+    ]
 '''
 
 from typing import List

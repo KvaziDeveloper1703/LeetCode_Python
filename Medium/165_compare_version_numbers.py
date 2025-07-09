daf95cbf9cf70c04f3aae31b97bde1984e845cbf
Viktor <viktor.sbruev.1999@gmail.com>
2025-07-09 17:32:53 +0300

    Formatted task 165

--- Medium/165_compare_version_numbers.py
+++ Medium/165_compare_version_numbers.py
@@ -1,56 +1,54 @@
 '''
-Given two version strings version1 and version2, compare them.
-A version string consists of revisions separated by dots '.'. Each revision is treated as an integer, ignoring any leading zeros.
+Given two version strings version_1 and version_2, compare them. A version string consists of revisions separated by dots '.'. Each revision is treated as an integer, ignoring any leading zeros.
 
 To compare version strings:
-+ Compare each revision from left to right.
-+ If one version has fewer revisions, treat the missing ones as 0.
+    + Compare each revision from left to right;
+    + If one version has fewer revisions, treat the missing ones as 0.
 
 Return:
-+ -1 if version1 < version2
-+ 1 if version1 > version2
-+ 0 if they are equal
+    + -1 if version_1 < version_2;
+    + 1 if version_1 > version_2;
+    + 0 if they are equal.
 
 Examples:
-Input: version1 = "1.2", version2 = "1.10"
+Input: version_1 = "1.2", version_2 = "1.10"
 Output: -1
 
-Input: version1 = "1.01", version2 = "1.001"
+Input: version_1 = "1.01", version_2 = "1.001"
 Output: 0
 
-Даны две строки version1 и version2, представляющие версии.
-Версия состоит из числовых ревизий, разделённых точками '.'. Каждую ревизию нужно интерпретировать как целое число, игнорируя ведущие нули.
+Даны две строки version_1 и version_2, представляющие версии. Версия состоит из числовых ревизий, разделённых точками '.'. Каждую ревизию нужно интерпретировать как целое число, игнорируя ведущие нули.
 
 Сравнение выполняется:
-+ Слева направо, по каждой ревизии.
-+ Если в одной версии ревизий меньше, недостающие считаются равными 0.
+    + Слева направо, по каждой ревизии;
+    + Если в одной версии ревизий меньше, недостающие считаются равными 0.
 
 Верните:
-+ -1, если version1 < version2
-+ 1, если version1 > version2
-+ 0, если версии равны
+    + -1, если version_1 < version_2;
+    + 1, если version_1 > version_2;
+    + 0, если версии равны.
 
 Примеры:
-Ввод: version1 = "1.2", version2 = "1.10"
+Ввод: version_1 = "1.2", version_2 = "1.10"
 Вывод: -1
 
-Ввод: version1 = "1.01", version2 = "1.001"
+Ввод: version_1 = "1.01", version_2 = "1.001"
 Вывод: 0
 '''
 
-def compare_version(version1: str, version2: str) -> int:
-    v1_parts = list(map(int, version1.split('.')))
-    v2_parts = list(map(int, version2.split('.')))
+def compare_version(version_1: str, version_2: str) -> int:
+    version_1_parts = list(map(int, version_1.split('.')))
+    version_2_parts = list(map(int, version2.split('.')))
 
-    max_length = max(len(v1_parts), len(v2_parts))
+    max_length = max(len(version_1_parts), len(version_2_parts))
 
-    v1_parts.extend([0] * (max_length - len(v1_parts)))
-    v2_parts.extend([0] * (max_length - len(v2_parts)))
+    version_1_parts.extend([0] * (max_length - len(version_1_parts)))
+    version_2_parts.extend([0] * (max_length - len(version_2_parts)))
 
     for i in range(max_length):
-        if v1_parts[i] > v2_parts[i]:
+        if version_1_parts[i] > version_2_parts[i]:
             return 1
-        elif v1_parts[i] < v2_parts[i]:
+        elif version_1_parts[i] < version_2_parts[i]:
             return -1
 
     return 0

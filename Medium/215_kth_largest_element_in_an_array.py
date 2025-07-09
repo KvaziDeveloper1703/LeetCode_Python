ae764e76b78660dda16c64e65982109f8193cdfc
Viktor <viktor.sbruev.1999@gmail.com>
2025-07-10 01:29:51 +0300

    Formatted task 215

--- Medium/215_kth_largest_element_in_an_array.py
+++ Medium/215_kth_largest_element_in_an_array.py
@@ -1,8 +1,9 @@
 '''
-Given an integer array numbers and an integer k, return the kth largest element in the array.
-Note that the kth largest means the element that would be in the k-th position from the end if the array were sorted in ascending order. 
-It does not have to be a distinct element.
-Can you solve this without fully sorting the array?
+Given an integer array numbers and an integer k.
+
+Return the kth largest element in the array.
+
+Note that the kth largest means the element that would be in the k-th position from the end if the array were sorted in ascending order.
 
 Examples:
 Input: numbers = [3, 2, 1, 5, 6, 4], k = 2
@@ -11,10 +12,11 @@ Output: 5
 Input: numbers = [3, 2, 3, 1, 2, 4, 5, 5, 6], k = 4
 Output: 4
 
-Дан массив целых чисел numbers и целое число k. Требуется вернуть k-й по величине элемент в массиве.
+Дан массив целых чисел numbers и целое число k. 
+
+Требуется вернуть k-й по величине элемент в массиве.
+
 Обратите внимание: k-й по величине означает элемент, который будет на k-м месте с конца при сортировке массива по возрастанию. 
-Элемент не обязан быть уникальным.
-Можно ли решить эту задачу без полной сортировки массива?
 
 Примеры:
 Ввод: numbers = [3, 2, 1, 5, 6, 4], k = 2
@@ -27,13 +29,13 @@ Output: 4
 import heapq
 from typing import List
 
-def findKthLargest(number_list: List[int], k: int) -> int:
-    smallest_heap_of_k_largest = number_list[:k]
+def find_kth_largest(numbers: List[int], k: int) -> int:
+    smallest_heap_of_k_largest = numbers[:k]
     heapq.heapify(smallest_heap_of_k_largest)
 
-    for current_number in number_list[k:]:
+    for current_number in numbers[k:]:
         if current_number > smallest_heap_of_k_largest[0]:
             heapq.heappop(smallest_heap_of_k_largest)
             heapq.heappush(smallest_heap_of_k_largest, current_number)
-    
+
     return smallest_heap_of_k_largest[0]

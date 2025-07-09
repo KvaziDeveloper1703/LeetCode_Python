b4db7d1c7fbdcb90733795caea16e9c7379f6c5b
Viktor <viktor.sbruev.1999@gmail.com>
2025-07-09 17:22:13 +0300

    Formatted task 164

--- Medium/164_maximum_gap.py
+++ Medium/164_maximum_gap.py
@@ -1,6 +1,7 @@
 '''
-Given an integer array numbers, return the maximum difference between two successive elements in its sorted form.
-If the array contains less than two elements, return 0.
+Given an integer array numbers.
+
+Return the maximum difference between two successive elements in its sorted form. If the array contains less than two elements, return 0.
 
 You must write an algorithm that runs in linear time (O(n)) and uses linear extra space (O(n)).
 
@@ -11,8 +12,9 @@ Output: 3
 Input: numbers = [10]
 Output: 0
 
-Дан массив целых чисел numbers. Верните максимальную разницу между двумя соседними элементами в отсортированной версии этого массива.
-Если в массиве меньше двух элементов, верните 0.
+Дан массив целых чисел numbers. 
+
+Верните максимальную разницу между двумя соседними элементами в отсортированной версии этого массива. Если в массиве меньше двух элементов, верните 0.
 
 Необходимо реализовать алгоритм с линейной сложностью по времени (O(n)) и линейной дополнительной памятью (O(n)).
 
@@ -29,23 +31,28 @@ from typing import List
 def maximum_gap(numbers: List[int]) -> int:
     if len(numbers) < 2:
         return 0
+
     min_value, max_value = min(numbers), max(numbers)
     if min_value == max_value:
         return 0
+
     N = len(numbers)
     bucket_size = max(1, (max_value - min_value) // (N - 1))
     bucket_count = (max_value - min_value) // bucket_size + 1
     buckets_min = [float('inf')] * bucket_count
     buckets_max = [float('-inf')] * bucket_count
+
     for number in numbers:
         idx = (number - min_value) // bucket_size
         buckets_min[idx] = min(buckets_min[idx], number)
         buckets_max[idx] = max(buckets_max[idx], number)
     max_gap = 0
     previous_max = min_value
+
     for i in range(bucket_count):
         if buckets_min[i] == float('inf'):
             continue
         max_gap = max(max_gap, buckets_min[i] - previous_max)
         previous_max = buckets_max[i]
+
     return max_gap

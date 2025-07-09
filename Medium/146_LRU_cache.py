3e601313cbd8fe3c9ab08c5eabaefaf423098942
Viktor <viktor.sbruev.1999@gmail.com>
2025-07-09 16:58:01 +0300

    Formatted task 146

--- Medium/146_LRU_cache.py
+++ Medium/146_LRU_cache.py
@@ -1,17 +1,15 @@
 '''
-Design a data structure for a Least Recently Used (LRU) cache. Implement the LRUCache class with the following methods:
-+ LRUCache(int capacity): Initializes the cache with a positive size capacity;
-+ int get(int key): Returns the value of the key if it exists, otherwise returns -1;
-+ void put(int key, int value): Updates the value of the key if it exists, or adds the key-value pair. If the cache exceeds capacity, evicts the least recently used key.
+Design a data structure for a Least Recently Used cache. Implement the LRUCache class with the following methods:
+    + LRUCache(int capacity): Initializes the cache with a positive size capacity;
+    + int get(int key): Returns the value of the key if it exists, otherwise returns -1;
+    + void put(int key, int value): Updates the value of the key if it exists, or adds the key-value pair. If the cache exceeds capacity, evicts the least recently used key.
 
 Both get and put must run in O(1) time complexity.
 
 Разработайте структуру данных для кэша с вытеснением наименее недавно использованного элемента. Реализуйте класс LRUCache со следующими методами:
-+ LRUCache(int capacity): Инициализирует кэш с положительной ёмкостью capacity;
-+ int get(int key): Возвращает значение по ключу, если оно существует в кэше, иначе возвращает -1;
-+ void put(int key, int value): Обновляет значение по ключу, если он уже существует, или добавляет новую пару ключ-значение.
-
-Если при этом размер кэша превышает заданную ёмкость, то необходимо удалить наименее недавно использованный ключ.
+    + LRUCache(int capacity): Инициализирует кэш с положительной ёмкостью capacity;
+    + int get(int key): Возвращает значение по ключу, если оно существует в кэше, иначе возвращает -1;
+    + void put(int key, int value): Обновляет значение по ключу, если он уже существует, или добавляет новую пару ключ-значение. Если при этом размер кэша превышает заданную ёмкость, то необходимо удалить наименее недавно использованный ключ.
 
 Обе операции — get и put — должны выполняться за O(1) по времени.
 '''

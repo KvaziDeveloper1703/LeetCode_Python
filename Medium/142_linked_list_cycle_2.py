72245c8187653d660dfa4915857e31daf676e9d2
Viktor <viktor.sbruev.1999@gmail.com>
2025-07-09 16:56:21 +0300

    Formatted task 142

--- Medium/142_linked_list_cycle_2.py
+++ Medium/142_linked_list_cycle_2.py
@@ -1,51 +1,56 @@
 '''
-Given the head of a singly linked list, return the node where the cycle begins. If there is no cycle, return null.
-A cycle exists if a node in the list can be reached again by continuously following the next pointer.
+Given the head of a singly linked list.
 
-Note: Internally, pos is used to denote the index where the tail connects to form a cycle. It is not passed as an argument, and you should not modify the linked list.
+Return the node where the cycle begins. If there is no cycle, return null.
+
+A cycle exists if a node in the list can be reached again by continuously following the next pointer.
+Internally, position is used to denote the index where the tail connects to form a cycle. 
+It is not passed as an argument, and you should not modify the linked list.
 
 Examples:
-Input: head = [3,2,0,-4], pos = 1
+Input: head = [3,2,0,-4], position = 1
 Output: Node at index 1
 
-Input: head = [1,2], pos = 0
+Input: head = [1,2], position = 0
 Output: Node at index 0
 
-Дан указатель head на начало односвязного списка. Верните узел, в котором начинается цикл. Если цикла нет, верните null.
-Цикл существует, если, следуя по next, можно попасть на уже посещённый узел.
+Дан указатель head на начало односвязного списка. 
 
-Примечание: параметр pos обозначает индекс узла, к которому соединяется хвост (0-индексация). Он не передаётся в функцию. Список нельзя модифицировать.
+Верните узел, в котором начинается цикл. Если цикла нет, верните null.
+
+Цикл существует, если, следуя по next, можно попасть на уже посещённый узел.
+Параметр position обозначает индекс узла, к которому соединяется хвост. 
+Он не передаётся в функцию. Список нельзя модифицировать.
 
 Примеры:
-Ввод: head = [3,2,0,-4], pos = 1
+Ввод: head = [3,2,0,-4], position = 1
 Вывод: Узел с индексом 1
 
-Ввод: head = [1,2], pos = 0
+Ввод: head = [1,2], position = 0
 Вывод: Узел с индексом 0
 '''
 
 from typing import Optional
 
 class ListNode:
-    def __init__(self, val: int = 0, next: Optional['ListNode'] = None):
-        self.val = val
+    def __init__(self, value: int = 0, next: Optional['ListNode'] = None):
+        self.value = value
         self.next = next
 
-class Solution:
-    def detectCycle(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        slow = fast = head
+def detect_cycle(head: Optional[ListNode]) -> Optional[ListNode]:
+    slow = fast = head
 
-        while fast and fast.next:
-            slow = slow.next
-            fast = fast.next.next
-            if slow == fast:
-                break
-        else:
-            return None
+    while fast and fast.next:
+        slow = slow.next
+        fast = fast.next.next
+        if slow == fast:
+            break
+    else:
+        return None
 
-        slow = head
-        while slow != fast:
-            slow = slow.next
-            fast = fast.next
+    slow = head
+    while slow != fast:
+        slow = slow.next
+        fast = fast.next
 
-        return slow
+    return slow

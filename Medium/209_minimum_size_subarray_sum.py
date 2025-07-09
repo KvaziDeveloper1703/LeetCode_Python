1784203a1d393ac850d87afbd009d88cb2a2ddc5
Viktor <viktor.sbruev.1999@gmail.com>
2025-07-09 18:05:49 +0300

    Formatted task 209

--- Medium/209_minimum_size_subarray_sum.py
+++ Medium/209_minimum_size_subarray_sum.py
@@ -1,7 +1,7 @@
 '''
 You are given an array of positive integers numbers and a positive integer target.
-Find the minimal length of a contiguous subarray of which the sum is greater than or equal to target. 
-If no such subarray exists, return 0.
+
+Find the minimal length of a contiguous subarray of which the sum is greater than or equal to target. If no such subarray exists, return 0.
 
 Examples:
 Input: target = 7, numbers = [2,3,1,2,4,3]
@@ -11,8 +11,8 @@ Input: target = 4, numbers = [1,4,4]
 Output: 1
 
 Дан массив положительных целых чисел numbers и положительное целое число target.
-Найдите минимальную длину непрерывного подмассива, сумма элементов которого больше либо равна target.
-Если такого подмассива не существует, верните 0.
+
+Найдите минимальную длину непрерывного подмассива, сумма элементов которого больше либо равна target. Если такого подмассива не существует, верните 0.
 
 Примеры:
 Ввод: target = 7, numbers = [2, 3, 1, 2, 4, 3]
@@ -24,7 +24,7 @@ Output: 1
 
 from typing import List
 
-def min_sub_array_length(target: int, numbers: List[int]) -> int:
+def minimum_sub_array_length(target: int, numbers: List[int]) -> int:
     left = 0
     total = 0
     min_length = float('inf')

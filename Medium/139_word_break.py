044d88c7001af57bfa4395250d99db4168c7a30e
Viktor <viktor.sbruev.1999@gmail.com>
2025-07-09 16:50:52 +0300

    Formatted task 139

--- Medium/139_word_break.py
+++ Medium/139_word_break.py
@@ -1,37 +1,38 @@
 '''
-Given a string S and a list of strings word_dict representing a dictionary, return true if S can be segmented into a space-separated sequence of one or more words from the dictionary.
-You may reuse the same word from the dictionary multiple times in the segmentation.
+Given a string S and a list of strings dictionary_of_words representing a dictionary.
+
+Return True if S can be segmented into a space-separated sequence of one or more words from the dictionary. You may reuse the same word from the dictionary multiple times in the segmentation.
 
 Examples:
-Input: S = "leetcode", word_dict = ["leet", "code"]
-Output: true
+Input: S = "leetcode", dictionary_of_words = ["leet", "code"]
+Output: True
 
-Input: S = "applepenapple", word_dict = ["apple", "pen"]
-Output: true
+Input: S = "applepenapple", dictionary_of_words = ["apple", "pen"]
+Output: True
 
-Дана строка S и список строк word_dict, представляющий словарь. Верните true, если строку S можно разбить на последовательность слов из словаря, разделённых пробелами.
+Дана строка S и список строк dictionary_of_words, представляющий словарь. Верните True, если строку S можно разбить на последовательность слов из словаря, разделённых пробелами.
 Разрешается многократно использовать одни и те же слова из словаря.
 
 Примеры:
-Вход: S = "leetcode", word_dict = ["leet", "code"]
-Выход: true
+Ввод: S = "leetcode", dictionary_of_words = ["leet", "code"]
+Вывод: True
 
-Вход: S = "applepenapple", word_dict = ["apple", "pen"]
-Выход: true
+Ввод: S = "applepenapple", dictionary_of_words = ["apple", "pen"]
+Вывод: True
 '''
 
 from typing import List
 
-def word_break(input_string: str, dictionary_words: List[str]) -> bool:
-    word_set = set(dictionary_words)
-    can_be_segmented = [False] * (len(input_string) + 1)
+def word_break(S: str, dictionary_of_words: List[str]) -> bool:
+    set_of_words = set(dictionary_of_words)
+    can_be_segmented = [False] * (len(S) + 1)
     can_be_segmented[0] = True
 
-    for end_index in range(1, len(input_string) + 1):
+    for end_index in range(1, len(S) + 1):
         for start_index in range(end_index):
-            word = input_string[start_index:end_index]
-            if can_be_segmented[start_index] and word in word_set:
+            word = S[start_index:end_index]
+            if can_be_segmented[start_index] and word in set_of_words:
                 can_be_segmented[end_index] = True
                 break
 
-    return can_be_segmented[len(input_string)]
+    return can_be_segmented[len(S)]

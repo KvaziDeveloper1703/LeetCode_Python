a983633572c932072658aabc043ddf597f72e84b
Viktor <viktor.sbruev.1999@gmail.com>
2025-07-09 17:20:42 +0300

    Formatted task 155

--- Medium/155_min_stack.py
+++ Medium/155_min_stack.py
@@ -1,26 +1,26 @@
 '''
 Design a stack that supports push, pop, top, and retrieving the minimum element in constant time.
+
 Implement the MinStack class:
-+ MinStack() initializes the stack object;
-+ void push(int value) pushes the element value onto the stack;
-+ void pop() removes the element on the top of the stack;
-+ int top() gets the top element of the stack;
-+ int get_min() retrieves the minimum element in the stack.
+    + MinStack() initializes the stack object;
+    + void push(int value) pushes the element value onto the stack;
+    + void pop() removes the element on the top of the stack;
+    + int top() gets the top element of the stack;
+    + int get_min() retrieves the minimum element in the stack.
 
 You must implement a solution with O(1) time complexity for each function.
 
-Реализуйте стек MinStack, поддерживающий следующие операции за O(1):
-+ MinStack() — инициализирует объект стека;
-+ void push(int value) — помещает элемент value в стек;
-+ void pop() — удаляет элемент с вершины стека;
-+ int top() — возвращает верхний элемент стека;
-+ int get_min() — возвращает минимальный элемент в стеке.
+Реализуйте стек MinStack:
+    + MinStack() — инициализирует объект стека;
+    + void push(int value) — помещает элемент value в стек;
+    + void pop() — удаляет элемент с вершины стека;
+    + int top() — возвращает верхний элемент стека;
+    + int get_min() — возвращает минимальный элемент в стеке.
 
 Все функции должны работать за O(1) времени.
 '''
 
 class MinStack:
-
     def __init__(self):
         self.stack = []
         self.min_stack = []

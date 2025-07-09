bfa77c7902098becbb6aa91e4ec4415cd0ee951a
Viktor <viktor.sbruev.1999@gmail.com>
2025-07-09 13:58:09 +0300

    Formatted task 138

--- Medium/138_copy_list_with_random_pointer.py
+++ Medium/138_copy_list_with_random_pointer.py
@@ -1,15 +1,33 @@
 '''
-A linked list of length N is given, where each node contains an additional random pointer that could point to any node in the list or be null.
-Construct a deep copy of the list. The deep copy should consist of exactly N brand new nodes, where each new node has its value set to the value of its corresponding original node. 
-Both the next and random pointers of the new nodes should point to new nodes in the copied list such that the pointers in the original list and copied list represent the same list state. 
-None of the pointers in the new list should point to nodes in the original list.
-For example, if two nodes X and Y in the original list are such that X.random --> Y, then in the copied list, x.random --> y.
+A linked list of length N is given, where each node contains an additional pointer random, which may point to any node in the list or be null.
 
-Return the head of the copied linked list.
+It is required to construct a deep copy of this list. The deep copy should consist of exactly N completely new nodes, in which:
+    + the value of each new node matches the value of the corresponding node in the original list,
+    + the next and random pointers of the new nodes should point to new nodes in the copied list so that the structure of connections fully replicates the structure of the original list,
+    + none of the pointers in the new list should point to nodes from the original list.
+
+For example, if there are two nodes X and Y in the original list such that X.random --> Y, then in the copied list for the corresponding nodes x and y it must hold that x.random --> y.
+
+You need to return the head of the new (copied) linked list.
 
 Example:
 Input: head = [[7,null],[13,0],[11,4],[10,2],[1,0]]
 Output: [[7,null],[13,0],[11,4],[10,2],[1,0]]
+
+Дана связанная (однонаправленная) список длины N, где каждый узел содержит дополнительный указатель random, который может указывать на любой узел в списке или быть равен null.
+
+Необходимо построить глубокую копию этого списка. Глубокая копия должна состоять ровно из N совершенно новых узлов, в которых:
+    + значение каждого нового узла совпадает со значением соответствующего узла исходного списка,
+    + указатели next и random новых узлов должны указывать на новые узлы в скопированном списке так, чтобы структура связей полностью повторяла структуру исходного списка,
+    + никакие указатели в новом списке не должны указывать на узлы исходного списка.
+
+Например, если в исходном списке два узла X и Y такие, что X.random --> Y, то в скопированном списке для соответствующих узлов x и y должно выполняться x.random --> y. 
+
+Нужно вернуть голову нового (скопированного) связанного списка.
+
+Пример:
+Ввод: head = [[7,null],[13,0],[11,4],[10,2],[1,0]]
+Вывод: [[7,null],[13,0],[11,4],[10,2],[1,0]]
 '''
 
 from typing import Optional

b1689023a21046fc88b3c45fb47ba4099517a5d1
Viktor <viktor.sbruev.1999@gmail.com>
2025-07-10 19:22:38 +0300

    Formatted task 452

--- Medium/452_minimum_number_of_arrows_to_burst_balloons.py
+++ Medium/452_minimum_number_of_arrows_to_burst_balloons.py
@@ -1,6 +1,11 @@
 '''
-There are some spherical balloons taped onto a flat wall that represents the XY-plane. The balloons are represented as a 2D integer array points, where points[i] = [x_start, x_end] denotes a balloon whose horizontal diameter stretches between x_start and x_end. You do not know the exact y-coordinates of the balloons.
-Arrows can be shot up directly vertically (in the positive y-direction) from different points along the x-axis. A balloon with x_start and x_end is burst by an arrow shot at x if x_start <= x <= x_end. There is no limit to the number of arrows that can be shot. A shot arrow keeps traveling up infinitely, bursting any balloons in its path.
+There are some spherical balloons taped onto a flat wall that represents the XY-plane.
+    + The balloons are represented as a 2D integer array points, where points[i] = [x_start, x_end] denotes a balloon whose horizontal diameter stretches between x_start and x_end;
+    + You do not know the exact y-coordinates of the balloons;
+    + Arrows can be shot up directly vertically (in the positive y-direction) from different points along the x-axis;
+    + A balloon with x_start and x_end is burst by an arrow shot at x if x_start <= x <= x_end;
+    + There is no limit to the number of arrows that can be shot. A shot arrow keeps traveling up infinitely, bursting any balloons in its path.
+
 Given the array points, return the minimum number of arrows that must be shot to burst all balloons.
 
 Examples:
@@ -10,8 +15,13 @@ Output: 2
 Input: points = [[1,2],[3,4],[5,6],[7,8]]
 Output: 4
 
-На плоской стене, представляющей собой координатную плоскость XY, прикреплены сферические воздушные шары. Каждый шар задан в виде двумерного массива points, где points[i] = [x_start, x_end] — это горизонтальный диаметр шара, простирающийся от x_start до x_end. Точные координаты по y неизвестны.
-Вы можете стрелять стрелами вверх (по положительному направлению оси Y) из любых точек на оси X. Одна стрела, пущенная из точки x, пробивает все шары, у которых x_start <= x <= x_end. Стрел можно выпустить сколько угодно, и они летят вверх бесконечно, пробивая все шары на своём пути.
+На плоской стене, представляющей собой координатную плоскость XY, прикреплены сферические воздушные шары.
+    + Каждый шар задан в виде двумерного массива points, где points[i] = [x_start, x_end] — это горизонтальный диаметр шара, простирающийся от x_start до x_end;
+    + Точные координаты по y неизвестны;
+    + Вы можете стрелять стрелами вверх (по положительному направлению оси Y) из любых точек на оси X;
+    + Одна стрела, пущенная из точки x, пробивает все шары, у которых x_start <= x <= x_end;
+    + Стрел можно выпустить сколько угодно, и они летят вверх бесконечно, пробивая все шары на своём пути.
+
 По заданному массиву points необходимо вернуть минимальное количество стрел, необходимых для того, чтобы пробить все шары.
 
 Примеры:

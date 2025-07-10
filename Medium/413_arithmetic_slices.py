763f6eec94272baf20e05d6ccd17348e77f3b524
Viktor <viktor.sbruev.1999@gmail.com>
2025-07-10 18:51:02 +0300

    Formatted task 413

--- Medium/413_arithmetic_slices.py
+++ Medium/413_arithmetic_slices.py
@@ -1,13 +1,9 @@
 '''
 An integer array is called arithmetic if it has at least three elements and the difference between any two consecutive elements is the same.
 
-Examples of arithmetic sequences:
-    + [1, 3, 5, 7, 9]
-    + [7, 7, 7, 7]
-    + [3, -1, -5, -9]
+Given an integer array numbers.
 
-Given an integer array numbers, return the number of all subarrays of nums that are arithmetic sequences.
-A subarray is a contiguous portion of the array.
+Return the number of all subarrays of nums that are arithmetic sequences.
 
 Examples:
 Input: numbers = [1, 2, 3, 4]
@@ -18,20 +14,16 @@ Output: 0
 
 Массив целых чисел называется арифметическим, если он содержит как минимум три элемента и разность между любыми двумя последовательными элементами одинаковая.
 
-Примеры арифметических последовательностей:
-    + [1, 3, 5, 7, 9]
-    + [7, 7, 7, 7]
-    + [3, -1, -5, -9]
+Дан массив целых чисел numbers. 
 
-Дан массив целых чисел numbers. Нужно вернуть количество всех подмассивов, которые являются арифметическими последовательностями.
-Подмассив — это непрерывная часть массива.
+Нужно вернуть количество всех подмассивов, которые являются арифметическими последовательностями.
 
 Примеры:
-Вход: numbers = [1, 2, 3, 4]
-Выход: 3
+Ввод: numbers = [1, 2, 3, 4]
+Вывод: 3
 
-Вход: numbers = [1]
-Выход: 0
+Ввод: numbers = [1]
+Вывод: 0
 '''
 
 from typing import List
@@ -42,7 +34,7 @@ def number_of_arithmetic_slices(numbers: List[int]) -> int:
 
     total = 0
     count = 0
-    
+
     for i in range(2, len(numbers)):
         if numbers[i] - numbers[i-1] == numbers[i-1] - numbers[i-2]:
             count += 1

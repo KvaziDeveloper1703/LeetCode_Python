abc54198453ab44a1c0b4888fa14a4d858edbbb1
Viktor <viktor.sbruev.1999@gmail.com>
2025-07-10 16:16:08 +0300

    Formatted task 235

--- Medium/235_lowest_common_ancestor_of_a_binary_search_tree.py
+++ Medium/235_lowest_common_ancestor_of_a_binary_search_tree.py
@@ -1,8 +1,7 @@
 '''
-Given a binary search tree (BST), find the lowest common ancestor (LCA) of two given nodes p and q.
+Given a binary search tree, find the lowest common ancestor of two given nodes p and q.
 
-According to Wikipedia:
-“The lowest common ancestor is defined between two nodes p and q as the lowest node in the tree T that has both p and q as descendants (where we allow a node to be a descendant of itself).”
+According to Wikipedia: “The lowest common ancestor is defined between two nodes p and q as the lowest node in the tree T that has both p and q as descendants (where we allow a node to be a descendant of itself).”
 
 Examples:
 Input: root = [6,2,8,0,4,7,9,null,null,3,5], p = 2, q = 8
@@ -11,10 +10,9 @@ Output: 6
 Input: root = [6,2,8,0,4,7,9,null,null,3,5], p = 2, q = 4
 Output: 2
 
-Дано дерево поиска (BST). Найдите наименьшего общего предка (LCA) для двух заданных узлов p и q.
+Дано дерево поиска. Найдите наименьшего общего предка для двух заданных узлов p и q.
 
-Согласно определению с Википедии:
-“Наименьший общий предок — это самый низкий узел в дереве T, который имеет оба узла p и q в качестве потомков (разрешается считать, что узел является потомком самого себя).”
+Согласно определению с Википедии: “Наименьший общий предок — это самый низкий узел в дереве T, который имеет оба узла p и q в качестве потомков (разрешается считать, что узел является потомком самого себя).”
 
 Примеры:
 Ввод: root = [6,2,8,0,4,7,9,null,null,3,5], p = 2, q = 8
@@ -30,14 +28,13 @@ class TreeNode:
         self.left = left
         self.right = right
 
-class Solution:
-    def lowest_common_ancestor(self, root: TreeNode, p: TreeNode, q: TreeNode) -> TreeNode:
-        current = root
+def lowest_common_ancestor(root: TreeNode, p: TreeNode, q: TreeNode) -> TreeNode:
+    current = root
 
-        while current:
-            if p.value < current.value and q.value < current.value:
-                current = current.left
-            elif p.value > current.value and q.value > current.value:
-                current = current.right
-            else:
-                return current
+    while current:
+        if p.value < current.value and q.value < current.value:
+            current = current.left
+        elif p.value > current.value and q.value > current.value:
+            current = current.right
+        else:
+            return current

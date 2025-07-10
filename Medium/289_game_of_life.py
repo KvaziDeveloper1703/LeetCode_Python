3c8bd859ec2c2cf2209bd470094f05f6635592fd
Viktor <viktor.sbruev.1999@gmail.com>
2025-07-10 16:36:40 +0300

    Formatted task 289

--- Medium/289_game_of_life.py
+++ Medium/289_game_of_life.py
@@ -1,13 +1,13 @@
 '''
 You are given a 2D grid board representing the current state of Conway's Game of Life, a cellular automaton. Each cell has two possible states:
-+ 1 — live;
-+ 0 — dead.
+    + 1 — live;
+    + 0 — dead.
 
 Each cell interacts with its eight neighbors (horizontal, vertical, and diagonal), and its next state is determined by the following rules:
-+ Underpopulation: Any live cell with fewer than two live neighbors dies;
-+ Survival: Any live cell with two or three live neighbors lives on to the next generation;
-+ Overpopulation: Any live cell with more than three live neighbors dies;
-+ Reproduction: Any dead cell with exactly three live neighbors becomes a live cell.
+    + Underpopulation: Any live cell with fewer than two live neighbors dies;
+    + Survival: Any live cell with two or three live neighbors lives on to the next generation;
+    + Overpopulation: Any live cell with more than three live neighbors dies;
+    + Reproduction: Any dead cell with exactly three live neighbors becomes a live cell.
 
 All cells are updated simultaneously, based on the current state of the grid.
 
@@ -15,33 +15,37 @@ Example:
 Input: board = [[0,1,0], 
                 [0,0,1], 
                 [1,1,1], 
-                [0,0,0]]
-Output: [[0,0,0], 
-         [1,0,1], 
-         [0,1,1], 
-         [0,1,0]]
+                [0,0,0]
+        ]
+Output: [   [0,0,0], 
+            [1,0,1], 
+            [0,1,1], 
+            [0,1,0]
+    ]
 
 Дана двумерная сетка board, представляющая текущее состояние Игры «Жизнь» Конвея — клеточного автомата. Каждая ячейка может находиться в одном из двух состояний:
-1 — живая клетка;
-0 — мёртвая клетка.
+    1 — живая клетка;
+    0 — мёртвая клетка.
 
 Каждая клетка взаимодействует с восемью соседями (по горизонтали, вертикали и диагонали). Состояние клетки в следующем поколении определяется по следующим правилам:
-+ Недостаточная популяция: Любая живая клетка с менее чем двумя живыми соседями умирает;
-+ Выживание: Любая живая клетка с двумя или тремя живыми соседями продолжает жить;
-+ Перенаселение: Любая живая клетка с более чем тремя живыми соседями умирает;
-+ Воспроизводство: Любая мёртвая клетка с ровно тремя живыми соседями становится живой.
+    + Недостаточная популяция: Любая живая клетка с менее чем двумя живыми соседями умирает;
+    + Выживание: Любая живая клетка с двумя или тремя живыми соседями продолжает жить;
+    + Перенаселение: Любая живая клетка с более чем тремя живыми соседями умирает;
+    + Воспроизводство: Любая мёртвая клетка с ровно тремя живыми соседями становится живой.
 
 Все клетки обновляются одновременно, основываясь на текущем состоянии сетки.
 
 Пример:
-Ввод: board = [[0, 1, 0],  
-               [0, 0, 1],  
-               [1, 1, 1],  
-               [0, 0, 0]]
+Ввод: board = [ [0, 1, 0],  
+                [0, 0, 1],  
+                [1, 1, 1],  
+                [0, 0, 0]
+        ]
 Вывод: [[0, 0, 0],  
         [1, 0, 1],  
         [0, 1, 1],  
-        [0, 1, 0]]
+        [0, 1, 0]
+    ]
 '''
 
 from typing import List
@@ -70,10 +74,10 @@ def game_of_life(board: List[List[int]]) -> None:
 
             if board[row_index][column_index] == 1:
                 if live_neighbors < 2 or live_neighbors > 3:
-                    board[row_index][column_index] = -1  # Will die
+                    board[row_index][column_index] = -1
             else:
                 if live_neighbors == 3:
-                    board[row_index][column_index] = 2  # Will become alive
+                    board[row_index][column_index] = 2
 
     for row_index in range(number_of_rows):
         for column_index in range(number_of_columns):

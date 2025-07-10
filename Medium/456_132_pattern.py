4787f3900aa4f74279b50cd9ba8661e6bc00a5c8
Viktor <viktor.sbruev.1999@gmail.com>
2025-07-10 19:27:43 +0300

    Formatted task 456

--- Medium/456_132_pattern.py
+++ Medium/456_132_pattern.py
@@ -1,30 +1,25 @@
 '''
-Given an array of n integers numbers, a 132 pattern is a subsequence of three elements numbers[i], numbers[j], and numbers[k] such that:
-    + i < j < k, and
-    + numbers[i] < numbers[k] < numbers[j]
+Given an array of n integers numbers, a 132 pattern is a subsequence of three elements numbers[i], numbers[j], and numbers[k] such that: i < j < k, and numbers[i] < numbers[k] < numbers[j]
 
-Return true if there is at least one such 132 pattern in the array. Otherwise, return false.
+Return True if there is at least one such 132 pattern in the array. Otherwise, return False.
 
 Examples:
 Input: numbers = [1, 2, 3, 4]
-Output: false
+Output: False
 
 Input: numbers = [3, 1, 4, 2]
-Output: true
+Output: True
 
-Дан массив из n целых чисел numbers.
-Шаблон 132 — это подпоследовательность из трёх элементов numbers[i], numbers[j] и numbers[k], такая что:
-    + i < j < k, и
-    + numbers[i] < numbers[k] < numbers[j]
+Дан массив из n целых чисел numbers. Шаблон 132 — это подпоследовательность из трёх элементов numbers[i], numbers[j] и numbers[k], такая что: i < j < k, и numbers[i] < numbers[k] < numbers[j]
 
-Верните true, если в массиве существует хотя бы один шаблон 132. В противном случае верните false.
+Верните True, если в массиве существует хотя бы один шаблон 132. В противном случае верните False.
 
 Примеры:
 Ввод: numbers = [1, 2, 3, 4]
-Вывод: false
+Вывод: False
 
 Ввод: numbers = [3, 1, 4, 2]
-Вывод: true
+Вывод: True
 '''
 
 from typing import List

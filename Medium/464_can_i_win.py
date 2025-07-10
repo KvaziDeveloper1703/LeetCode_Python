2f8454dbd6192619acb6946a6b5119f204835fca
Viktor <viktor.sbruev.1999@gmail.com>
2025-07-10 19:41:34 +0300

    Formatted task 464

--- Medium/464_can_i_win.py
+++ Medium/464_can_i_win.py
@@ -2,38 +2,37 @@
 In the "100 game", two players take turns adding numbers to a running total. Each player can choose any integer from 1 to 10, and the player who makes the total reach or exceed 100 wins.
 
 Now, imagine a variation of the game:
-    + Players cannot reuse numbers — once a number is picked, it’s removed from the pool.
-    + The pool consists of all integers from 1 to maxChoosableInteger.
-    + Players take turns picking unused numbers from the pool and adding them to the running total.
-    + The first player to make the total reach or exceed desiredTotal wins.
+    + Players cannot reuse numbers — once a number is picked, it’s removed from the pool;
+    + The pool consists of all integers from 1 to max_choosable_integer;
+    + Players take turns picking unused numbers from the pool and adding them to the running total;
+    + The first player to make the total reach or exceed desired_total wins.
 
-Given two integers maxChoosableInteger and desiredTotal, return true if the first player can force a win, assuming both players play optimally. Otherwise, return false.
+Given two integers max_choosable_integer and desired_total, return True if the first player can force a win, assuming both players play optimally. Otherwise, return False.
 
 Examples:
-Input: maxChoosableInteger = 10, desiredTotal = 11
-Output: false
+Input: max_choosable_integer = 10, desired_total = 11
+Output: False
 
-Input: maxChoosableInteger = 10, desiredTotal = 0
-Output: true
+Input: max_choosable_integer = 10, desired_total = 0
+Output: True
 
 В классической игре "100" два игрока по очереди добавляют к общей сумме числа от 1 до 10. Побеждает тот, кто первым доведёт сумму до 100 или больше.
 
 В изменённой версии игры:
     + Каждое число можно выбрать только один раз.
-    + Игроки берут числа по очереди из общего набора от 1 до maxChoosableInteger.
+    + Игроки берут числа по очереди из общего набора от 1 до max_choosable_integer.
     + После выбора число удаляется из набора.
-    + Цель — достичь или превысить desiredTotal.
+    + Цель — достичь или превысить desired_total.
 
 Побеждает тот, кто первый достигнет этой цели.
 
-Вам даны два числа: maxChoosableInteger и desiredTotal.
-Верните true, если первый игрок может гарантированно победить, если оба играют оптимально. Иначе верните false.
+Вам даны два числа: max_choosable_integer и desired_total. Верните True, если первый игрок может гарантированно победить, если оба играют оптимально. Иначе верните False.
 
 Примеры:
-Ввод: maxChoosableInteger = 10, desiredTotal = 11
-Вывод: false
+Ввод: max_choosable_integer = 10, desired_total = 11
+Вывод: False
 
-Ввод: maxChoosableInteger = 10, desiredTotal = 0
+Ввод: max_choosable_integer = 10, desired_total = 0
 Вывод: true
 """
 

a317e18978e731b665761d7957cf00ad2f9e2181
Viktor <viktor.sbruev.1999@gmail.com>
2025-07-10 17:57:41 +0300

    Formatted task 306

--- Medium/306_additive_number.py
+++ Medium/306_additive_number.py
@@ -2,22 +2,22 @@
 An additive number is a string whose digits can form an additive sequence.
 
 A valid additive sequence must:
-+ Contain at least three numbers.
-+ Each number (starting from the third) must be the sum of the previous two.
-+ No number in the sequence should have leading zeros.
+    + Contain at least three numbers;
+    + Each number starting from the third must be the sum of the previous two;
+    + No number in the sequence should have leading zeros.
 
-Given a string consisting only of digits, return true if it can form an additive number, otherwise return false.
+Given a string consisting only of digits, return True if it can form an additive number, otherwise return False.
 
 Examples:
 Input: "112358"
-Output: true
+Output: True
 
 Аддитивное число — это строка, состоящая из цифр, которая может быть разбита на последовательность чисел, удовлетворяющую следующим условиям:
-+ Последовательность должна содержать как минимум три числа.
-+ Начиная с третьего числа, каждое число должно быть суммой двух предыдущих.
-+ В последовательности не допускаются ведущие нули (например, "1, 2, 03" или "1, 02, 3" — неверные последовательности).
+    + Последовательность должна содержать как минимум три числа;
+    + Начиная с третьего числа, каждое число должно быть суммой двух предыдущих;
+    + В последовательности не допускаются ведущие нули.
 
-Дана строка, содержащая только цифры. Верните true, если она представляет аддитивное число, и false в противном случае.
+Дана строка, содержащая только цифры. Верните True, если она представляет аддитивное число, и False в противном случае.
 
 Примеры:
 Ввод: "112358"
@@ -32,8 +32,7 @@ def is_additive_number(number: str) -> bool:
             first_number_str = number[:first_end]
             second_number_str = number[first_end:second_end]
 
-            if (len(first_number_str) > 1 and first_number_str[0] == '0') or \
-               (len(second_number_str) > 1 and second_number_str[0] == '0'):
+            if (len(first_number_str) > 1 and first_number_str[0] == '0') or (len(second_number_str) > 1 and second_number_str[0] == '0'):
                 continue
 
             first_number = int(first_number_str)

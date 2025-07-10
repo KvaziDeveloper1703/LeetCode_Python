5a643d86149f48c5e9a350b50178efa68c6910d1
Viktor <viktor.sbruev.1999@gmail.com>
2025-07-10 16:30:25 +0300

    Formatted task 287

--- Medium/287_find_the_duplicate_number.py
+++ Medium/287_find_the_duplicate_number.py
@@ -1,5 +1,6 @@
 '''
-You are given an array of integers numbers containing n + 1 integers where each integer is in the range [1, n] inclusive.
+Given an array of integers numbers containing n + 1 integers where each integer is in the range [1, n] inclusive.
+
 There is only one repeated number in the array, but it may appear more than once.
 Return the repeated number.
 
@@ -16,6 +17,7 @@ Input: numbers = [3,1,3,4,2]
 Output: 3
 
 Дан массив целых чисел numbers, содержащий n + 1 элемент, где каждый элемент находится в диапазоне от 1 до n включительно.
+
 В массиве ровно одно число повторяется.
 Найдите и верните повторяющееся число.
 
@@ -34,15 +36,18 @@ Output: 3
 
 from typing import List
 
-def findDuplicate(numbers: List[int]) -> int:
+def find_duplicate(numbers: List[int]) -> int:
     slow = numbers[0]
     fast = numbers[0]
+
     while True:
         slow = numbers[slow]
         fast = numbers[numbers[fast]]
         if slow == fast:
             break
+
     slow = numbers[0]
+
     while slow != fast:
         slow = numbers[slow]
         fast = numbers[fast]

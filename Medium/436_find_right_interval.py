288fcb98deecd0439ce82c17c25187bdfca2f532
Viktor <viktor.sbruev.1999@gmail.com>
2025-07-10 19:02:33 +0300

    Formatted task 436

--- Medium/436_find_right_interval.py
+++ Medium/436_find_right_interval.py
@@ -1,11 +1,8 @@
 '''
 You are given an array of intervals, where intervals[i] = [startᵢ, endᵢ], and each startᵢ is unique.
-For each interval i, the right interval is defined as an interval j such that:
-    + startⱼ >= endᵢ, and
-    + startⱼ is the smallest possible value that satisfies the above condition.
+For each interval i, the right interval is defined as an interval j such that: startⱼ >= endᵢ, and startⱼ is the smallest possible value that satisfies the above condition.
 
-Return an array of indices — for each interval i, return the index j of its right interval.
-If no such interval j exists, return -1 for that position.
+Return an array of indices — for each interval i, return the index j of its right interval. If no such interval j exists, return -1 for that position.
 
 Examples:
 Input: intervals = [[1,2]]
@@ -15,12 +12,9 @@ Input: intervals = [[3,4],[2,3],[1,2]]
 Output: [-1, 0, 1]
 
 Дан массив интервалов, где intervals[i] = [startᵢ, endᵢ], и все startᵢ — уникальны.
-Для каждого интервала i найдите его правый интервал j, такой что:
-    + startⱼ >= endᵢ, и
-    + startⱼ — наименьшее возможное значение, удовлетворяющее этому условию.
+Для каждого интервала i найдите его правый интервал j, такой что: startⱼ >= endᵢ, и startⱼ — наименьшее возможное значение, удовлетворяющее этому условию.
 
-Верните массив индексов: для каждого интервала i укажите индекс j его правого интервала.
-Если такого интервала не существует — поставьте -1 на этой позиции.
+Верните массив индексов: для каждого интервала i укажите индекс j его правого интервала. Если такого интервала не существует — поставьте -1 на этой позиции.
 
 Примеры:
 Ввод: intervals = [[1,2]]
@@ -32,6 +26,7 @@ Output: [-1, 0, 1]
 
 def find_right_interval(intervals: list[list[int]]) -> list[int]:
     list_start_and_index = []
+
     for index in range(len(intervals)):
         start_point = intervals[index][0]
         list_start_and_index.append((start_point, index))

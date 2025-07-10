910645685ac2ba9ed6d451cf88eb320e5a05adcd
Viktor <viktor.sbruev.1999@gmail.com>
2025-07-10 18:09:35 +0300

    Formatted task 322

--- Medium/322_coin_change.py
+++ Medium/322_coin_change.py
@@ -1,8 +1,7 @@
 '''
-You are given an integer array coins representing coins of different denominations, and an integer amount representing a total amount of money.
-Return the fewest number of coins that you need to make up that amount.
-If that amount of money cannot be made up by any combination of the coins, return -1.
-You may assume that you have an infinite number of each kind of coin.
+Given an integer array coins representing coins of different denominations, and an integer amount representing a total amount of money.
+
+Return the fewest number of coins that you need to make up that amount. If that amount of money cannot be made up by any combination of the coins, return -1. You may assume that you have an infinite number of each kind of coin.
 
 Examples:
 Input: coins = [1, 2, 5], amount = 11
@@ -12,9 +11,8 @@ Input: coins = [2], amount = 3
 Output: -1
 
 Вам дан массив целых чисел coins, представляющий номиналы монет, и целое число amount, представляющее общую сумму денег.
-Найдите наименьшее количество монет, необходимое для составления этой суммы.
-Если невозможно набрать такую сумму с помощью данных монет, верните -1.
-Можно считать, что каждой монеты доступно бесконечно много.
+
+Найдите наименьшее количество монет, необходимое для составления этой суммы. Если невозможно набрать такую сумму с помощью данных монет, верните -1. Можно считать, что каждой монеты доступно бесконечно много.
 
 Примеры:
 Ввод: coins = [1, 2, 5], amount = 11
@@ -24,7 +22,7 @@ Output: -1
 Вывод: -1
 '''
 
-def coin_сhange(self, coins: List[int], amount: int) -> int:
+def coin_сhange(coins: List[int], amount: int) -> int:
     min_number_of_coins = [float('inf')] * (amount + 1)
     min_number_of_coins[0] = 0
 

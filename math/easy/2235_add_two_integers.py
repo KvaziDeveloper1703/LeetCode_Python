cd44926c49a31a798c80676115b6c8f0d3c184d1
Viktor <viktor.sbruev.1999@gmail.com>
2025-12-11 22:27:37 +0200

    Added task 2235

---
+++ math/easy/2235_add_two_integers.py
@@ -0,0 +1,26 @@
+'''
+You are given two integers, number_1 and number_2.
+
+Your task is to return the sum of these two integers.
+
+Examples:
+Input: number_1 = 12, number_2 = 5
+Output: 17
+
+Input: number_1 = -10, number_2 = 4
+Output: -6
+
+Даны два целых числа - number_1 и number_2.
+
+Нужно вернуть их сумму.
+
+Примеры:
+Ввод: number_1 = 12, number_2 = 5
+Вывод: 17
+
+Ввод: number_1 = -10, number_2 = 4
+Вывод: -6
+'''
+
+def sum(number_1: int, number_2: int) -> int:
+    return number_1 + number_2
